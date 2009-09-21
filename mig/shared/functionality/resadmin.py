22c68705e911e7d4439ffe7e59e49960dd4336a3
Jonas Bardino <jonas.bardino@gmail.com>
2009-09-21 10:13:35 +0000

    optimize resource administration load times with a lot of sandbox or oneclick resources
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@622 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/functionality/resadmin.py
+++ mig/shared/functionality/resadmin.py
@@ -395,6 +395,11 @@ def main(client_id, user_arguments_dict):
     base = configuration.resource_home
     owned = 0
     for resource_dir in glob.glob(base + os.sep + '*'):
+        dir_name = os.path.basename(resource_dir)
+        if dir_name.startswith('sandbox.'):
+            continue
+        elif dir_name.startswith('oneclick.'):
+            continue
         if os.path.isdir(resource_dir):
             unique_resource_name = resource_dir.replace(base, '')
             if is_owner(client_id, unique_resource_name,
