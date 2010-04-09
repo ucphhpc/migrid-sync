85e5bbbaa8e3583c89fd312af73cbdd283609df8
Jonas Bardino <jonas.bardino@gmail.com>
2010-04-09 09:49:00 +0000

    merge some improvements on docs page from grid.dk rev 1016
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1030 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/functionality/docs.py
+++ mig/shared/functionality/docs.py
@@ -272,13 +272,24 @@ def main(client_id, user_arguments_dict):
     output_objects.append({'object_type': 'header', 'text'
                           : '%s On-demand Documentation' % \
                             configuration.short_title })
-    output_objects.append({'object_type': 'text',
-                           'text': '''
+    if not show:
+        output_objects.append({'object_type': 'text',
+                               'text': '''
 This is the integrated help system for %s. You can search for a topic in the
 search field or select the particular section directly. Please note that the
 integrated help is rather limited to short overviews and technical
-specifications. Please find more user friendly tutorials and examples on the
-official site support pages.''' % configuration.short_title})
+specifications.''' % configuration.short_title })
+
+        output_objects.append({'object_type': 'text',
+                               'text': '''
+You can find more user friendly tutorials and examples on the
+official site support pages:''' })
+        output_objects.append({'object_type': 'link', 'destination':
+                               configuration.site_external_doc,
+                               'class': 'urllink', 'title': 'external documentation',
+                               'text': 'external %s documentation' % \
+                               configuration.site_title })
+
     html = '<p>Filter (using *,? etc.)'
     html += "<form method='post' action='docs.py'>"
     html += "<input type='hidden' name='show' value='' />"
