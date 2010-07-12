b6681034eafcb67e9e22cd045230574440d88dac
Jonas Bardino <jonas.bardino@gmail.com>
2010-07-12 07:41:44 +0000

    change sometimes confusing slash separator in multilinkline output to use comma. E.g. used for the list og Setting sub pages where they could be confused for current path shortcuts.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1184 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/output.py
+++ mig/shared/output.py
@@ -778,7 +778,7 @@ Exit code: %s Description: %s<br />
             if len(links) == 0:
                 lines.append('No links found!')
             else:
-                lines.append(' / '.join([html_link(link) for link in
+                lines.append(' , '.join([html_link(link) for link in
                              links]))
         elif i['object_type'] == 'file':
             lines.append(i['name'])
