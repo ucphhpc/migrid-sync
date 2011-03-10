97485fa467c0c869a0fab23c973511df2b59d6d1
Jonas Bardino <jonas.bardino@gmail.com>
2011-03-10 14:48:06 +0000

    add license header and introduce command line options
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1539 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/grsfs-fuse/benchmarks/code/rpcclient.py
+++ mig/grsfs-fuse/benchmarks/code/rpcclient.py
@@ -1,4 +1,96 @@
-import xmlrpclib,timeit
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# --- BEGIN_HEADER ---
+#
+# rpcclient - simple rpc client benchmark
+# Copyright (C) 2003-2011  The MiG Project lead by Brian Vinter
+# 
+# This file is part of MiG.
+# 
+# MiG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# MiG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# -- END_HEADER ---
+#
 
-setup = """import xmlrpclib; proxy = xmlrpclib.ServerProxy("http://n7:8000/")"""
-print timeit.repeat("proxy.x()", setup = setup, repeat=3, number=1000)
+
+"""Simple RPC client benchmark"""
+
+import sys
+import getopt
+import xmlrpclib
+import timeit
+
+def default_configuration():
+    """Return dictionary with default configuration values"""
+    conf = {'url': "http://localhost:8000/", 'repeat': 3, 'number': 10000}
+    return conf
+
+def usage():
+    """Usage help"""
+    print("Usage: %s" % sys.argv[0])
+    print("Run RPC client benchmark")
+    print("Options and default values:")
+    for (key, val) in default_configuration().items():
+        print("--%s: %s" % (key, val))
+                
+def main(conf):
+    """Run timed benchmark"""
+    print timeit.repeat("proxy.x()", setup = conf['setup'],
+                        repeat=conf['repeat'], number=conf['number'])
+
+
+if __name__ == '__main__':
+    conf = default_configuration()
+
+    # Parse command line
+
+    try:
+        (opts, args) = getopt.getopt(sys.argv[1:],
+                                     'hn:r:u:', [
+            'help',
+            'number=',
+            'repeat=',
+            'url=',
+            ])
+    except getopt.GetoptError, err:
+        print('Error in option parsing: ' + err.msg)
+        usage()
+        sys.exit(1)
+        
+    for (opt, val) in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+        elif opt in ('-n', '--number'):
+            try:
+                conf["number"] = int(val)
+            except Exception, err:
+                print('Error in parsing number value: %s' % err)
+                sys.exit(1)
+        elif opt in ('-r', '--repeat'):
+            try:
+                conf["repeat"] = int(val)
+            except Exception, err:
+                print('Error in parsing repeat value: %s' % err)
+                sys.exit(1)
+        elif opt in ('-u', '--url'):
+            conf["url"] = val
+        else:
+            print("unknown option: %s" % opt)
+            usage()
+            sys.exit(1)
+    conf['setup'] = """import xmlrpclib; proxy = xmlrpclib.ServerProxy('%(url)s')""" % conf
+    main(conf)
--- mig/grsfs-fuse/benchmarks/code/rpcserver.py
+++ mig/grsfs-fuse/benchmarks/code/rpcserver.py
@@ -1,10 +1,101 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# --- BEGIN_HEADER ---
+#
+# rpcserver - minimal rpc benchmark server
+# Copyright (C) 2003-2011  The MiG Project lead by Brian Vinter
+# 
+# This file is part of MiG.
+# 
+# MiG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# MiG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# -- END_HEADER ---
+#
+
+
+"""Minimal RPC benchmark server"""
+
+import sys
+import getopt
 import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
+def default_configuration():
+    """Return dictionary with default configuration values"""
+    # Use empty address to listen on all interfaces
+    conf = {'address': "", 'port': 8000}
+    return conf
+
+def usage():
+    """Usage help"""
+    print("Usage: %s" % sys.argv[0])
+    print("Run RPC benchmark server")
+    print("Options and default values:")
+    for (key, val) in default_configuration().items():
+        print("--%s: %s" % (key, val))
+                
 def x():
         return True
 
-server = SimpleXMLRPCServer(('', 8000))
-print "Listening on port 8000..."
-server.register_function(x, "x")
-server.serve_forever()
+def main(conf):
+    """Run minimal benchmark server"""
+    server = SimpleXMLRPCServer((conf['address'], conf['port']))
+    print("Listening on '%(address)s:%(port)d..." % conf)
+    server.register_function(x, "x")
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    conf = default_configuration()
+
+    try:
+        raise Exception("test")
+    except Exception as err:
+        print("caught error: %s" % err)
+        
+    # Parse command line
+
+    try:
+            (opts, args) = getopt.getopt(sys.argv[1:],
+                                         'a:hp:', [
+                    'address=',
+                    'help',
+                    'port=',
+                    ])
+    except getopt.GetoptError as err:
+        print('Error in option parsing: ' + err.msg)
+        usage()
+        sys.exit(1)
+        
+    for (opt, val) in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+        elif opt in ('-a', '--address'):
+                conf["address"] = val
+        elif opt in ('-p', '--port'):
+            try:
+                conf["port"] = int(val)
+            except Exception, err:
+                print('Error in parsing port value: %s' % err)
+                sys.exit(1)
+        elif opt in ('-u', '--url'):
+            conf["url"] = val
+        else:
+            print("unknown option: %s" % opt)
+            usage()
+            sys.exit(1)
+    main(conf)
