0ca413a3cbbfa05441bb88810373c0ad613ab118
Jonas Bardino <jonas.bardino@gmail.com>
2011-03-14 15:24:56 +0000

    use apt-ftparchive for all operations and create both raw and gzipped files to make update-manager happy
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1561 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/install/deb-repo/updaterepo.sh
+++ mig/install/deb-repo/updaterepo.sh
@@ -11,17 +11,17 @@ SOURCE_DIR="${DIST}/${SECTION}/source"
 echo '= Scanning binaries ='
 for ARCH in $ARCHITECTURES; do
         BINARY_DIR="${DIST}/${SECTION}/binary-${ARCH}"
-        dpkg-scanpackages ${POOL_DIR} /dev/null | gzip -9c > ${BINARY_DIR}/Packages.gz
-	#gunzip -c ${BINARY_DIR}/Packages.gz > ${BINARY_DIR}/Packages
+	apt-ftparchive -o APT::FTPArchive::Release::Origin='Minimum intrusion Grid' -o APT::FTPArchive::Release::Label='MiG' -o APT::FTPArchive::Release::Suite='stable' -o APT::FTPArchive::Release::Codename='stable' -o APT::FTPArchive::Release::Architectures="${ARCHITECTURES}" -o APT::FTPArchive::Release::Components='main stable' packages ${POOL_DIR} | gzip -9c > ${BINARY_DIR}/Packages.gz
+	gunzip -c ${BINARY_DIR}/Packages.gz > ${BINARY_DIR}/Packages
 done
 
 echo '= Scanning sources ='
-#dpkg-scansources --debug ${POOL_DIR} /dev/null
-dpkg-scansources ${POOL_DIR} /dev/null | gzip -9c > ${SOURCE_DIR}/Sources.gz
+apt-ftparchive -o APT::FTPArchive::Release::Origin='Minimum intrusion Grid' -o APT::FTPArchive::Release::Label='MiG' -o APT::FTPArchive::Release::Suite='stable' -o APT::FTPArchive::Release::Codename='stable' -o APT::FTPArchive::Release::Architectures="${ARCHITECTURES}" -o APT::FTPArchive::Release::Components='main stable' sources ${POOL_DIR} | gzip -9c > ${SOURCE_DIR}/Sources.gz
+gunzip -c ${SOURCE_DIR}/Sources.gz > ${SOURCE_DIR}/Sources
 
 echo '= Updating repo index ='
 rm -f  ${DIST}/Release.gpg
-apt-ftparchive -o APT::FTPArchive::Release::Origin='Minimum intrusion Grid' -o APT::FTPArchive::Release::Label='MiG' -o APT::FTPArchive::Release::Suite='stable' -o APT::FTPArchive::Release::Codename='stable' -o APT::FTPArchive::Release::Architectures="${ARCHITECTURES}" -o APT::FTPArchive::Release::Components='main stable' release . > ${DIST}/Release
+(cd ${DIST} && apt-ftparchive -o APT::FTPArchive::Release::Origin='Minimum intrusion Grid' -o APT::FTPArchive::Release::Label='MiG' -o APT::FTPArchive::Release::Suite='stable' -o APT::FTPArchive::Release::Codename='stable' -o APT::FTPArchive::Release::Architectures="${ARCHITECTURES}" -o APT::FTPArchive::Release::Components='main stable' release . > Release)
 echo ''
 echo '= Signing repo index with MiG signing key ='
 echo '... the passphrase is included in the MiG developers VGrid SCM'
