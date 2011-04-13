b8e335c565e24c04a5c6ca4a4bcd2112c5de209d
Benjamin Sedoc <bsedoc@yahoo.com>
2011-04-13 11:26:49 +0000

    Fixed timelimit specification to use minutes instead of seconds. Also a minor --job-name flag correction.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1612 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/resource/slurm-scripts/submit_job.sh
+++ mig/resource/slurm-scripts/submit_job.sh
@@ -18,9 +18,12 @@ else
     mail_opt="--mail-user=$MIG_ADMINEMAIL"
 fi
 
-# PBS does not allow total disk limit so we ignore it
-sbatch --time=$MIG_JOBCPUTIME --nodes=${MIG_JOBNODECOUNT} --mincpus=${MIG_JOBCPUCOUNT} \
-    --mem=${MIG_JOBMEMORY} --no-requeue --job-name $MIG_JOBNAME \
+# mig cputime uses seconds but sbatch uses minutes
+mins=$(($MIG_JOBCPUTIME/60))
+secs=$(($MIG_JOBCPUTIME%60))
+
+sbatch --time=$mins:$secs --nodes=${MIG_JOBNODECOUNT} --mincpus=${MIG_JOBCPUCOUNT} \
+    --mem=${MIG_JOBMEMORY} --no-requeue --job-name=$MIG_JOBNAME \
     -o $MIG_JOBNAME.out -e $MIG_JOBNAME.err --workdir=$MIG_JOBDIR $mail_opt $@
 if [ $? -ne 0 ]; then
     echo "Failed to submit to SLURM - try again later"
