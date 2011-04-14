f83bd5251fec999b8174d56658922e31d69a07ad
Jonas Bardino <jonas.bardino@gmail.com>
2011-04-14 10:36:00 +0000

    prepare for future People page
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1616 b75ad72c-e7d7-11dd-a971-7dbc132099af

---
+++ mig/shared/user.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# --- BEGIN_HEADER ---
+#
+# user - helper functions for user related tasks
+# Copyright (C) 2003-2011  The MiG Project lead by Brian Vinter
+#
+# This file is part of MiG.
+#
+# MiG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# MiG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# -- END_HEADER ---
+#
+
+"""User related functions - especially for People interface"""
+
+import dircache
+import os
+try:
+    from hashlib import md5 as hash_algo
+except ImportError:
+    from md5 import new as hash_algo
+
+from shared.useradm import client_id_dir
+import shared.profilekeywords as profilekeywords
+
+def anon_user_id(user_id):
+    """Generates an anonymous but (practically) unique user ID for user with
+    provided unique user_id. The anonymous ID is just a md5 hash of the
+    user_id to keep ID relatively short.
+    """
+    anon_id = hash_algo(user_id).hexdigest()
+    return anon_id
+
+def list_users(user_home):
+    """Return a list of all users by listing the user homes in user_home.
+    Uses dircache for efficiency when used more than once per session.
+    """
+    users = []
+    children = dircache.listdir(user_home)
+    for name in children:
+        path = os.path.join(user_home, name)
+
+        # skip all files and dot dirs - they are _not_ users
+        
+        if not os.path.isdir(path):
+            continue
+        if path.find(os.sep + '.') != -1:
+            continue
+        users.append(name)
+    return users
+
+def anon_to_real_user_map(user_home):
+    """Return a mapping from anonymous user names to real names"""
+    anon_map = {}
+    for name in list_users(user_home):
+        anon_map[anon_user_id(name)] = name
+    return anon_map
+
+def real_to_anon_user_map(user_home):
+    """Return a mapping from real user names to anonymous names"""
+    user_map = {}
+    for name in list_users(user_home):
+        user_map[name] = anon_user_id(name)
+    return user_map
+
