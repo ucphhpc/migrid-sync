0e6815255853d520f477665fd229c3db1bffae33
Benjamin Sedoc <bsedoc@yahoo.com>
2011-10-12 11:37:06 +0000

    template for user_settings file.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1736 b75ad72c-e7d7-11dd-a971-7dbc132099af

---
+++ user-projects/MatlabOnGrid/gridmatlab/user_settings_template.py
@@ -0,0 +1,4 @@
+
+root_directory= PATH_TO_MATLAB_ON_GRID # example : /home/user/MatlabOnGrid
+
+MCR_path = PATH_TO_MCR_FOLDER # (only used for local mode) example : "/home/user/programs/MATLAB_Compiler_Runtime/v713/"
