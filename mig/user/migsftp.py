7a2e53fbac55c1bd7e8d095b10b330e820260a10
Jonas Bardino <jonas.bardino@gmail.com>
2011-11-22 18:56:02 +0000

    more configurable sftp client
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1760 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/user/migsftp.py
+++ mig/user/migsftp.py
@@ -3,7 +3,7 @@
 #
 # --- BEGIN_HEADER ---
 #
-# migsftp - simple sftp client
+# migsftp - sample paramiko-based sftp client for user home access
 # Copyright (C) 2003-2011  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
@@ -25,19 +25,79 @@
 # -- END_HEADER ---
 #
 
-"""Test python+paramiko sftp against MiG server"""
+"""Sample Paramiko-based sftp client working with your MiG home.
+
+Run with:
+python migsftp.py
+
+Please check the global configuration section below if it fails. The comments
+should help solve the most common problems.
+
+This example should be a good starting point for writing your own custom sftp
+client acting on your MiG home.
+"""
 
 import os
 import sys
 import paramiko
 
-print "Please enter the looong username from your MiG ssh settings page"
-user = raw_input('Username: ')
+
+### Global configuration ###
+
+server_fqdn = 'dk.migrid.org'
+server_port = 2222
+server_host_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA0ImsGTKx3Rky7jaGDRVtse80YUcVTYW5NCvU0ntclfosdlFdDli8S3tOLk47DcwZkYt1/XY4rP/LN6unVTiZK7dpRTACuSGrKc/TVM63TzG9Zwq1M95pNLdhgRJen1Ez7CzbrWDcsFJNfjxJtvnIWuKmXJ8NBbmhw1nqtZRdvcF7aLX12KxCxcpJLPtU0N/cbRghi2BTYsGbPUrVd1vYJKhtvc2dQ+vfOiYGSj1bo3LOdTsLmpOoImGvYyGnpA8mVgc4sbWW6/RVSkIJxnyoUeP/xgsMQlfcXLZ/9vi/QPe64UVAAAdk18+eNnjHq2Qs8fxHMVyV2vhLpP/xFdJVNQ=="
+known_hosts_path = os.path.expanduser("~/.ssh/known_hosts")
+user_key = None
+host_key_policy = paramiko.RejectPolicy()
+data_compression = True
+
+# Uncomment the next line if you don't have a valid key in ssh-agent or ~/.ssh/
+# but use a key from ~/.mig/id_rsa instead. Obviously, you can modify the path
+# if your key is stored elsewhere.
+#user_key = [os.path.expanduser('~/.mig/id_rsa')]
+
+# Uncomment the next line if you have not connected to MiG before
+# and want to silently accept the host key - please beware of the
+# security implications!
+#host_key_policy = paramiko.AutoAddPolicy()
+# ... or the next line if you want a warning in that case but want to continue
+#host_key_policy = paramiko.WarningPolicy()
+
+# Uncomment the next line if don't want compressed transfers. This is a trade
+# off between CPU usage and throughput
+#data_compression = False
+
+
+### Initialize client session ###
+
+# Get auto-generated username from command line
+
+print "Please enter/paste the looong username from your MiG ssh settings page"
+user_name = raw_input('Username: ')
+
+if len(user_name) < 64:
+    print "Warning: the supplied username is shorter than expected!"
+    print "Please verify it on your MiG ssh Settings page in case of failure."
+
+# Connect with provided settings
+
 ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(
-        paramiko.AutoAddPolicy())
-ssh.connect('dk-cert.migrid.org', username=user, port=2222)
+known_host_keys = ssh.get_host_keys()
+key_type, key_data = server_host_key.split(' ')[:2]
+pub_key = paramiko.PKey(msg=server_fqdn, data=key_data)
+known_host_keys.add(server_fqdn, key_type, pub_key)
+known_host_keys.load(known_hosts_path)
+ssh.set_missing_host_key_policy(host_key_policy)
+ssh.connect(server_fqdn, username=user_name, port=server_port,
+            key_filename=user_key, compress=data_compression)
 ftp = ssh.open_sftp()
+
+
+### Sample actions on your MiG home directory ###
+
+# List and stat files in the remote .ssh dir which should always be there
+
 base = '.ssh'
 files = ftp.listdir(base)
 path_stat = ftp.stat(base)
@@ -47,4 +107,9 @@ for name in files:
     rel_path = os.path.join(base, name)
     path_stat = ftp.stat(rel_path)
     print "stat %s:\n%s" % (rel_path, path_stat)
+
+
+### Clean up before exit ###
+
 ftp.close()
+ssh.close()
