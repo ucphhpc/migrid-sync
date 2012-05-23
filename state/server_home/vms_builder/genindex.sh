11f5a2238cc6f8e031dfdbb02dadfc7df6ec9e5f
Jonas Bardino <jonas.bardino@gmail.com>
2012-05-23 15:36:14 +0000

    image index generator
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@1988 b75ad72c-e7d7-11dd-a971-7dbc132099af

---
+++ state/server_home/vms_builder/genindex.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Generates html index of qcow2 and vmdk data images in target dir 
+
+# Change run to 'echo' to only show commands for partial manual runs
+#run='echo'
+run=''
+
+srcdir="$1"
+if [ ! -d $srcdir ]; then
+    echo "No such input dir: $srcdir"
+    exit 1
+fi
+index="$srcdir/index.html"
+
+$run sync
+echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>MiG Virtual Machine Images</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="generator" content="emacs">
+<meta name="description" content="MiG virtual machine images">
+<meta name="keywords" content="virtual machine image">
+<meta name="copyright" content="The MiG team lead by Brian Vinter">
+<link rel="stylesheet" type="text/css" href="/images/site.css" media="screen"/>
+<link rel="stylesheet" type="text/css"
+      href="/images/css/jquery.managers.css" media="screen"/>
+<link rel="stylesheet" type="text/css" href="/images/css/jquery-ui.css"
+      media="screen"/>
+</head>
+<body>
+<div class="title">
+<h1>MiG Virtual Machine Images</h1>
+</div>
+<div id="content">
+<h1>Individual Images</h1>' > $index
+echo '<ul>' >> $index
+
+for src in $(ls $srcdir/*.vmdk $srcdir/*.qcow2 2> /dev/null); do
+    echo "Adding $src"
+    img=$(basename $src)
+    echo '<li><a href="'$img'">'$img'</a></li>' >> $index
+done
+echo '</ul>' >> $index
+echo '</div>
+</body>
+</html>' >> $index
+
+$run sync
