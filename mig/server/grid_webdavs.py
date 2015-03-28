3316fc29469d12c98ad1eb9361974c4b21663ad2
Jonas Bardino <jonas.bardino@gmail.com>
2015-03-28 15:54:08 +0000

    temporarily disable last webdavs hardening as it breaks most OSX uploads, except very small files. Investigate and fix if possible.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@2567 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/server/grid_webdavs.py
+++ mig/server/grid_webdavs.py
@@ -46,6 +46,7 @@ try:
     from wsgidav.server import __file__ as server_init_path
     sys.path.append(os.path.dirname(server_init_path))
     from cherrypy import wsgiserver
+    from cherrypy.wsgiserver.ssl_builtin import BuiltinSSLAdapter
     from cherrypy.wsgiserver.ssl_pyopenssl import pyOpenSSLAdapter
     from OpenSSL import SSL
     from wsgidav.fs_dav_provider import FileResource, FolderResource, \
@@ -418,22 +419,24 @@ def run(configuration):
         config['ssl_private_key'] = configuration.user_davs_key
         config['ssl_certificate_chain'] = ''
 
+        # TMP! reverted to basic SSL due to problems with OSX uploads otherwise
         # Important: disable SSLv2/SSLv3 and weak ciphers like we do in Apache
         # http://roadha.us/2014/10/disable-sslv3-avoid-poodle-attack-web-py/
         cert = config['ssl_certificate']
         key = config['ssl_private_key']
         chain = config['ssl_certificate_chain']
-        wsgiserver.CherryPyWSGIServer.ssl_adapter = pyOpenSSLAdapter(cert, key, chain)
-        wsgiserver.CherryPyWSGIServer.ssl_adapter.context = SSL.Context(SSL.SSLv23_METHOD)
-        wsgiserver.CherryPyWSGIServer.ssl_adapter.context.set_options(SSL.OP_NO_SSLv2)
-        wsgiserver.CherryPyWSGIServer.ssl_adapter.context.set_options(SSL.OP_NO_SSLv3)
-        wsgiserver.CherryPyWSGIServer.ssl_adapter.context.use_certificate_file(cert)
-        wsgiserver.CherryPyWSGIServer.ssl_adapter.context.use_privatekey_file(key)
-        if chain:
-            wsgiserver.CherryPyWSGIServer.ssl_adapter.context.use_certificate_chain_file(chain)
-        # Mirror Apache ciphers
-        ciphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA"
-        wsgiserver.CherryPyWSGIServer.ssl_adapter.context.set_cipher_list(ciphers)
+        wsgiserver.CherryPyWSGIServer.ssl_adapter = BuiltinSSLAdapter(cert, key, chain)
+        #wsgiserver.CherryPyWSGIServer.ssl_adapter = pyOpenSSLAdapter(cert, key, chain)
+        #wsgiserver.CherryPyWSGIServer.ssl_adapter.context = SSL.Context(SSL.SSLv23_METHOD)
+        #wsgiserver.CherryPyWSGIServer.ssl_adapter.context.set_options(SSL.OP_NO_SSLv2)
+        #wsgiserver.CherryPyWSGIServer.ssl_adapter.context.set_options(SSL.OP_NO_SSLv3)
+        #wsgiserver.CherryPyWSGIServer.ssl_adapter.context.use_certificate_file(cert)
+        #wsgiserver.CherryPyWSGIServer.ssl_adapter.context.use_privatekey_file(key)
+        #if chain:
+        #    wsgiserver.CherryPyWSGIServer.ssl_adapter.context.use_certificate_chain_file(chain)
+        ## Mirror Apache ciphers
+        #ciphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA"
+        #wsgiserver.CherryPyWSGIServer.ssl_adapter.context.set_cipher_list(ciphers)
 
     # Use bundled CherryPy WSGI Server to support SSL
     version = "%s WebDAV" % configuration.short_title
