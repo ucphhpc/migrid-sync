35d34cfb7a2c33042d8ac5f30f09d2d2ddc0cb54
Jonas Bardino <jonas.bardino@gmail.com>
2015-05-14 15:54:53 +0000

    clean up oidping and always log result for easier debug of user problems resulting from oid server outages
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@2630 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/functionality/oidping.py
+++ mig/shared/functionality/oidping.py
@@ -27,14 +27,13 @@
 
 """Check availability of OpenID server back end"""
 
-import os
 import urllib
 
 import shared.returnvalues as returnvalues
 from shared.functional import validate_input
-from shared.init import initialize_main_variables, find_entry
+from shared.init import initialize_main_variables
 
-def signature(configuration):
+def signature():
     """Signature of the main function"""
 
     defaults = {'url': ['']}
@@ -45,36 +44,50 @@ def main(client_id, user_arguments_dict):
 
     (configuration, logger, output_objects, op_name) = \
         initialize_main_variables(client_id, op_header=False, op_menu=False)
-    defaults = signature(configuration)[1]
+    defaults = signature()[1]
     (validate_status, accepted) = validate_input(user_arguments_dict,
             defaults, output_objects, allow_rejects=False)
     if not validate_status:
         return (accepted, returnvalues.CLIENT_ERROR)
 
     oid_url = accepted['url'][-1]
-    openid_status = {'object_type': 'openid_status', 'server': None, 'status': None,
-                     'error': ""} 
+    openid_status = {'object_type': 'openid_status', 'server': None,
+                     'status': None, 'error': ""}
+    # IMPORTANT: we only allow ping of configured openid servers to avoid abuse
+    # otherwise the urlopen could be tricked to use e.g. file:/etc/passwd or
+    # be used to attack remote sites
     if oid_url in configuration.user_openid_providers:
         # TODO: build url from conf
         ping_url = oid_url.replace("/id/", "/ping")
         openid_status['server'] = ping_url
-        ping_status = urllib.urlopen(ping_url)
-        http_status = ping_status.getcode()
-        if http_status == 200:
+        try:
+            ping_status = urllib.urlopen(ping_url)
+            http_status = ping_status.getcode()
             data = ping_status.read()
             ping_status.close()
-            # TODO: better parsing
-            if "<h1>True</h1>" in data:
-                openid_status['status'] = "online"
+            if http_status == 200:
+                # TODO: better parsing
+                if "<h1>True</h1>" in data:
+                    openid_status['status'] = "online"
+                else:
+                    openid_status['status'] = "down"
+                    openid_status['error'] = data
             else:
-                openid_status['status'] = "down"
-                openid_status['error'] = data
-        else:
                 openid_status['status'] = "down"
                 openid_status['error'] = "server returned error code %s" % \
                                          http_status
+        except Exception, exc:
+            openid_status['status'] = "down"
+            openid_status['error'] = "unexpected server response (%s)" % exc
+        if openid_status['status'] == "online":
+            logger.info("%s on %s succeeded" % (op_name, oid_url))
+        else:
+            logger.error("%s against %s returned error: " % (op_name, oid_url) \
+                         + " %(error)s (%(status)s)" % openid_status)
     else:
-        openid_status['server'] = 'no such server configured'
+        logger.error("%s against %s is not a valid openid provider" % \
+                     (op_name, oid_url))
+        openid_status['server'] = "no such server configured"
         openid_status['status'] = "unavailable"
         openid_status['error'] = "OpenID login from %s not enabled" % oid_url
     output_objects.append(openid_status)
