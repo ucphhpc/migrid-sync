02a981f4b3b4a8b25ddb5c6373c7e943699db239
Jonas Bardino <bardino@nbi.ku.dk>
2015-11-18 13:45:17 +0000

    minor sync between requestnewjob and requestinteractivejob
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@2852 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/cgi-sid/requestinteractivejob.py
+++ mig/cgi-sid/requestinteractivejob.py
@@ -3,8 +3,8 @@
 #
 # --- BEGIN_HEADER ---
 #
-# requestinteractivejob - [insert a few words of module description on this line]
-# Copyright (C) 2003-2009  The MiG Project lead by Brian Vinter
+# requestinteractivejob - handle interactive job requests from resources
+# Copyright (C) 2003-2015  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
 #
@@ -25,6 +25,8 @@
 # -- END_HEADER ---
 #
 
+"""Handle request for an interactive job from a resource"""
+
 import cgi
 import os
 import sys
@@ -35,6 +37,7 @@ from shared.cgishared import init_cgiscript_possibly_with_cert
 from shared.conf import get_resource_configuration, get_resource_exe
 from shared.fileio import unpickle
 from shared.findtype import is_resource
+from shared.httpsclient import check_source_ip
 from shared.livedisplaysfunctions import get_users_display_number
 from shared.validstring import valid_dir_input
 from shared.ssh import execute_on_resource, copy_file_to_exe
@@ -43,7 +46,6 @@ from shared.ssh import execute_on_resource, copy_file_to_exe
 
 (logger, configuration, client_id, o) = \
     init_cgiscript_possibly_with_cert()
-
 if str(os.getenv('REQUEST_METHOD')) != 'GET':
     o.out('Please use HTTP GET')
     o.reply_and_exit(o.ERROR)
@@ -54,28 +56,40 @@ if str(os.getenv('HTTPS')) != 'on':
     o.out('Please use HTTPS with session id for authenticating job requests!'
           )
     o.reply_and_exit(o.ERROR)
-logger.info('REQUESTINTERACTIVEJOB CALLED')
+
 
 # TODO: add session ID check here
 
 remote_ip = str(os.getenv('REMOTE_ADDR'))
 
 fieldstorage = cgi.FieldStorage()
-
-jobid = fieldstorage.getfirst('jobid', '')
-exe_name = fieldstorage.getfirst('exe', '')
+exe = fieldstorage.getfirst('exe', '')
 unique_resource_name = fieldstorage.getfirst('unique_resource_name', '')
+cputime = fieldstorage.getfirst('cputime', '10000')
+nodecount = fieldstorage.getfirst('nodecount', '1')
 localjobname = fieldstorage.getfirst('localjobname', '')
+#sandboxkey = fieldstorage.getfirst('sandboxkey', '')
+execution_delay = fieldstorage.getfirst('execution_delay', '0')
+#exe_pgid = fieldstorage.getfirst('exe_pgid', '0')
 sessionid = fieldstorage.getfirst('sessionid', '')
-
-o.out('interactivejob request from %s %s %s %s' % (remote_ip, exe_name,
-      unique_resource_name, jobid))
+jobid = fieldstorage.getfirst('jobid', '')
+o.out("interactive job request from '%s;%s;%s;%s;%s;%s;%s;%s;%s" % (
+    remote_ip,
+    exe,
+    unique_resource_name,
+    cputime,
+    nodecount,
+    localjobname,
+    execution_delay,
+    jobid,
+    sessionid,
+    ))
 
 # Please note that base_dir must end in slash to avoid access to other
 # resource dirs when own name is a prefix of another resource name
 
-base_dir = os.path.abspath(configuration.resource_home + os.sep
-                            + unique_resource_name) + os.sep
+base_dir = os.path.abspath(os.path.join(configuration.resource_home,
+                                        unique_resource_name)) + os.sep
 
 # No owner check here so we need to specifically check for illegal
 # directory traversals
@@ -86,11 +100,21 @@ if not valid_dir_input(configuration.resource_home,
     # out of bounds - rogue resource!?!?
 
     o.out('invalid unique_resource_name! %s' % unique_resource_name)
-    o.internal('requestinteractivejob called with illegal parameter(s) in what appears to be an illegal directory traversal attempt!: unique_resource_name %s, exe_name %s, client_id %s'
-                % (unique_resource_name, exe_name, client_id))
+    o.internal('requestinteractivejob called with illegal parameter(s) in what appears to be an illegal directory traversal attempt!: unique_resource_name %s, exe %s, client_id %s'
+                % (unique_resource_name, exe, client_id))
     o.reply_and_exit(o.CLIENT_ERROR)
 
-if exe_name == '':
+# Check that resource address matches request source to make DoS harder
+try:
+    check_source_ip(remote_ip, unique_resource_name)
+except ValueError, vae:
+    o.out("Warning: interactive job request not sent from expected resource address!")
+    o.internal("invalid interactive job request: %s" % vae)
+    o.reply_and_exit(o.CLIENT_ERROR)
+
+# TODO: add full session ID check here
+
+if exe == '':
     o.out('requestinteractivejob error! exe was not specified in the query string. Looks like a mis-configured resource!'
           )
     o.reply_and_exit(o.ERROR)
@@ -110,13 +134,6 @@ if localjobname == '':
           )
     o.reply_and_exit(o.ERROR)
 
-# Check that resource address matches request source
-# TODO: get real ip and enable this check
-# remote_ip = str(os.getenv("REMOTE_ADDR"))
-# resource_ip = "0.0.0.0"
-# if remote_ip != resource_ip:
-#    print "Warning: job request not sent from expected resource address!"
-#    logger.warning("job request not issued from address of resource! (%s != %s)", remote_ip, resource_ip)
 
 # TODO: check that the person who submitted the job (where the session ID points) is also the one that submitted the
 # received jobid (to avoid a verified user specifies another users job id)
@@ -150,18 +167,18 @@ if not is_resource(unique_resource_name, configuration.resource_home):
           )
     o.reply_and_exit(o.ERROR)
 
-(status, resource_config) = \
+(status, resource_conf) = \
     get_resource_configuration(configuration.resource_home,
                                unique_resource_name, logger)
 if not status:
-    o.out("No resouce_config for: '" + unique_resource_name + "'\n")
+    o.out("No resouce_conf for: '" + unique_resource_name + "'\n")
     o.reply_and_exit(o.ERROR)
 
 logger.info('getting exe')
-(status, exe) = get_resource_exe(resource_config, exe_name, logger)
+(status, exe_conf) = get_resource_exe(resource_conf, exe, logger)
 if not status:
     o.out("No EXE config for: '" + unique_resource_name + "' EXE: '"
-           + exe_name + "'")
+           + exe + "'")
     o.reply_and_exit(o.ERROR)
 
 # ################################################
@@ -197,12 +214,12 @@ local_filename = configuration.mig_system_files + jobid\
      + '.interactivejob'
 dest_filename = jobid + '.interactivejob'
 (copy_status, copy_msg) = copy_file_to_exe(local_filename,
-        dest_filename, resource_config, exe_name, logger)
+        dest_filename, resource_conf, exe, logger)
 if not copy_status:
     o.out(copy_msg)
     o.reply_and_exit(o.ERROR)
 logger.info('%s copied to resource %s  exe %s' % (local_filename,
-            unique_resource_name, exe_name))
+            unique_resource_name, exe))
 
 # TODO: does this work with execute_on_exe() instead of manual ssh?
 # execute .interactive on exe
@@ -210,9 +227,9 @@ logger.info('%s copied to resource %s  exe %s' % (local_filename,
 ssh_command = \
     'ssh -X %s@%s \\"cd %s; mv -f %s job-dir_%s; cd job-dir_%s; chmod +x %s; bash -c \'./%s\'\\"'\
      % (
-    exe['execution_user'],
-    exe['execution_node'],
-    exe['execution_dir'],
+    exe_conf['execution_user'],
+    exe_conf['execution_node'],
+    exe_conf['execution_dir'],
     dest_filename,
     localjobname,
     localjobname,
@@ -223,7 +240,7 @@ logger.info('execute_on_resource: %s' % ssh_command)
 exit_code = -1
 try:
     (exit_code, executed_command) = execute_on_resource(ssh_command,
-            False, resource_config, logger)
+            False, resource_conf, logger)
 except Exception, e:
     logger.error('Exception executing remote SSH from requestinteractivejob.py: %s '
                   % e)
@@ -235,5 +252,5 @@ if exit_code != 0:
     o.reply_and_exit(o.ERROR)
 
 o.out('requestinteractivejob OK. The job was started on the resource: %s exe: %s remote addr: %s'
-       % (unique_resource_name, exe, os.getenv('REMOTE_ADDR')))
+       % (unique_resource_name, exe_conf, os.getenv('REMOTE_ADDR')))
 o.reply_and_exit(o.OK)
