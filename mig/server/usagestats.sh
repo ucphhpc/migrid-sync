91f98fd15a9e3e8a714e24f081452d5f41968e2a
Jonas Bardino <bardino@nbi.ku.dk>
2016-02-13 10:30:07 +0000

    simple helper to show usage stats e.g. from a cron job
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@2918 b75ad72c-e7d7-11dd-a971-7dbc132099af

---
+++ mig/server/usagestats.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Show basic stats about migrid users and storage use
+# Optionally limits df output to any file system types given on command-line
+
+SERVERDIR=$(dirname $0)
+if [ -z "$SERVERDIR" ]; then
+    SERVERDIR="."
+fi
+STATEDIR="$SERVERDIR/../../state"
+
+declare -a DFOPTS
+for FSTYPE in $@; do
+    FSOPTS+=("-t $FSTYPE")
+done
+
+echo "=== Disk Use ==="
+df -h ${FSOPTS[@]}
+
+echo "=== Disk Mounts ==="
+mount ${FSOPTS[@]}
+
+echo "Where"
+echo " * vgrid_files_home is all vgrid shared folders"
+echo " * vgrid_private_base/vgrid_public_base are all vgrid web portals"
+echo " * user_home is all user home dirs"
+echo " * freeze_home is frozen archives from all users"
+
+
+echo "=== Registered Users ==="
+$SERVERDIR/searchusers.py | grep -v 'Matching users' | sed 's/ : .*//g' | sort | uniq | wc -l
+
+
+echo "=== Registered VGrids ==="
+find $STATEDIR/vgrid_home -type d | grep -v .svn | wc -l
+
+echo "=== Frozen Archives ==="
+find $STATEDIR/freeze_home -maxdepth 1 -type d -name 'archive-*' | wc -l
+
