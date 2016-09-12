55203ee35d097ca907eb5d253735475bcd586523
Jonas Bardino <bardino@nbi.ku.dk>
2016-09-12 08:29:03 +0000

    workarounds for inconsistent ctimes and update to new client-specific freeze location
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3210 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/server/usagestats.sh
+++ mig/server/usagestats.sh
@@ -36,16 +36,27 @@ echo "=== Registered VGrids ==="
 find $STATEDIR/vgrid_home -type d | grep -v .svn | wc -l
 
 echo "=== Frozen Archives ==="
-find $STATEDIR/freeze_home -maxdepth 1 -type d -name 'archive-*' | wc -l
+# TODO: update to fit only new clien_id location when migrated
+find $STATEDIR/freeze_home -maxdepth 2 -type d -name 'archive-*' | wc -l
 
 echo ""
 
 echo "== This Week =="
 echo "=== Registered Users ==="
-find $STATEDIR/user_home -maxdepth 1 -type d -ctime -7 | grep -v .svn | wc -l
+# NOTE: we explicitly seach in +C=* to avoid symlink redundancy
+# .htaccess generally remains unchanged except if user signs up again.
+# TODO: this is inaccurate as user_home ctime gets updated (by gluster?)
+# TODO: add ctime option to searchusers script and use that instead?
+#find $STATEDIR/user_home -maxdepth 1 -type d -ctime -7 | grep -v .svn | wc -l
+find $STATEDIR/user_home/+C=* -maxdepth 1 -type f -name '.htaccess' -ctime -7 | wc -l
 
 echo "=== Registered VGrids ==="
-find $STATEDIR/vgrid_home -maxdepth 1 -type d -ctime -7 | grep -v .svn | wc -l
+# NOTE: no maxdepth since nested vgrids are allowed, mindepth is known for target, however
+# TODO: this is inaccurate as vgrid_home ctime gets updated (by gluster or dircache?)
+#find $STATEDIR/vgrid_home -type d -ctime -7 | grep -v .svn | wc -l
+#find $STATEDIR/vgrid_public_base -type f -path '.vgridscm/cgi-bin/hgweb.cgi' -ctime -7 | wc -l
+find $STATEDIR/vgrid_public_base -mindepth 3 -type f -path '*/.vgridscm/cgi-bin/hgweb.cgi' -ctime -7 | wc -l
 
 echo "=== Frozen Archives ==="
-find $STATEDIR/freeze_home -maxdepth 1 -type d -name 'archive-*' -ctime -7 | wc -l
+# TODO: update to fit only new client_id location when migrated
+find $STATEDIR/freeze_home -mindepth 2 -maxdepth 3 -type f -name meta.pck -ctime -7 | wc -l
