98d9bdf92dc3966f0b003d50fe09898c6bd6118f
Jonas Bardino <bardino@nbi.ku.dk>
2017-02-16 11:41:20 +0000

    fix some partial or missing license headers
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3329 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/server/fixentities.py
+++ mig/server/fixentities.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+#
+# --- BEGIN_HEADER ---
+#
 #
 # fixentities - fix all resources and vgrids to use migrated user IDs
-# Copyright (C) 2009  Jonas Bardino
+# Copyright (C) 2003-2017  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
 #
@@ -10,15 +14,19 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#
+# 
 # MiG is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+# 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+#
+# --- END_HEADER ---
+#
 
 """Fix resource and vgrid owners and members files to contain only migrated
 user IDs from user DB if at all possible"""
