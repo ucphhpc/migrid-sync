24b558ea9c0d5a26b62ac5bc946653602f39b68f
Jonas Bardino <bardino@nbi.ku.dk>
2017-03-20 16:16:38 +0000

    minor: update stale copyright notes
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3390 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/functionality/docs.py
+++ mig/shared/functionality/docs.py
@@ -4,7 +4,7 @@
 # --- BEGIN_HEADER ---
 #
 # docs - online documentation generator
-# Copyright (C) 2003-2016  The MiG Project lead by Brian Vinter
+# Copyright (C) 2003-2017  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
 #
