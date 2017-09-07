62e68ac8461831930ddd929070509e33dfb49795
Jonas Bardino <bardino@nbi.ku.dk>
2017-09-07 10:02:50 +0000

    include experimental PAM and NSS code to allow e.g. openssh-based sftp login with MiG user auth and chrooting into user home. This should prove useful both for OpenSSH with paramiko as subsystem provider and for future full-blown RestrictFS IO daemons
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3565 b75ad72c-e7d7-11dd-a971-7dbc132099af

---
+++ mig/libnss-mig/libnss_mig.c
@@ -0,0 +1,264 @@
+/*
+ * libnss_mig.c
+ *
+ * NSS module for supporting native login of MiG 
+ * virtual users. The module checks if a user exists
+ * in the MiG system, and maps such a user to the
+ * mig-user UID and GID, but with a custom home folder.
+ *
+ * Written by Kenneth Skovhede <skovhede@nbi.ku.dk>
+ *
+ * This module is based on libnss-ato which was written by
+ * Pietro Donatini <pietro.donatini@unibo.it>.
+ *
+ * this product may be distributed under the terms of
+ * the GNU Lesser Public License.
+ *
+ * version 0.1 
+ * 
+ */
+
+#include <nss.h>
+#include <pwd.h>
+#include <shadow.h>
+#include <string.h>
+#include <stdio.h>
+#include <wordexp.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <errno.h>
+
+/* for security reasons */
+#define MIN_UID_NUMBER   500
+#define MIN_GID_NUMBER   500
+#define CONF_FILE "/etc/libnss_mig.conf"
+
+/* For statically allocated buffers */
+#define MAX_USERNAME_LENGTH (1024)
+#define PATH_BUF_LEN (2048)
+
+/* Flag to filter out debug messages */
+//#define DEBUG 1
+
+/* Helper function that writes messages to syslog */
+static void writelogmessage(int priority, const char* msg, ...) {
+	va_list args;
+
+#ifndef DEBUG
+	if (priority == LOG_DEBUG)
+		return;
+#endif /*DEBUG*/
+
+	openlog("libnss_mig", LOG_PID, LOG_AUTHPRIV);
+	va_start(args, msg);
+	vsyslog(priority, msg, args);
+	va_end(args);
+}
+
+/*
+ * the configuration /etc/libnss-mig.conf is a line
+ * with the local user data as in /etc/passwd. For example:
+ * mig-user:x:1001:1001:P D ,,,:/home/mig/state/user_home/:/bin/bash
+ * Extra lines are comments (not processed).
+ * The home folder is expected to point to the MiG state folder,
+ * where the supplied username will be appended
+ * The password is ignored and set to "x" to indicate no password
+ */
+
+static struct passwd *
+read_conf() 
+{
+	FILE *fd;
+	struct passwd *conf;
+
+	if ((fd = fopen(CONF_FILE, "r")) == NULL ) {
+		writelogmessage(LOG_ERR, "Failed to load file %s\n", CONF_FILE);
+		return NULL;
+	}
+	
+	conf = fgetpwent(fd);
+
+	if (conf == NULL) {
+		writelogmessage(LOG_ERR, "Failed to load file %s, error: %d\n", CONF_FILE, errno);
+		fclose(fd);
+		return NULL;
+	}
+
+	if ( conf->pw_uid < MIN_UID_NUMBER ) {
+		writelogmessage(LOG_WARNING, "UID was %d adjusting to %d\n", conf->pw_uid, MIN_UID_NUMBER);
+		conf->pw_uid = MIN_UID_NUMBER;
+	}
+
+	if ( conf->pw_gid < MIN_GID_NUMBER ) {
+		writelogmessage(LOG_WARNING, "GID was %d adjusting to %d\n", conf->pw_gid, MIN_GID_NUMBER);
+		conf->pw_gid = MIN_GID_NUMBER;
+	}
+
+	fclose(fd);
+	return conf;
+}
+
+/* 
+ * Allocate some space from the nss static buffer.  The buffer and buflen
+ * are the pointers passed in by the C library to the _nss_ntdom_*
+ * functions. 
+ *
+ *  Taken from glibc 
+ */
+
+static char * 
+get_static(char **buffer, size_t *buflen, int len)
+{
+	char *result;
+
+	/* Error check.  We return false if things aren't set up right, or
+         * there isn't enough buffer space left. */
+
+	if ((buffer == NULL) || (buflen == NULL) || (*buflen < len)) {
+		return NULL;
+	}
+
+	/* Return an index into the static buffer */
+
+	result = *buffer;
+	*buffer += len;
+	*buflen -= len;
+
+	return result;
+}
+
+
+enum nss_status
+_nss_mig_getpwnam_r( const char *name, 
+	   	     struct passwd *p, 
+	             char *buffer, 
+	             size_t buflen, 
+	             int *errnop)
+{
+	struct passwd *conf;
+	size_t name_len = strlen(name);
+
+	/* Since we rely on mapping the username to a path on disk,
+	   make sure the name does not contain strange things */
+	if (name_len > MAX_USERNAME_LENGTH || strstr(name, "..") != NULL || strstr(name, "/") != NULL || strstr(name, ":") != NULL) {
+		writelogmessage(LOG_WARNING, "Invalid username (%d): %s\n", name_len, name);
+		return NSS_STATUS_NOTFOUND;
+	}
+
+	if ((conf = read_conf()) == NULL) {
+		writelogmessage(LOG_WARNING, "Invalid config file, username = %s\n", name);
+		return NSS_STATUS_NOTFOUND;
+	}
+
+    char pathbuf[PATH_BUF_LEN];
+    size_t pathlen = strlen(conf->pw_dir);
+
+    /* Make sure we can fit the path into the buffer */
+    if (pathlen + name_len + 2 > PATH_BUF_LEN) {
+		writelogmessage(LOG_WARNING, "Expanded path too long, %d vs %d\n", pathlen + name_len + 2, PATH_BUF_LEN);
+    	return NSS_STATUS_NOTFOUND;
+    }
+
+    /* Build the full path */
+	strcpy(pathbuf, conf->pw_dir);
+	if (pathbuf[pathlen] != '/')
+	{
+		pathbuf[pathlen] = '/';
+		pathbuf[pathlen + 1] = 0;
+		pathlen++;
+	}
+
+	strcpy(pathbuf + pathlen, name);
+
+	/* Do resolution to remove any weirdness and symlinks */
+	char* resolved_path = realpath(pathbuf, NULL);
+	if (resolved_path == NULL) {
+		writelogmessage(LOG_WARNING, "Failed to resolve path to a real path: %s\n", pathbuf);
+		return NSS_STATUS_NOTFOUND;
+	}
+
+    /* Copy the path back to the statically 
+    * allocated buffer to avoid leaking the pointer later */
+	pathlen = strlen(resolved_path);
+	if (pathlen >= PATH_BUF_LEN) {
+		writelogmessage(LOG_WARNING, "Resolved path too long: %d, %d: %s\n", pathlen, PATH_BUF_LEN, resolved_path);
+		free(resolved_path);
+		resolved_path = NULL;
+		return NSS_STATUS_NOTFOUND;
+	}
+
+    /* Check if the folder actually exists */
+	DIR* homedir = opendir(resolved_path);
+	if (homedir) {
+		closedir(homedir);
+		/* Copy path into statically allocated buffer 
+		   so we can free the temporary buffer */
+		strcpy(pathbuf, resolved_path);
+		free(resolved_path);
+		resolved_path = NULL;
+	} else {
+		writelogmessage(LOG_WARNING, "Resolved path is not a directory: %s\n", resolved_path);
+		free(resolved_path);
+		resolved_path = NULL;
+		return NSS_STATUS_NOTFOUND;
+	}
+
+    /* All good, user is found, setup result pointer */
+	*p = *conf;
+
+	/* If out of memory */
+	if ((p->pw_name = get_static(&buffer, &buflen, strlen(name) + 1)) == NULL) {
+		return NSS_STATUS_TRYAGAIN;
+	}
+
+	/* pw_name stay as the name given */
+	strcpy(p->pw_name, name);
+
+	if ((p->pw_passwd = get_static(&buffer, &buflen, strlen("x") + 1)) == NULL) {
+        return NSS_STATUS_TRYAGAIN;
+    }
+
+    /* "x" is an indicator that the password is in the shadow file */
+	strcpy(p->pw_passwd, "x");
+
+	if ((p->pw_dir = get_static(&buffer, &buflen, pathlen + 1)) == NULL) {
+        return NSS_STATUS_TRYAGAIN;
+    }
+
+	strcpy(p->pw_dir, pathbuf);
+
+	writelogmessage(LOG_DEBUG, "Returning success for %s: %s\n", p->pw_name, p->pw_dir);
+	return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status
+_nss_mig_getspnam_r( const char *name,
+                     struct spwd *s,
+                     char *buffer,
+                     size_t buflen,
+                     int *errnop)
+{
+
+    /* If out of memory */
+    if ((s->sp_namp = get_static(&buffer, &buflen, strlen(name) + 1)) == NULL) {
+            return NSS_STATUS_TRYAGAIN;
+    }
+
+    strcpy(s->sp_namp, name);
+
+    if ((s->sp_pwdp = get_static(&buffer, &buflen, strlen("*") + 1)) == NULL) {
+            return NSS_STATUS_TRYAGAIN;
+    }
+
+    /* Set the password as invalid */
+    strcpy(s->sp_pwdp, "*");
+
+    s->sp_lstchg = 13571;
+    s->sp_min    = 0;
+    s->sp_max    = 99999;
+    s->sp_warn   = 7;
+
+    return NSS_STATUS_SUCCESS;
+}
---
+++ mig/pam-mig/libpam_mig.c
@@ -0,0 +1,452 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <pwd.h>
+#include <syslog.h>
+
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+
+#define PASSWORD_FILENAME "authorized_passwords"
+
+/* Various settings used to communicate chrooting */
+//#define ENABLE_CHROOT 1
+
+/* Setup for communicating between layers */
+#define PAM_DATA_NAME "MIG_DO_CHROOT"
+#define PAM_CHROOT_AUTHENTICATED ((void*)1)
+#define PAM_CHROOT_REQUEST ((void*)2)
+#define PAM_CHROOT_COMPLETED ((void*)3)
+
+/* For testing, the printf can be activated,
+   but should never be enabled in non-debug mode */
+//#define DEBUG_PRINTF 1
+/* Print debug messages as well */
+//#define DEBUG 1
+
+/* The sizes here are use to handle static
+   allocations of buffers */
+#define MAX_DIGEST_SIZE (2048)
+#define MAX_PATH_LENGTH (2048)
+
+/* Sanity limit to avoid really short and easily guessable
+   digest values */
+#define MIN_PBKDF_LENGTH (16)
+
+/* Helper function that writes messages to syslog */
+static void writelogmessage(int priority, const char* msg, ...) {
+	va_list args;
+
+#ifndef DEBUG
+	if (priority == LOG_DEBUG)
+		return;
+#endif /*DEBUG*/
+
+	openlog("pam_mig", LOG_PID, LOG_AUTHPRIV);
+	va_start(args, msg);
+	vsyslog(priority, msg, args);
+	va_end(args);
+
+#ifdef DEBUG
+#ifdef DEBUG_PRINTF
+	va_start(args, msg);
+	vprintf(msg, args);
+	va_end(args);	
+#endif /*DEBUG_PRINTF*/
+#endif /*DEBUG*/
+
+}
+
+/* Dump the code we depend on here, to prevent linker/loader dependencies */
+/*
+ * BEWARE: The sha2_* functions in pbkdf2 needs to be declared "static"
+ *         otherwise they clash with openssh and segfaults it.
+ *         the file has been modified to make these functions static
+ */
+
+#include "pbkdf2-sha256.c"
+#include "b64-decode.c"
+#include "b64.c"
+
+/* this function is ripped from pam_unix/support.c, it lets us do IO via PAM */
+static int converse( pam_handle_t *pamh, int nargs, struct pam_message **message, struct pam_response **response ) {
+	int retval ;
+	struct pam_conv *conv ;
+
+	retval = pam_get_item( pamh, PAM_CONV, (const void **) &conv ) ; 
+	if( retval==PAM_SUCCESS ) {
+		retval = conv->conv( nargs, (const struct pam_message **) message, response, conv->appdata_ptr ) ;
+	}
+
+	return retval ;
+}
+
+/* The do_chroot function invokes chroot to force the user into the home directory */
+#ifdef ENABLE_CHROOT
+static int do_chroot(pam_handle_t * pamh ) {
+	int retval;
+
+	const char* pUsername;
+	retval = pam_get_user(pamh, &pUsername, "Username: ");
+
+	if (retval != PAM_SUCCESS || pUsername == NULL || strlen(pUsername) == 0)
+	{
+		writelogmessage(LOG_WARNING, "Did not get a valid username ...\n");
+		if (retval != PAM_SUCCESS) {
+			return retval;
+		} else {
+			return PAM_AUTH_ERR;
+		}
+	}	
+
+	/* Since we rely on mapping the username to a path on disk,
+	   make sure the name does not contain strange things */
+	if (strstr(pUsername, "..") != NULL || strstr(pUsername, "/") != NULL || strstr(pUsername, ":") != NULL) {
+		writelogmessage(LOG_WARNING, "Username contained invalid chars: %s\n", pUsername);
+		return PAM_AUTH_ERR;
+	}
+
+	struct passwd* pw = getpwnam(pUsername);
+	if (pw == NULL) {
+		writelogmessage(LOG_WARNING, "User not found: %s\n", pUsername);
+		return PAM_AUTH_ERR;
+	}
+
+	if (chdir(pw->pw_dir) != 0) {
+		writelogmessage(LOG_WARNING, "Unable to chdir to %s\n", pw->pw_dir);
+		return PAM_AUTH_ERR;
+	}
+
+	writelogmessage(LOG_DEBUG, "Activating chroot for '%s': %s\n", pUsername, pw->pw_dir);
+	if (chroot(pw->pw_dir) == 0) {
+		writelogmessage(LOG_DEBUG, "Chroot activated (%s)!\n", strerror(errno));
+		if (chdir("/") != 0) {
+			writelogmessage(LOG_WARNING, "Unable to chdir to / after chroot\n");
+			return PAM_AUTH_ERR;
+		} else {
+			writelogmessage(LOG_DEBUG, "Changed into new root!\n");
+		}
+
+		writelogmessage(LOG_DEBUG, "Returning success ...\n");
+		return PAM_SUCCESS;
+	} else {
+		writelogmessage(LOG_WARNING, "Chroot failed to activate: %s!\n", strerror(errno));
+		return PAM_AUTH_ERR;		
+	}
+
+	return PAM_SUCCESS;
+}
+#endif /* ENABLE_CHROOT */
+
+PAM_EXTERN int pam_sm_close_session( pam_handle_t * pamh, int flags, int argc, const char** argv) {
+	writelogmessage(LOG_DEBUG, "pam_sm_close_session: %i, %i\n", flags, argc);
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_open_session( pam_handle_t * pamh, int flags, int argc, const char** argv) {
+	writelogmessage(LOG_DEBUG, "pam_sm_open_session: %i, %i\n", flags, argc);
+
+#ifdef ENABLE_CHROOT
+	int retval;
+
+    // TODO: Check if the user is a mig-mapped user,
+    // otherwise, do not chroot it
+
+	const void* val;
+	retval = pam_get_data(pamh, PAM_DATA_NAME, &val);
+	if (retval == PAM_SUCCESS && val == PAM_CHROOT_AUTHENTICATED) {
+		retval = pam_set_data(pamh, PAM_DATA_NAME, PAM_CHROOT_REQUEST, NULL);
+		if (retval != PAM_SUCCESS) {
+			writelogmessage(LOG_WARNING, "Failed to get set chroot hook\n");
+			return retval;		
+		} else {
+			writelogmessage(LOG_DEBUG, "Registered for chroot \n");
+		}
+	}
+
+#endif /*ENABLE_CHROOT*/
+
+
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_chauthtok( pam_handle_t * pamh, int flags, int argc, const char** argv) {
+	writelogmessage(LOG_DEBUG, "pam_sm_chauthtok: %i, %i\n", flags, argc);
+	return PAM_SUCCESS;
+}
+
+/* expected hook */
+PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
+	writelogmessage(LOG_DEBUG, "Set cred: %i, %i\n", flags, argc);
+
+#ifdef ENABLE_CHROOT
+
+	int retval;
+	const void* val;
+	retval = pam_get_data(pamh, PAM_DATA_NAME, &val);
+	if (retval == PAM_SUCCESS && val == PAM_CHROOT_REQUEST) {
+		retval = pam_set_data(pamh, PAM_DATA_NAME, PAM_CHROOT_COMPLETED, NULL);
+		if (retval != PAM_SUCCESS) {
+			writelogmessage(LOG_WARNING, "Failed to get unset chroot hook\n");
+			return retval;		
+		}
+		return do_chroot(pamh);
+	}
+#endif /*ENABLE_CHROOT*/
+
+	return PAM_SUCCESS;
+}
+
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+	writelogmessage(LOG_DEBUG, "Acct mgmt\n");
+	return PAM_SUCCESS;
+}
+
+/* expected hook, this is where custom stuff happens */
+PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
+	int retval;
+
+	writelogmessage(LOG_DEBUG, "In pam_sm_authenticate\n");
+
+	const char* pUsername;
+	retval = pam_get_user(pamh, &pUsername, "Username: ");
+
+	if (retval != PAM_SUCCESS || pUsername == NULL || strlen(pUsername) == 0)
+	{
+		writelogmessage(LOG_WARNING, "Did not get a valid username ...\n");
+		if (retval != PAM_SUCCESS) {
+			return retval;
+		} else {
+			return PAM_AUTH_ERR;
+		}
+
+	}	
+
+	/* Since we rely on mapping the username to a path on disk,
+	   make sure the name does not contain strange things */
+	if (strstr(pUsername, "..") != NULL || strstr(pUsername, "/") != NULL || strstr(pUsername, ":") != NULL) {
+		writelogmessage(LOG_WARNING, "Username contained invalid chars: %s\n", pUsername);
+		return PAM_AUTH_ERR;
+	}
+
+	writelogmessage(LOG_DEBUG, "Checking pw entry for '%s'\n", pUsername);
+
+	struct passwd* pw = getpwnam(pUsername);
+	if (pw == NULL) {
+		writelogmessage(LOG_WARNING, "User not found: %s\n", pUsername);
+		return PAM_AUTH_ERR;
+	}
+
+    const char* pPassword;
+	retval = pam_get_item(pamh, PAM_AUTHTOK, (const void**)&pPassword);
+	if (retval != PAM_SUCCESS) {
+		writelogmessage(LOG_INFO, "Failed to get password token\n");
+		return retval;
+	}
+
+	if (pPassword == NULL) {
+		writelogmessage(LOG_DEBUG, "No password, requesting one ...\n");
+		struct pam_message msg[1], *pmsg[1];
+		struct pam_response *resp;
+
+		pmsg[0] = &msg[0];
+		msg[0].msg_style = PAM_PROMPT_ECHO_OFF;
+		msg[0].msg = "Password: ";
+		resp = NULL;
+
+		retval = converse(pamh, 1, pmsg, &resp);
+		if (retval != PAM_SUCCESS)
+		{
+			writelogmessage(LOG_INFO, "Failed to converse\n");
+			return retval;
+		}
+
+		if (resp) {
+			if ((flags & PAM_DISALLOW_NULL_AUTHTOK) && resp[0].resp == NULL) {
+				writelogmessage(LOG_INFO, "Failed with nullauth\n");
+				free(resp);
+				return PAM_AUTH_ERR;
+			}
+
+			writelogmessage(LOG_DEBUG, "Got user password, checking with stored file ...\n");
+			pPassword = resp[0].resp;
+			resp[0].resp = NULL;
+		} else {
+			writelogmessage(LOG_INFO, "Failed to converse - 2\n");
+			return PAM_CONV_ERR;
+		}
+	}
+
+    const char* pService;
+
+	retval = pam_get_item(pamh, PAM_SERVICE, (const void**)&pService);
+	if (retval != PAM_SUCCESS) {
+		writelogmessage(LOG_WARNING, "Failed to get service name\n");
+		return retval;
+	}
+
+	char auth_filename[MAX_PATH_LENGTH];
+	if (MAX_PATH_LENGTH == snprintf(auth_filename, MAX_PATH_LENGTH, "%s/.%s/%s", pw->pw_dir, pService, PASSWORD_FILENAME))
+	{
+		writelogmessage(LOG_WARNING, "Path construction failed for: %s/.%s/%s\n", pw->pw_dir, pService, PASSWORD_FILENAME);
+		return PAM_AUTH_ERR;
+	}
+
+	if (access(auth_filename, R_OK) != 0) {
+		writelogmessage(LOG_WARNING, "Read access to file %s denied: %s\n", auth_filename, strerror(errno));
+		return PAM_AUTH_ERR;
+	}
+
+	struct stat st;
+	if (stat(auth_filename, &st) != 0) {
+		writelogmessage(LOG_WARNING, "Failed to read file size: %s\n", auth_filename);
+		return PAM_AUTH_ERR;
+	}
+
+	if (st.st_size > MAX_DIGEST_SIZE) {
+		writelogmessage(LOG_WARNING, "pbkdf digest file size was %d but only %d is allowed, filename: %s\n", st.st_size, MAX_DIGEST_SIZE, auth_filename);
+		return PAM_AUTH_ERR;
+	}
+
+	char pbkdf[MAX_DIGEST_SIZE];
+	FILE* fd = fopen(auth_filename, "rb");
+	if (fd == NULL) {
+		writelogmessage(LOG_WARNING, "Failed to open file for reading, filename: %s\n", auth_filename);
+		return PAM_AUTH_ERR;
+
+	}
+	if (fread(pbkdf, sizeof(char), st.st_size, fd) != st.st_size) {
+		writelogmessage(LOG_WARNING, "Failed to read %d bytes from filename: %s\n", st.st_size, auth_filename);
+		fclose(fd);
+		return PAM_AUTH_ERR;
+	}
+	fclose(fd);
+
+	if (strstr(pbkdf, "PBKDF2$") != pbkdf) {
+		writelogmessage(LOG_WARNING, "The pbkdf format was incorrect in file %s\n", auth_filename);
+		return PAM_AUTH_ERR;		
+	}
+
+	char* pHashAlg = strchr(pbkdf, '$');
+	if (pHashAlg == NULL) {
+		writelogmessage(LOG_WARNING, "The pbkdf hash algorithm was incorrect in %s\n", auth_filename);
+		return PAM_AUTH_ERR;		
+	}
+
+	pHashAlg++;
+
+	char* pItCount = strchr(pHashAlg, '$');
+	if (pItCount == NULL) {
+		writelogmessage(LOG_WARNING, "The pbkdf iteration count was incorrect in %s\n", auth_filename);
+		return PAM_AUTH_ERR;		
+	}
+
+	*pItCount = 0;
+	pItCount++;
+
+	char* pBase64Salt = strchr(pItCount, '$');
+	if (pBase64Salt == NULL) {
+		writelogmessage(LOG_WARNING, "The pbkdf salt was incorrect in %s\n", auth_filename);
+		return PAM_AUTH_ERR;		
+	}
+
+	*pBase64Salt = 0;
+	pBase64Salt++;
+
+	char* pBase64Hash = strchr(pBase64Salt, '$');
+	if (pBase64Hash == NULL) {
+		writelogmessage(LOG_WARNING, "The pbkdf salt was incorrect in %s\n", auth_filename);
+		return PAM_AUTH_ERR;		
+	}
+
+	*pBase64Hash = 0;
+	pBase64Hash++;
+
+	long iteration_count = strtol(pItCount, NULL, 10);
+	if (iteration_count <= 0) {
+		writelogmessage(LOG_WARNING, "The pbkdf iteration count was not a correct integer, file: %s\n", auth_filename);
+		return PAM_AUTH_ERR;
+	}
+
+	if (strcmp(pHashAlg, "sha256") != 0) {
+		writelogmessage(LOG_WARNING, "The hash algorithm should be sha256, but it was %s\n", pHashAlg);
+		return PAM_AUTH_ERR;
+	}
+
+	char pSaltAndHash[MAX_DIGEST_SIZE];
+
+	size_t salt_size = b64_get_decoded_buffer_size(strlen(pBase64Salt));
+	size_t hash_size = b64_get_decoded_buffer_size(strlen(pBase64Hash));
+
+	if (hash_size > (256/8)) {
+		writelogmessage(LOG_WARNING, "The hash was size %d, but it should be at most %d for SHA256\n", hash_size, 256 / 8);
+		return PAM_AUTH_ERR;
+	}
+
+	if (hash_size < MIN_PBKDF_LENGTH) {
+		writelogmessage(LOG_WARNING, "The hash was size %d, but it should be at least %d \n", hash_size, MIN_PBKDF_LENGTH);
+		return PAM_AUTH_ERR;
+	}
+
+	if (salt_size + hash_size > MAX_DIGEST_SIZE) {
+		writelogmessage(LOG_WARNING, "The expanded salt and hash were too big, reading from file: %s\n", auth_filename);
+		return PAM_AUTH_ERR;		
+	}
+
+	if (b64_decode((const uint8_t*)pBase64Salt, strlen(pBase64Salt), (uint8_t*)pSaltAndHash) != salt_size) {
+		writelogmessage(LOG_WARNING, "Failed to base64 decode salt from file: %s\n", auth_filename);
+		return PAM_AUTH_ERR;		
+	}
+	if (b64_decode((const uint8_t*)pBase64Hash, strlen(pBase64Hash), (uint8_t*)pSaltAndHash + salt_size) != hash_size) {
+		writelogmessage(LOG_WARNING, "Failed to base64 decode hash from file: %s\n", auth_filename);
+		return PAM_AUTH_ERR;				
+	}
+
+	writelogmessage(LOG_DEBUG, "Checking password with pbkdf value from %s ...\n", auth_filename);
+
+	char pResult[MAX_DIGEST_SIZE];
+
+	PKCS5_PBKDF2_HMAC(
+		(unsigned char*)pPassword, 
+		strlen(pPassword), 
+		(unsigned char*)pBase64Salt, 
+		strlen(pBase64Salt), 
+		iteration_count, 
+		hash_size,
+		(unsigned char*)pResult
+	);
+
+	size_t expaded_hash_size = b64_get_encoded_buffer_size(hash_size);
+	if (expaded_hash_size >= MAX_DIGEST_SIZE) {
+		writelogmessage(LOG_WARNING, "Failed to base64 encode hash from file: %s\n", auth_filename);
+		return PAM_AUTH_ERR;				
+	}
+
+	b64_encode((const uint8_t*)pResult, hash_size, (uint8_t*)&pbkdf);
+	//b64 encode does not null terminate the string
+	pbkdf[expaded_hash_size] = 0;
+
+    if (strcmp(pBase64Hash, pbkdf) != 0) {
+		writelogmessage(LOG_INFO, "Supplied password did not match the stored pbkdf digest\n");
+		writelogmessage(LOG_DEBUG, "Supplied password  \"%s\" did not match the stored pbkdf digest \"%s\"\n", pbkdf, pBase64Hash);
+		return PAM_AUTH_ERR;    	
+    }
+
+#ifdef ENABLE_CHROOT
+	retval = pam_set_data(pamh, PAM_DATA_NAME, PAM_CHROOT_AUTHENTICATED, NULL);
+	if (retval != PAM_SUCCESS) {
+		writelogmessage(LOG_WARNING, "Failed to get set chroot hook\n");
+		return retval;		
+	}    
+#endif /*ENABLE_CHROOT*/
+
+	writelogmessage(LOG_DEBUG, "Return success\n");
+	return PAM_SUCCESS;
+}
