b748554ca3b12613a5c606171e4a42c2a01bd9ad
Jonas Bardino <bardino@nbi.ku.dk>
2017-09-14 08:07:39 +0000

    standardize license header for PAM and NSS modules in agreement with Kenneth
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3575 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/libnss-mig/libnss_mig.c
+++ mig/libnss-mig/libnss_mig.c
@@ -1,21 +1,51 @@
 /*
- * libnss_mig.c
+ * --- BEGIN_HEADER ---
  *
+ * libnss_mig - NSS module for MiG user authentication
+ * Copyright (C) 2003-2017  The MiG Project lead by Brian Vinter
+ *
+ * This file is part of MiG
+ *
+ * MiG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * MiG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * -- END_HEADER ---
+ *
+ * This module is based on libnss-ato which was written by
+ * Pietro Donatini <pietro.donatini@unibo.it>.
+ *
+ * Original copyright notice follows:
+ * This product may be distributed under the terms of
+ * the GNU Lesser Public License.
+ *
+ * version 0.1 
+ * 
+ * ---
+ *
+ * This MiG version was written by Kenneth Skovhede <skovhede@nbi.ku.dk>
+ * and extended for sharelinks by Jonas Bardino <bardino@nb.ku.dk>.
+ *
+ */
+
+
+/*
  * NSS module for supporting native login of MiG 
  * virtual users. The module checks if a user exists
  * in the MiG system, and maps such a user to the
  * mig-user UID and GID, but with a custom home folder.
  *
- * Written by Kenneth Skovhede <skovhede@nbi.ku.dk>
- *
- * This module is based on libnss-ato which was written by
- * Pietro Donatini <pietro.donatini@unibo.it>.
- *
- * this product may be distributed under the terms of
- * the GNU Lesser Public License.
- *
- * version 0.1 
- * 
  */
 
 #include <nss.h>
--- mig/pam-mig/libpam_mig.c
+++ mig/pam-mig/libpam_mig.c
@@ -1,3 +1,36 @@
+/*
+ * libpam_mig - PAM module for MiG user authentication
+ * Copyright (C) 2003-2017  The MiG Project lead by Brian Vinter
+ *
+ * This file is part of MiG
+ *
+ * MiG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * MiG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+/*
+ * PAM module for supporting native login of MiG 
+ * virtual users. The module checks if a user exists
+ * in the MiG system, and maps such a user to the
+ * mig-user UID and GID, but with a custom home folder.
+ *
+ * Written by Kenneth Skovhede <skovhede@nbi.ku.dk>
+ * Extended for sharelinks by Jonas Bardino <bardino@nb.ku.dk>
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
