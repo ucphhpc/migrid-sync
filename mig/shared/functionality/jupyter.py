8affa80c70c87c41f92b024f2de698e60f7b1724
Rasmus Munk <rasmus.munk@nbi.ku.dk>
2018-07-16 12:19:03 +0000

    Updated jupyter.py with new expected API header as specified by https://github.com/rasmunk/SwarmSpawner/blob/967b2f418fded7e4f7864967d39f80a9de8f3536/mig/swarmspawner.py#L648, also fixed the early is_active check and forward to pass mount content instead of the file object
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3842 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/functionality/jupyter.py
+++ mig/shared/functionality/jupyter.py
@@ -30,7 +30,7 @@ This backend makes two requests to the target url specified by the
 configuration.jupyter_url variable the first requests authenticates against the
 jupyterhub server, it passes the Remote-User header with the client_id's email.
 The second request takes the newly instantiated ssh keyset and passes it to the
-jupyterhub server via the Mig-Mount header.
+jupyterhub server via the Mount header.
 Subsequently any potentially old keysets for this user is removed and the new
 keyset is commited to the users configuration.jupyter_mount_files_dir
 directory. Finally this active keyset is linked to the directory where the sftp
@@ -45,9 +45,9 @@ keyset.
 import os
 import socket
 import sys
-import requests
 import time
 import shutil
+import requests
 import shared.returnvalues as returnvalues
 from shared.conf import get_configuration_object
 from binascii import hexlify
@@ -58,10 +58,8 @@ from shared.httpsclient import unescape
 from shared.init import initialize_main_variables
 from shared.ssh import generate_ssh_rsa_key_pair
 from shared.fileio import make_symlink, pickle, unpickle, write_file
-from shared.findtype import is_admin
 
 
-# TODO: Validate that this actually works
 def is_active(pickle_state, timeout=7200):
     """
     :param pickle_state: expects a pickle object dictionary that
@@ -71,16 +69,31 @@ def is_active(pickle_state, timeout=7200):
     default is 2 hours -> 7200 seconds
     :return: Boolean
     """
-    assert type(pickle_state) is dict, "pickle_state is a dictionary: %r" % \
-                                pickle_state
-    assert pickle_state.has_key('CREATED_TIMESTAMP')
+    assert isinstance(pickle_state, dict), "pickle_state is a dictionary: %r" % \
+                                           pickle_state
+    assert 'CREATED_TIMESTAMP' in pickle_state
 
     active = True
-    active_time = int(time.time()) \
-                  - int(pickle_state['CREATED_TIMESTAMP'])
+    active_time = int(time.time())\
+        - int(pickle_state['CREATED_TIMESTAMP'])
     if active_time > timeout:
         active = False
     return active
+
+
+def mig_to_mount_adapt(mig):
+    """
+    :param mig: expects a dictionary containing mig state keys including,
+    MOUNT_HOST, SESSIONID, MOUNTSSHPRIVATEKEY and TARGET_MOUNT_ADDR
+    :return: returns a dictionary
+    """
+    mount = {
+        'HOST': mig['MOUNT_HOST'],
+        'USERNAME': mig['SESSIONID'],
+        'PRIVATEKEY': mig['MOUNTSSHPRIVATEKEY'],
+        'PATH': mig['TARGET_MOUNT_ADDR']
+    }
+    return mount
 
 
 def remove_jupyter_mount(jupyter_mount_path, configuration):
@@ -100,7 +113,7 @@ def remove_jupyter_mount(jupyter_mount_path, configuration):
     # Remove subsys sftp files
     if configuration.site_enable_sftp_subsys:
         auth_dir = os.path.join(configuration.mig_system_files,
-                                  'jupyter_mount')
+                                'jupyter_mount')
         for auth_file in os.listdir(auth_dir):
             if auth_file.split('.authorized_keys')[0] in filename:
                 os.remove(os.path.join(auth_dir, auth_file))
@@ -113,7 +126,7 @@ def prune_jupyter_mounts(jupyter_mounts, configuration):
     """
     If multiple jupyter mounts are active, remove every except for the
     latests one
-    :param active_jupyter_mounts: list of jupyter mount pickle state paths
+    :param jupyter_mounts: list of jupyter mount pickle state paths
     :param configuration the MiG configuration object
     :return: a list containing only
     """
@@ -126,8 +139,7 @@ def prune_jupyter_mounts(jupyter_mounts, configuration):
             remove_jupyter_mount(latest, configuration)
             latest = mount_file
 
-    jupyter_mounts.clear()
-    jupyter_mounts.append(latest)
+    return [latest]
 
 
 def jupyter_host(configuration, output_objects, user):
@@ -142,7 +154,7 @@ def jupyter_host(configuration, output_objects, user):
     """
     configuration.logger.info("User: %s finished, redirecting to the jupyter host" % user)
     status = returnvalues.OK
-    home = configuration.jupyter_base_url
+    home = ''.join([configuration.jupyter_base_url, '/home'])
     headers = [('Location', home), ('Remote-User', user)]
     output_objects.append({'object_type': 'start', 'headers': headers})
     return (output_objects, status)
@@ -154,7 +166,7 @@ def reset():
     """
     configuration = get_configuration_object()
     auth_path = os.path.join(configuration.mig_system_files,
-                               'jupyter_mount')
+                             'jupyter_mount')
     mnt_path = configuration.jupyter_mount_files_dir
     link_path = configuration.sessid_to_jupyter_mount_link_home
     if os.path.exists(auth_path):
@@ -169,13 +181,11 @@ def reset():
 
 def signature():
     """Signature of the main function"""
-    defaults = {}
-    return ['', defaults]
+    return ['', {}]
 
 
 def main(client_id, user_arguments_dict):
     """Main function used by front end"""
-
     (configuration, logger, output_objects, op_name) = \
         initialize_main_variables(client_id, op_header=False)
     client_dir = client_id_dir(client_id)
@@ -192,17 +202,18 @@ def main(client_id, user_arguments_dict):
     if not validate_status:
         return (accepted, returnvalues.CLIENT_ERROR)
 
-    logger.info("User: %s executing %s" % (client_id, op_name))
-
+    logger.debug("User: %s executing %s" % (client_id, op_name))
     if not configuration.site_enable_jupyter:
-        output_objects.append({'object_type': 'error_text', 'text':
-            '''The Jupyter service is not enabled on the system'''})
+        output_objects.append(
+            {'object_type': 'error_text',
+             'text': '''The Jupyter service is not enabled on the system'''})
         return (output_objects, returnvalues.SYSTEM_ERROR)
 
     if not configuration.site_enable_sftp_subsys and not \
             configuration.site_enable_sftp:
-        output_objects.append({'object_type': 'error_text', 'text':
-            '''The required sftp service is not enabled on the system'''})
+        output_objects.append(
+            {'object_type': 'error_text',
+             'text': '''The required sftp service is not enabled on the system'''})
         return (output_objects, returnvalues.SYSTEM_ERROR)
 
     # Test target jupyter url
@@ -211,16 +222,15 @@ def main(client_id, user_arguments_dict):
         session.get(configuration.jupyter_url)
     except requests.ConnectionError as err:
         logger.error("Failed to establish connection to %s error %s",
-                    configuration.jupyter_url, err)
+                     configuration.jupyter_url, err)
         output_objects.append(
             {'object_type': 'error_text',
              'text': '''Failed to establish connection to the Jupyter service'''})
         return (output_objects, returnvalues.CLIENT_ERROR)
 
-
     username = unescape(os.environ.get('REMOTE_USER', '')).strip()
     # TODO, activate admin info
-    #remote_user = {'USER': username, 'IS_ADMIN': is_admin(client_id,
+    # remote_user = {'USER': username, 'IS_ADMIN': is_admin(client_id,
     #                                                      configuration,
     # logger)}
 
@@ -258,7 +268,7 @@ def main(client_id, user_arguments_dict):
 
     # Check for correct permissions, 0755
     no_g_write_dirs = [os.path.dirname(os.path.dirname(configuration.user_home)),
-                     configuration.user_home, user_home_dir]
+                       configuration.user_home, user_home_dir]
     for u_dir in no_g_write_dirs:
         # check group flag
         if os.stat(u_dir).st_mode & 022:
@@ -266,10 +276,12 @@ def main(client_id, user_arguments_dict):
             logger.info("User: %s directory %s"
                         " had invalid permissions %s,"
                         " resetting to 755" % (
-                         client_id, u_dir, old_perm))
+                            client_id, u_dir, old_perm))
             os.chmod(u_dir, 0755)
 
-    url_mount = configuration.jupyter_url + '/mount'
+    url_base = configuration.jupyter_url
+    url_auth = ''.join([url_base, configuration.jupyter_base_url, '/hub/home'])
+    url_mount = ''.join([url_base, configuration.jupyter_base_url, '/hub/mount'])
 
     # Does the client home dir contain an active mount key
     # If so just keep on using it.
@@ -277,23 +289,23 @@ def main(client_id, user_arguments_dict):
                            os.listdir(mnt_path)
                            if jfile.endswith('.jupyter_mount')]
 
-    logger.debug("User: %s mount files: %s", client_id,
+    logger.info("User: %s mount files: %s", client_id,
                 "\n".join(jupyter_mount_files))
     logger.debug("Remote-User %s", remote_user)
     active_mounts = []
     for jfile in jupyter_mount_files:
         jupyter_dict = unpickle(jfile, logger)
         # Remove not active keys
-        if not is_active(jupyter_dict, timeout=10):
+        if not is_active(jupyter_dict):
             remove_jupyter_mount(jfile, configuration)
         else:
             active_mounts.append(jfile)
 
     logger.debug("User: %s active keys: %s", client_id,
-                "\n".join(active_mounts))
+                 "\n".join(active_mounts))
     # If multiple are active, remove oldest
     if len(active_mounts) > 1:
-        prune_jupyter_mounts(active_mounts, configuration)
+        active_mounts = prune_jupyter_mounts(active_mounts, configuration)
         if len(active_mounts) != 1:
             logger.error("After pruning jupyter keys %s are still active",
                          len(active_mounts))
@@ -302,12 +314,17 @@ def main(client_id, user_arguments_dict):
     # service, pass most recent mount information
     if len(active_mounts) == 1:
         # Check whether the user should authenticate and pass mount information
-        auth_mount_header = {'Remote-User': remote_user, 'Mig-Mount': str(
-            active_mounts[0])}
+        jupyter_dict = unpickle(active_mounts[0], logger)
+        mount_dict = mig_to_mount_adapt(jupyter_dict)
+        logger.debug("Existing keys %s", mount_dict)
+        auth_mount_header = {'Remote-User': remote_user, 'Mount': str(
+            mount_dict)}
 
         session = requests.session()
+        # Authenticate
+        session.get(url_auth, headers=auth_mount_header)
         # Provide the active homedrive mount information
-        session.get(url_mount, headers=auth_mount_header)
+        session.post(url_mount, headers=auth_mount_header)
 
         # Redirect client to jupyterhub
         return jupyter_host(configuration, output_objects, remote_user)
@@ -315,11 +332,6 @@ def main(client_id, user_arguments_dict):
     # Create a new keyset
     # Create login session id
     sessionid = hexlify(open('/dev/urandom').read(session_id_bytes))
-
-    # Create ssh rsa keya and known_hosts
-    mount_private_key = ""
-    mount_public_key = ""
-    mount_known_hosts = ""
 
     # Generate private/public keys
     (mount_private_key, mount_public_key) = generate_ssh_rsa_key_pair()
@@ -341,43 +353,36 @@ def main(client_id, user_arguments_dict):
                    os.path.join(subsys_path, sessionid
                                 + '.authorized_keys'), logger, umask=027)
 
-    mount_known_hosts = "%s,[%s]:%s" % (sftp_addresses[0],
-                                        sftp_addresses[0],
-                                        configuration.user_sftp_show_port)
-
     logger.debug("User: %s - Creating a new jupyter mount keyset - "
                  "private_key: %s public_key: %s ", client_id,
-                mount_private_key, mount_public_key)
+                 mount_private_key, mount_public_key)
 
-    jupyter_dict = {}
-    jupyter_dict['MOUNT_HOST'] = configuration.short_title
-    jupyter_dict['SESSIONID'] = sessionid
-    jupyter_dict['USER_CERT'] = client_id
-    # don't need fraction precision, also not all systems provide fraction
-    # precision.
-    jupyter_dict['CREATED_TIMESTAMP'] = int(time.time())
-    jupyter_dict['MOUNTSSHPRIVATEKEY'] = mount_private_key
-    jupyter_dict['MOUNTSSHPUBLICKEY'] = mount_public_key
-    # Used by the jupyterhub to know which host to mount against
-    jupyter_dict['TARGET_MOUNT_ADDR'] = "@" + sftp_addresses[0] + ":"
+    jupyter_dict = {
+        'MOUNT_HOST': configuration.short_title,
+        'SESSIONID': sessionid,
+        'USER_CERT': client_id,
+        # don't need fraction precision, also not all systems provide fraction
+        # precision.
+        'CREATED_TIMESTAMP': int(time.time()),
+        'MOUNTSSHPRIVATEKEY': mount_private_key,
+        'MOUNTSSHPUBLICKEY': mount_public_key,
+        # Used by the jupyterhub to know which host to mount against
+        'TARGET_MOUNT_ADDR': "@" + sftp_addresses[0] + ":"
+    }
 
-    # Only post the required keys
-    transfer_dict = {key: jupyter_dict[key] for key in ('MOUNT_HOST',
-                                                       'SESSIONID',
-                                                       'MOUNTSSHPRIVATEKEY',
-                                                       'TARGET_MOUNT_ADDR')}
-
-    logger.debug("User: %s Mig-Mount header: %s", client_id, jupyter_dict)
+    # Only post the required keys, adapt to API expectations
+    mount_dict = mig_to_mount_adapt(jupyter_dict)
+    logger.debug("User: %s Mount header: %s", client_id, mount_dict)
 
     # Auth and pass a new set of valid mount keys
-    url_mount = configuration.jupyter_url + '/mount'
     auth_mount_header = {'Remote-User': remote_user,
-                         'Mig-Mount': str(transfer_dict)}
+                         'Mount': str(mount_dict)}
 
     # First login
     session = requests.session()
+    session.get(url_auth, headers=auth_mount_header)
     # Provide homedrive mount information
-    session.get(url_mount, headers=auth_mount_header)
+    session.post(url_mount, headers=auth_mount_header)
 
     # Update pickle with the new valid key
     jupyter_mount_state_path = os.path.join(mnt_path,
@@ -387,7 +392,7 @@ def main(client_id, user_arguments_dict):
 
     # Link jupyter pickle state file
     linkdest_new_jupyter_mount = os.path.join(mnt_path,
-                                                  sessionid + '.jupyter_mount')
+                                              sessionid + '.jupyter_mount')
 
     linkloc_new_jupyter_mount = os.path.join(link_home,
                                              sessionid + '.jupyter_mount')
@@ -416,7 +421,8 @@ if __name__ == "__main__":
     if sys.argv[2:]:
         query_string = sys.argv[2]
     os.environ['QUERY_STRING'] = query_string
-    client_id = "/C=DK/ST=NA/L=NA/O=NBI/OU=NA/CN=Rasmus Munk/emailAddress=rasmus.munk@nbi.ku.dk"
+    client_id = "/C=DK/ST=NA/L=NA/O=NBI/OU=NA/CN=" \
+                "Rasmus Munk/emailAddress=rasmus.munk@nbi.ku.dk"
     if sys.argv[3:]:
         client_id = sys.argv[3]
     os.environ['SSL_CLIENT_S_DN'] = client_id
