ucphhpc/migrid-sync

commit ce41e5c8f8db3667ee9a86c6c1206e2ee209584f

add freeze_admin list option to ease (admin-only) testing of archives with possibility of deleting otherwise permanent archives. Robustness fixes for legacy archives with missing PUBLISH field.
git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3822 b75ad72c-e7d7-11dd-a971-7dbc132099af

mig/shared/freezefunctions.py
@@ -720,7 +720,8 @@ def create_frozen_archive(freeze_meta, freeze_copy, freeze_move,
     freeze_dict = init_res
     freeze_id = freeze_dict['ID']
     if existing_archive:
-        published = freeze_dict['PUBLISH']
+        # Legacy archives may not have all fields set
+        published = freeze_dict.get('PUBLISH', False)
         state = freeze_dict.get('STATE', keyword_final)
         freeze_dict.update(freeze_meta)
     else:
@@ -762,7 +763,7 @@ def create_frozen_archive(freeze_meta, freeze_copy, freeze_move,
                       (freeze_id, client_id, freeze_entries))
         return (False, "Error: final archives must have one or more files")
 
-    if freeze_dict['PUBLISH']:
+    if published:
         (web_status, web_res) = write_landing_page(freeze_dict, arch_dir,
                                                    frozen_files, configuration)
     elif published:
@@ -844,7 +845,7 @@ def delete_archive_files(freeze_dict, client_id, path_list, configuration):
             _logger.warning("failed to save pruned cache in %s: %s" %
                             (cache_path, err))
 
-    if freeze_dict['PUBLISH']:
+    if freeze_dict.get('PUBLISH', False):
         frozen_files = [i['name'] for i in freeze_dict.get('FILES', [])
                         if i['name'] != public_archive_index]
         (web_status, web_res) = write_landing_page(freeze_dict, arch_dir,
@@ -869,7 +870,7 @@ def delete_frozen_archive(freeze_dict, client_id, configuration):
     _logger = configuration.logger
     freeze_id = freeze_dict['ID']
     arch_dir = get_frozen_root(client_id, freeze_id, configuration)
-    if freeze_dict['PUBLISH']:
+    if freeze_dict.get('PUBLISH', False):
         (web_status, web_res) = remove_landing_page(freeze_dict, arch_dir,
                                                     configuration,
                                                     allow_missing=True)

mig/shared/configuration.py
@@ -1414,6 +1414,12 @@ location.""" % self.config_file
             self.site_permanent_freeze = permanent_freeze
         else:
             self.site_permanent_freeze = freeze_flavors.keys()
+        if config.has_option('SITE', 'freeze_admins'):
+            admins = config.get('SITE', 'freeze_admins')
+            self.site_freeze_admins = [admin.strip()
+                                       for admin in admins.split(',')]
+        else:
+            self.site_freeze_admins = []
         if config.has_option('SITE', 'freeze_to_tape'):
             self.site_freeze_to_tape = config.get('SITE', 'freeze_to_tape')
         else:

mig/shared/functionality/deletefreeze.py
@@ -36,13 +36,14 @@ import shared.returnvalues as returnvalues
 from shared.base import client_id_dir
 from shared.defaults import freeze_flavors, keyword_final, keyword_all
 from shared.freezefunctions import is_frozen_archive, get_frozen_archive, \
-     delete_frozen_archive, delete_archive_files, TARGET_ARCHIVE, TARGET_PATH
+    delete_frozen_archive, delete_archive_files, TARGET_ARCHIVE, TARGET_PATH
 from shared.functional import validate_input_and_cert, REJECT_UNSET
 from shared.handlers import safe_handler, get_csrf_limit
 from shared.init import initialize_main_variables, find_entry
 from shared.validstring import valid_user_path
 
 valid_targets = [TARGET_ARCHIVE, TARGET_PATH]
+
 
 def signature():
     """Signature of the main function"""
@@ -70,7 +71,7 @@ def main(client_id, user_arguments_dict):
         client_id,
         configuration,
         allow_rejects=False,
-        )
+    )
     if not validate_status:
         return (accepted, returnvalues.CLIENT_ERROR)
 
@@ -86,7 +87,7 @@ CSRF-filtered POST requests to prevent unintended updates'''
 
     if not flavor in freeze_flavors.keys():
         output_objects.append({'object_type': 'error_text', 'text':
-                           'Invalid freeze flavor: %s' % flavor})
+                               'Invalid freeze flavor: %s' % flavor})
         return (output_objects, returnvalues.CLIENT_ERROR)
 
     title = freeze_flavors[flavor]['deletefreeze_title']
@@ -119,12 +120,12 @@ Please contact the site admins %s if you think it should be enabled.
                                'text': "No such frozen archive: '%s'"
                                % freeze_id})
         return (output_objects, returnvalues.CLIENT_ERROR)
-    
+
     (load_status, freeze_dict) = get_frozen_archive(client_id, freeze_id,
                                                     configuration,
                                                     checksum_list=[])
     if not load_status:
-        logger.error("%s: load failed for '%s': %s" % \
+        logger.error("%s: load failed for '%s': %s" %
                      (op_name, freeze_id, freeze_dict))
         output_objects.append(
             {'object_type': 'error_text',
@@ -134,24 +135,26 @@ Please contact the site admins %s if you think it should be enabled.
 
     # Make sure the frozen archive belongs to the user trying to delete it
     if client_id != freeze_dict['CREATOR']:
-        logger.error("%s: illegal access attempt for '%s': %s" % \
-                         (op_name, freeze_id, client_id))
-        output_objects.append({'object_type': 'error_text', 'text': \
-        'You are not the owner of frozen archive "%s"' % freeze_id})
+        logger.error("%s: illegal access attempt for '%s': %s" %
+                     (op_name, freeze_id, client_id))
+        output_objects.append({'object_type': 'error_text', 'text':
+                               'You are not the owner of frozen archive "%s"'
+                               % freeze_id})
         return (output_objects, returnvalues.CLIENT_ERROR)
 
-    if freeze_dict.get('FLAVOR','freeze') != flavor:
-        logger.error("%s: flavor mismatch for '%s': %s vs %s" % \
+    if freeze_dict.get('FLAVOR', 'freeze') != flavor:
+        logger.error("%s: flavor mismatch for '%s': %s vs %s" %
                      (op_name, freeze_id, flavor, freeze_dict))
-        output_objects.append({'object_type': 'error_text', 'text'
-                               : 'No such %s archive "%s"' % (flavor,
-                                                              freeze_id)})
+        output_objects.append({'object_type': 'error_text', 'text':
+                               'No such %s archive "%s"' % (flavor, freeze_id)})
         return (output_objects, returnvalues.CLIENT_ERROR)
 
     # Prevent user-delete of the frozen archive if configuration forbids it.
     # We exclude any archives in the pending intermediate freeze state.
+    # Freeze admins are also excluded from the restrictions.
     if freeze_dict.get('STATE', keyword_final) == keyword_final and \
-           flavor in configuration.site_permanent_freeze:
+            flavor in configuration.site_permanent_freeze and \
+            not client_id in configuration.site_freeze_admins:
         output_objects.append(
             {'object_type': 'error_text', 'text':
              "Can't change %s archives like '%s' yourself due to site policy"
@@ -170,7 +173,7 @@ Please contact the site admins %s if you think it should be enabled.
         # Delete the entire freeze archive
         (del_status, msg) = delete_frozen_archive(freeze_dict, client_id,
                                                   configuration)
-    
+
         # If something goes wrong when trying to delete freeze archive
         # freeze_id, an error is displayed.
         if not del_status:
@@ -178,7 +181,7 @@ Please contact the site admins %s if you think it should be enabled.
                                                       freeze_id, msg))
             output_objects.append(
                 {'object_type': 'error_text', 'text':
-                 'Could not remove entire %s archive %s: %s' % \
+                 'Could not remove entire %s archive %s: %s' %
                  (flavor, freeze_id, msg)})
             return (output_objects, returnvalues.SYSTEM_ERROR)
 
@@ -206,39 +209,39 @@ Please contact the site admins %s if you think it should be enabled.
                                % (client_id, op_name, abs_path, path))
                 output_objects.append(
                     {'object_type': 'error_text', 'text':
-                     'Not allowed to delete %s - outside archive %s !' % \
+                     'Not allowed to delete %s - outside archive %s !' %
                      (path, freeze_id)})
                 continue
             del_paths.append(path)
-            
+
         (del_status, msg_list) = delete_archive_files(freeze_dict, client_id,
                                                       del_paths, configuration)
-    
+
         # If something goes wrong when trying to delete files from archive
         # freeze_id, an error is displayed.
         if not del_status:
-            logger.error("%s: delete archive file(s) failed for '%s':\n%s" % \
+            logger.error("%s: delete archive file(s) failed for '%s':\n%s" %
                          (op_name, freeze_id, '\n'.join(msg_list)))
             output_objects.append(
                 {'object_type': 'error_text', 'text':
                  'Could not remove file(s) from archive %s: %s'
-                                   % (freeze_id, '\n '.join(msg_list))})
+                 % (freeze_id, '\n '.join(msg_list))})
             return (output_objects, returnvalues.SYSTEM_ERROR)
 
         # If deletion of files from archive freeze_id is successful, we just
         # return OK
         else:
-            logger.info("%s: delete %d files successful for '%s': %s" % \
+            logger.info("%s: delete %d files successful for '%s': %s" %
                         (op_name, len(path_list), freeze_id, client_id))
             output_objects.append(
                 {'object_type': 'text', 'text':
-                 'Successfully deleted %d file(s) from archive: "%s"' % \
+                 'Successfully deleted %d file(s) from archive: "%s"' %
                  (len(path_list), freeze_id)})
 
     # Success - show link to overview
     output_objects.append({'object_type': 'link', 'destination':
-                                   'freezedb.py',
-                                   'class': 'infolink iconspace',
-                                   'title': 'Show archives',
-                                   'text': 'Show archives'})
-    return (output_objects, returnvalues.OK) 
+                           'freezedb.py',
+                           'class': 'infolink iconspace',
+                           'title': 'Show archives',
+                           'text': 'Show archives'})
+    return (output_objects, returnvalues.OK)

mig/shared/functionality/freezedb.py
@@ -30,22 +30,24 @@
 import shared.returnvalues as returnvalues
 from shared.defaults import default_pager_entries, csrf_field, keyword_final
 from shared.freezefunctions import build_freezeitem_object, \
-     list_frozen_archives, get_frozen_meta, get_frozen_archive, TARGET_ARCHIVE
+    list_frozen_archives, get_frozen_meta, get_frozen_archive, TARGET_ARCHIVE
 from shared.functional import validate_input_and_cert
 from shared.handlers import get_csrf_limit, make_csrf_token
 from shared.html import jquery_ui_js, man_base_js, man_base_html, \
-     html_post_helper, themed_styles
+    html_post_helper, themed_styles
 from shared.init import initialize_main_variables, find_entry
 
 list_operations = ['showlist', 'list']
 show_operations = ['show', 'showlist']
 allowed_operations = list(set(list_operations + show_operations))
 
+
 def signature():
     """Signature of the main function"""
 
     defaults = {'operation': ['show']}
     return ['frozenarchives', defaults]
+
 
 def main(client_id, user_arguments_dict):
     """Main function used by front end"""
@@ -62,12 +64,12 @@ def main(client_id, user_arguments_dict):
         client_id,
         configuration,
         allow_rejects=False,
-        )
+    )
     if not validate_status:
         return (accepted, returnvalues.CLIENT_ERROR)
 
     operation = accepted['operation'][-1]
-    
+
     if not configuration.site_enable_freeze:
         output_objects.append({'object_type': 'text', 'text':
                                '''Freezing archives is disabled on this site.
@@ -77,7 +79,7 @@ Please contact the site admins %s if you think it should be enabled.
 
     if not operation in allowed_operations:
         output_objects.append({'object_type': 'text', 'text':
-                               '''Operation must be one of %s.''' % \
+                               '''Operation must be one of %s.''' %
                                ', '.join(allowed_operations)})
         return (output_objects, returnvalues.OK)
 
@@ -87,9 +89,13 @@ Please contact the site admins %s if you think it should be enabled.
         # jquery support for tablesorter and confirmation on delete
         # table initially sorted by col. 5 (State), 3 (Created date), 2 (name)
 
-        refresh_call = 'ajax_freezedb(%s, "%s")' % \
-                       (str(configuration.site_permanent_freeze),
-                        keyword_final)
+        if client_id in configuration.site_freeze_admins:
+            permanent_flavors = []
+        else:
+            permanent_flavors = configuration.site_permanent_freeze
+        # NOTE: must insert permanent_flavors list as string here
+        refresh_call = 'ajax_freezedb(%s, "%s")' % (str(permanent_flavors),
+                                                    keyword_final)
         table_spec = {'table_id': 'frozenarchivetable', 'sort_order':
                       '[[5,1],[3,1],[2,0]]', 'refresh_call': refresh_call}
         (add_import, add_init, add_ready) = man_base_js(configuration,
@@ -102,19 +108,19 @@ Please contact the site admins %s if you think it should be enabled.
         output_objects.append({'object_type': 'html_form',
                                'text': man_base_html(configuration)})
 
-        output_objects.append({'object_type': 'header', 'text'
-                               : 'Frozen Archives'})
+        output_objects.append(
+            {'object_type': 'header', 'text': 'Frozen Archives'})
 
         output_objects.append(
-            {'object_type': 'text', 'text' :
+            {'object_type': 'text', 'text':
              '''Frozen archives are write-once collections of files used e.g.
 in relation to conference paper submissions. Please note that local policies
 may prevent users from deleting frozen archives without explicit acceptance
 from the management.
         '''})
 
-        output_objects.append({'object_type': 'sectionheader', 'text'
-                          : 'Existing frozen archives'})
+        output_objects.append(
+            {'object_type': 'sectionheader', 'text': 'Existing frozen archives'})
 
         # Helper form for removes
 
@@ -140,24 +146,22 @@ from the management.
         if not list_status:
             logger.error("%s: failed for '%s': %s" % (op_name,
                                                       client_id, ret))
-            output_objects.append({'object_type': 'error_text', 'text'
-                                   : ret})
+            output_objects.append({'object_type': 'error_text', 'text': ret})
             return (output_objects, returnvalues.SYSTEM_ERROR)
 
         logger.debug("%s %s: building list of archives" % (op_name, operation))
         for freeze_id in ret:
             # TODO: add file count to meta and switch here
-            #(load_status, freeze_dict) = get_frozen_meta(client_id, freeze_id,
+            # (load_status, freeze_dict) = get_frozen_meta(client_id, freeze_id,
             #                                             configuration)
             (load_status, freeze_dict) = get_frozen_archive(client_id,
                                                             freeze_id,
                                                             configuration,
                                                             checksum_list=[])
             if not load_status:
-                logger.error("%s: load failed for '%s': %s" % \
+                logger.error("%s: load failed for '%s': %s" %
                              (op_name, freeze_id, freeze_dict))
-                output_objects.append({'object_type': 'error_text', 'text'
-                                       : 'Could not read details for "%s"' % \
+                output_objects.append({'object_type': 'error_text', 'text': 'Could not read details for "%s"' %
                                        freeze_id})
                 return (output_objects, returnvalues.SYSTEM_ERROR)
             freeze_item = build_freezeitem_object(configuration, freeze_dict,
@@ -166,39 +170,41 @@ from the management.
             flavor = freeze_item.get('flavor', 'freeze')
 
             if client_id != freeze_item['creator']:
-                logger.warning("skip archive %s with wrong owner: %s vs %s" % \
+                logger.warning("skip archive %s with wrong owner: %s vs %s" %
                                (freeze_id, client_id, freeze_item['creator']))
                 continue
 
             # Users may view all their archives
             freeze_item['viewfreezelink'] = {
                 'object_type': 'link',
-                'destination': "showfreeze.py?freeze_id=%s;flavor=%s" % \
+                'destination': "showfreeze.py?freeze_id=%s;flavor=%s" %
                 (freeze_id, flavor),
-                'class': 'infolink iconspace', 
-                'title': 'View frozen archive %s' % freeze_id, 
+                'class': 'infolink iconspace',
+                'title': 'View frozen archive %s' % freeze_id,
                 'text': ''}
             # Users may edit pending archives
             if freeze_item['state'] != keyword_final:
                 freeze_item['editfreezelink'] = {
                     'object_type': 'link',
                     'destination': "adminfreeze.py?freeze_id=%s" % freeze_id,
-                    'class': 'adminlink iconspace', 
-                    'title': 'Edit archive %s' % freeze_id, 
+                    'class': 'adminlink iconspace',
+                    'title': 'Edit archive %s' % freeze_id,
                     'text': ''}
-            # Users may delete pending or non permanent archives
+            # Users may delete pending or non permanent archives.
+            # Freeze admins may delete all their own archives.
             if freeze_item['state'] != keyword_final or \
-                   flavor not in configuration.site_permanent_freeze:
+                    flavor not in configuration.site_permanent_freeze or \
+                    client_id in configuration.site_freeze_admins:
                 freeze_item['delfreezelink'] = {
                     'object_type': 'link', 'destination':
-                    "javascript: confirmDialog(%s, '%s', %s, %s);" % \
+                    "javascript: confirmDialog(%s, '%s', %s, %s);" %
                     ('delfreeze', 'Really remove %s?' % freeze_id, 'undefined',
                      "{freeze_id: '%s', flavor: '%s'}" % (freeze_id, flavor)),
-                    'class': 'removelink iconspace', 'title': 'Remove %s' % \
+                    'class': 'removelink iconspace', 'title': 'Remove %s' %
                     freeze_id, 'text': ''}
-    
+
             frozenarchives.append(freeze_item)
-        logger.debug("%s %s: inserting list of %d archives" % \
+        logger.debug("%s %s: inserting list of %d archives" %
                      (op_name, operation, len(frozenarchives)))
 
     output_objects.append({'object_type': 'frozenarchives',
@@ -222,14 +228,14 @@ Choose one of the archive methods below to make a manual archive:
                                'destination': 'adminfreeze.py?flavor=freeze',
                                'class': 'addlink iconspace',
                                'title': 'Make a new freeze archive of e.g. '
-                               'research data to be published', 
+                               'research data to be published',
                                'text': 'Create a new freeze archive'})
         output_objects.append({'object_type': 'html_form', 'text': '</p><p>'})
         output_objects.append({'object_type': 'link',
                                'destination': 'adminfreeze.py?flavor=backup',
                                'class': 'addlink iconspace',
-                               'title': 'Make a new backup archive of %s data' \
-                               % configuration.short_title, 
+                               'title': 'Make a new backup archive of %s data'
+                               % configuration.short_title,
                                'text': 'Create a new backup archive'})
         output_objects.append({'object_type': 'html_form', 'text': "</p>"})
 
@@ -240,12 +246,12 @@ with optional encryption of all your backup contents.'''})
             output_objects.append({'object_type': 'html_form', 'text': """
 For further details please refer to the """})
             output_objects.append({'object_type': 'link',
-                               'destination': 'settings.py?topic=duplicate',
-                               'class': 'duplicatilink iconspace',
-                               'title': 'Open Duplicati settings', 
-                               'text': 'Duplicati Settings'})
+                                   'destination': 'settings.py?topic=duplicate',
+                                   'class': 'duplicatilink iconspace',
+                                   'title': 'Open Duplicati settings',
+                                   'text': 'Duplicati Settings'})
             output_objects.append({'object_type': 'html_form', 'text':
-                                   """ and the %s documentation.""" % \
+                                   """ and the %s documentation.""" %
                                    configuration.short_title})
 
         if configuration.site_enable_seafile:
@@ -255,12 +261,12 @@ for which you want automatic file versioning and easy roll-back support.'''})
             output_objects.append({'object_type': 'html_form', 'text': """
 For further details please refer to the """})
             output_objects.append({'object_type': 'link',
-                               'destination': 'settings.py?topic=seafile',
-                               'class': 'seafilelink iconspace',
-                               'title': 'Open Seafile settings', 
-                               'text': 'Seafile Settings'})
+                                   'destination': 'settings.py?topic=seafile',
+                                   'class': 'seafilelink iconspace',
+                                   'title': 'Open Seafile settings',
+                                   'text': 'Seafile Settings'})
             output_objects.append({'object_type': 'html_form', 'text':
-                                   """ and the %s documentation.""" % \
+                                   """ and the %s documentation.""" %
                                    configuration.short_title})
     logger.info("%s %s end for %s" % (op_name, operation, client_id))
     return (output_objects, returnvalues.OK)

commit 1099e4de58bf02d1b63d8f000462c6fb7f3bc248

revert regression introduced in r3822, which caused missing publish on initial archive create. Tweak make_symlink in force-mode to only delete if a file is actually there.
git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@3989 b75ad72c-e7d7-11dd-a971-7dbc132099af

mig/shared/fileio.py
@@ -193,7 +193,7 @@ def delete_file(path, logger, allow_broken_symlink=False, allow_missing=False):
 def make_symlink(dest, src, logger, force=False):
     """Wrapper to make src a symlink to dest path"""
 
-    if force and delete_symlink(src, logger):
+    if os.path.exists(src) and force and delete_symlink(src, logger):
         logger.debug('deleted existing symlink: %s' % (src))
 
     try:

mig/shared/freezefunctions.py
@@ -740,6 +740,7 @@ def create_frozen_archive(freeze_meta, freeze_copy, freeze_move,
     if not init_status:
         _logger.error(init_res)
         return (init_status, init_res)
+
     # We received the updated freeze_meta dict from init
     freeze_dict = init_res
     freeze_id = freeze_dict['ID']
@@ -787,7 +788,7 @@ def create_frozen_archive(freeze_meta, freeze_copy, freeze_move,
                       (freeze_id, client_id, freeze_entries))
         return (False, "Error: final archives must have one or more files")
 
-    if published:
+    if freeze_dict.get('PUBLISH', False):
         (web_status, web_res) = write_landing_page(freeze_dict, arch_dir,
                                                    frozen_files, configuration)
     elif published: