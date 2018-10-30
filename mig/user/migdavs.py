2334820b478bd550305f39fc6850df8eb0a22f9d
Martin Garrigues Rehr <rehr@nbi.ku.dk>
2018-10-30 10:08:12 +0000

    Added tests for remote copy and move. Adjusted tests for GDP users and added GDP user detection.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@4004 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/user/migdavs.py
+++ mig/user/migdavs.py
@@ -54,6 +54,7 @@ import getpass
 import netrc
 import os
 import sys
+import httplib2
 
 import python_webdav
 import python_webdav.client
@@ -97,11 +98,48 @@ class MiGDAVClient(python_webdav.client.Client):
         # TODO: convert to stat tuple/object
         return property_lists
 
+    def exists(self, path):
+        """Wrap underlying connection.client.send_get to provide
+        something like os.path.exists"""
+        result = False
+        try:
+            resource_path = "%s/%s" % (
+                self.connection.path.rstrip('/'), path.lstrip('/'))
+            resp, content = self.connection.send_get(resource_path)
+            if int(resp.get('status', 0)) == 200:
+                result = True
+        except httplib2.ServerNotFoundError:
+            raise
+        except Exception, exc:
+            result = False
+
+        return result
+
     def rm(self, path):
         """Wrap underlying connection.client.delete_resource to provide
-        something like os.remove .
+        something like os.remove
         """
         return self.client.delete_resource(self.connection, path)
+
+    def cp(self, src, dest):
+        """Wrap underlying connection.client.delete_resource to provide
+        something like shutil.copyfile(src, dst)
+        """
+        return self.client.copy_resource(self.connection, src, dest)
+
+    def mv(self, src, dest):
+        """Wrap underlying connection to provide
+        something shutil.move((src, dst)
+        """
+        try:
+            full_destination = httplib2.urlparse.urljoin(
+                self.connection.host, dest)
+            headers = {'Destination': full_destination}
+            resp, content = self.connection._send_request(
+                'MOVE', src, headers=headers)
+            return resp, content
+        except httplib2.ServerNotFoundError:
+            raise
 
     def get(self, src_path, dst_path):
         """Wrap send_get to emulate sftp get. The existing download_file method
@@ -191,64 +229,98 @@ Settings page""" % server_fqdn
     print "auth as user %s" % user_name
     client.set_connection(user_name, password)
 
+    gdp_user = False
+    if user_name.count('@') == 2:
+        gdp_user = True
+        print "GDP user detected"
+
     # Sample actions on your MiG home directory ###
 
     # List and stat files in the remote .davs dir which should always be there
+    # for NON GDP users
 
-    base = '.davs'
-    print "listing files in %s" % base
-    # ls returns list of list with hrefs - convert to relative paths
-    # list includes directory itself so filter on type
-    nested_hrefs = client.ls(base, list_format=('T', 'F'))
-    hrefs = [i[1] for i in nested_hrefs if i[0] != 'collection']
-    files = []
-    for uri in hrefs:
-        # NOTE: uri may be full URL or slash-prefixed server path here - strip
-        rel_path = uri.replace(server_url, '').lstrip('/')
-        rel_path = rel_path.replace(base, '').lstrip('/')
-        files.append(rel_path)
-    print "got files: %s" % files
-    path_stat = client.stat(base)
-    print "stat %s:\n%s" % (base, path_stat)
-    print "files in %s dir:\n%s" % (base, files)
-    for name in files:
-        rel_path = os.path.join(base, name)
-        print "stat on %s" % rel_path
-        path_stat = client.stat(rel_path)
-        print "stat %s:\n%s" % (rel_path, path_stat)
-    welcome = 'welcome.txt'
-    print "download %s from home" % welcome
-    client.get(welcome, welcome)
-    path_stat = os.stat(welcome)
-    print "local stat %s:\n%s" % (welcome, path_stat)
+    if not gdp_user:
+        base = '.davs'
+        if not client.exists(base):
+            print "ERROR: remote dir '%s' does _NOT_ exist" % base
+        else:
+            print "listing files in %s" % base
+            # ls returns list of list with hrefs - convert to relative paths
+            # list includes directory itself so filter on type
+            nested_hrefs = client.ls(base, list_format=('T', 'F'))
+            hrefs = [i[1] for i in nested_hrefs if i[0] != 'collection']
+            files = []
+            for uri in hrefs:
+                # NOTE: uri may be full URL or slash-prefixed server path here - strip
+                rel_path = uri.replace(server_url, '').lstrip('/')
+                rel_path = rel_path.replace(base, '').lstrip('/')
+                files.append(rel_path)
+            print "got files: %s" % files
+            path_stat = client.stat(base)
+            print "stat '%s':\n%s" % (base, path_stat)
+            print "files in '%s' dir:\n%s" % (base, files)
+            for name in files:
+                rel_path = os.path.join(base, name)
+                print "stat on '%s'" % rel_path
+                path_stat = client.stat(rel_path)
+                print "stat '%s':\n%s" % (rel_path, path_stat)
+
+    # GDP users chroot to vgrid_files_home/VGRIDNAME
+
+    if not gdp_user:
+        welcome = 'welcome.txt'
+    else:
+        welcome = 'README'
+    if not client.exists(welcome):
+        print "ERROR: remote file '%s' does _NOT_ exist" % welcome
+        exit(1)
+    else:
+        print "download '%s' from home" % welcome
+        client.get(welcome, welcome)
+        path_stat = os.stat(welcome)
+        print "local stat '%s':\n%s" % (welcome, path_stat)
     dummy = 'this-is-a-migdavs-dummy-file.txt'
     dummy_text = "sample file\ncontents from client\n"
     dummy_fd = open(dummy, "w")
     dummy_fd.write(dummy_text)
     dummy_fd.close()
-    print "create dummy in %s" % dummy
+    print "create dummy '%s' in home" % dummy
     path_stat = os.stat(dummy)
-    print "local stat %s:\n%s" % (dummy, path_stat)
-    print "upload migdavsdummy in %s home" % dummy
+    print "local stat '%s':\n%s" % (dummy, path_stat)
+    print "upload migdavsdummy '%s' in home" % dummy
     client.put(dummy, dummy)
-    print "stat on uploaded %s" % dummy
+    print "stat on uploaded '%s'" % dummy
     path_stat = client.stat(dummy)
-    print "remote stat %s:\n%s" % (dummy, path_stat)
-    print "delete dummy in %s" % dummy
+    print "remote stat '%s':\n%s" % (dummy, path_stat)
+    print "delete dummy '%s' in home" % dummy
     os.remove(dummy)
-    print "verify gone: %s" % (dummy not in os.listdir('.'))
-    print "download %s from home" % dummy
+    print "verify gone: '%s'" % (dummy not in os.listdir('.'))
+    print "download '%s' from home" % dummy
     client.get(dummy, dummy)
     path_stat = os.stat(dummy)
-    print "local stat %s:\n%s" % (dummy, path_stat)
+    print "local stat '%s':\n%s" % (dummy, path_stat)
     dummy_fd = open(dummy, "r")
     verify_text = dummy_fd.read()
     dummy_fd.close()
     print "verify correct contents: %s" % (dummy_text == verify_text)
-    print "delete dummy in %s" % dummy
+    print "delete dummy '%s' in home " % dummy
     os.remove(dummy)
-    print "delete remote dummy in %s home" % dummy
+    dummy_copy = "%s.copy" % dummy
+    print "remote copy dummy '%s' -> '%s' in home" % (dummy, dummy_copy)
+    client.cp(dummy, dummy_copy)
+    path_stat = client.stat(dummy_copy)
+    print "remote stat '%s':\n%s" % (dummy_copy, path_stat)
+    dummy_move = "%s.move" % dummy
+    print "move remote dummy '%s' -> '%s' in home" % (dummy, dummy_move)
+    client.mv(dummy, dummy_move)
+    path_stat = client.stat(dummy_move)
+    print "remote stat '%s':\n%s" % (dummy_move, path_stat)
+    print "delete remote dummy '%s' in home" % dummy
     client.rm(dummy)
+    print "delete remote dummy '%s' in home" % dummy_copy
+    client.rm(dummy_copy)
+    print "delete remote dummy '%s' in home" % dummy_move
+    client.rm(dummy_move)
 
     illegal_dir = '../'
     illegal_dummy = os.path.join(illegal_dir, 'tmp-' + dummy)
@@ -261,9 +333,9 @@ Settings page""" % server_fqdn
         if new_pwd == old_pwd:
             raise Exception('chdir did not change anything!')
         else:
-            print "changed to illegal dir %s from %s" % (new_pwd, old_pwd)
+            print "changed to illegal dir '%s' from '%s'" % (new_pwd, old_pwd)
     except Exception, exc:
-        print "correctly rejected change dir to illegal destination: %s" % exc
+        print "correctly rejected change dir to illegal destination: '%s'" % exc
     print "copy remote dummy outside home - should fail"
     try:
         resp, contents = client.copy(dummy, illegal_dummy)
@@ -274,9 +346,9 @@ Settings page""" % server_fqdn
     try:
         resp, contents = client.rm(illegal_dummy)
         if resp.status_code in range(400, 410):
-            raise Exception('delete failed with %s' % resp)
+            raise Exception("delete failed with '%s'" % resp)
         else:
-            print "copied to illegal destination %s" % illegal_dummy
+            print "copied to illegal destination '%s'" % illegal_dummy
     except Exception, exc:
         print "correctly rejected removal of illegal dummy: %s" % exc
 
