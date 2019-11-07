7967c86ee5bf8984e9ae3890fa554a61c18a6bf1
Martin Garrigues Rehr <rehr@nbi.ku.dk>
2019-11-07 14:02:00 +0000

    Formatted sslsession with 'indent --linux-style'
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@4375 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/src/sslsession/_sslsession.c
+++ mig/src/sslsession/_sslsession.c
@@ -36,16 +36,15 @@ Copied from 'Python-2.7.5/Modules/socketmodule.h'
 typedef int SOCKET_T;
 
 typedef struct {
-    PyObject_HEAD
-    SOCKET_T sock_fd;           /* Socket file descriptor */
-    int sock_family;            /* Address family, e.g., AF_INET */
-    int sock_type;              /* Socket type, e.g., SOCK_STREAM */
-    int sock_proto;             /* Protocol type, usually 0 */
-    PyObject *(*errorhandler)(void); /* Error handler; checks
-                                        errno, returns NULL and
-                                        sets a Python exception */
-    double sock_timeout;                 /* Operation timeout in seconds;
-                                        0.0 means non-blocking */
+	PyObject_HEAD SOCKET_T sock_fd;	/* Socket file descriptor */
+	int sock_family;	/* Address family, e.g., AF_INET */
+	int sock_type;		/* Socket type, e.g., SOCK_STREAM */
+	int sock_proto;		/* Protocol type, usually 0 */
+	PyObject *(*errorhandler) (void);	/* Error handler; checks
+						   errno, returns NULL and
+						   sets a Python exception */
+	double sock_timeout;	/* Operation timeout in seconds;
+				   0.0 means non-blocking */
 } PySocketSockObject;
 
 /******************************************************** 
@@ -55,14 +54,13 @@ Copied from 'Python-2.7.5/Modules/_ssl.c'
 #define X509_NAME_MAXLEN 256
 
 typedef struct {
-    PyObject_HEAD
-    PySocketSockObject *Socket;         /* Socket on which we're layered */
-    SSL_CTX*            ctx;
-    SSL*                ssl;
-    X509*               peer_cert;
-    char                server[X509_NAME_MAXLEN];
-    char                issuer[X509_NAME_MAXLEN];
-    int                 shutdown_seen_zero;
+	PyObject_HEAD PySocketSockObject * Socket;	/* Socket on which we're layered */
+	SSL_CTX *ctx;
+	SSL *ssl;
+	X509 *peer_cert;
+	char server[X509_NAME_MAXLEN];
+	char issuer[X509_NAME_MAXLEN];
+	int shutdown_seen_zero;
 
 } PySSLObject;
 
@@ -70,71 +68,72 @@ typedef struct {
 END of "include copies"
 ********************************************************/
 
-static PyObject *PySSLSESSION_session_id(PyObject *self, PyObject *args) {
-    PyObject *pysslobject = Py_None;
-    PyObject *session_id = Py_None;
-    PySSLObject *pyssl = NULL;
+static PyObject *PySSLSESSION_session_id(PyObject * self, PyObject * args)
+{
+	PyObject *pysslobject = Py_None;
+	PyObject *session_id = Py_None;
+	PySSLObject *pyssl = NULL;
 
-    if (!PyArg_ParseTuple(args, "|O:session_id", &pysslobject))
-        return NULL;
-    pyssl = (PySSLObject *) pysslobject;
+	if (!PyArg_ParseTuple(args, "|O:session_id", &pysslobject))
+		return NULL;
+	pyssl = (PySSLObject *) pysslobject;
 
-    /*
-    fprintf(stderr, "\npysslobject: %p\n", pysslobject);
-    fprintf(stderr, "\npyssl: %p\n", pyssl);
-	fprintf(stderr, "\npyssl->ssl: %p\n", pyssl->ssl);
-    fprintf(stderr, "\npyssl->ssl->session: %p\n", pyssl->ssl->session);
-    fprintf(stderr, "\npyssl->ssl->session->session_id: %s\n", 
-    					pyssl->ssl->session->session_id);
-    */
-    
-    session_id = PyString_FromStringAndSize(
-                                (const char *) pyssl->ssl->session->session_id,
-    							SSL_MAX_SSL_SESSION_ID_LENGTH);
+	/*
+	   fprintf(stderr, "\npysslobject: %p\n", pysslobject);
+	   fprintf(stderr, "\npyssl: %p\n", pyssl);
+	   fprintf(stderr, "\npyssl->ssl: %p\n", pyssl->ssl);
+	   fprintf(stderr, "\npyssl->ssl->session: %p\n", pyssl->ssl->session);
+	   fprintf(stderr, "\npyssl->ssl->session->session_id: %s\n", 
+	   pyssl->ssl->session->session_id);
+	 */
 
-    return session_id;
+	session_id = PyString_FromStringAndSize((const char *)pyssl->ssl->
+						session->session_id,
+						SSL_MAX_SSL_SESSION_ID_LENGTH);
+
+	return session_id;
 }
 
-static PyObject *PySSLSESSION_master_key(PyObject *self, PyObject *args) {
-    PyObject *pysslobject = Py_None;
-    PyObject *master_key = Py_None;
-    PySSLObject *pyssl = NULL;
+static PyObject *PySSLSESSION_master_key(PyObject * self, PyObject * args)
+{
+	PyObject *pysslobject = Py_None;
+	PyObject *master_key = Py_None;
+	PySSLObject *pyssl = NULL;
 
-    if (!PyArg_ParseTuple(args, "|O:master_key", &pysslobject))
-        return NULL;
-    pyssl = (PySSLObject *) pysslobject;
+	if (!PyArg_ParseTuple(args, "|O:master_key", &pysslobject))
+		return NULL;
+	pyssl = (PySSLObject *) pysslobject;
 
-    /*
-    fprintf(stderr, "\npysslobject: %p\n", pysslobject);
-    fprintf(stderr, "\npyssl: %p\n", pyssl);
-	fprintf(stderr, "\npyssl->ssl: %p\n", pyssl->ssl);
-    fprintf(stderr, "\npyssl->ssl->session: %p\n", pyssl->ssl->session);
-    fprintf(stderr, "\npyssl->ssl->session->master_key: %s\n", 
-    					pyssl->ssl->session->master_key);
-    */
+	/*
+	   fprintf(stderr, "\npysslobject: %p\n", pysslobject);
+	   fprintf(stderr, "\npyssl: %p\n", pyssl);
+	   fprintf(stderr, "\npyssl->ssl: %p\n", pyssl->ssl);
+	   fprintf(stderr, "\npyssl->ssl->session: %p\n", pyssl->ssl->session);
+	   fprintf(stderr, "\npyssl->ssl->session->master_key: %s\n", 
+	   pyssl->ssl->session->master_key);
+	 */
 
-    master_key = PyString_FromStringAndSize(
-                                (const char *) pyssl->ssl->session->master_key,
-    					        SSL_MAX_MASTER_KEY_LENGTH);
+	master_key = PyString_FromStringAndSize((const char *)pyssl->ssl->
+						session->master_key,
+						SSL_MAX_MASTER_KEY_LENGTH);
 
-    return master_key;
+	return master_key;
 }
 
-static char session_id_docs[] =
-   "Returns SSL session id\n";
+static char session_id_docs[] = "Returns SSL session id\n";
 
-static char master_key_docs[] =
-   "Returns SSL session master key\n";
+static char master_key_docs[] = "Returns SSL session master key\n";
 
 static PyMethodDef PySSLSESSIONMethods[] = {
-   {"session_id", (PyCFunction)PySSLSESSION_session_id, 
-      METH_VARARGS, session_id_docs},
-   {"master_key", (PyCFunction)PySSLSESSION_master_key, 
-      METH_VARARGS, master_key_docs},
-   {NULL}
+	{"session_id", (PyCFunction) PySSLSESSION_session_id,
+	 METH_VARARGS, session_id_docs},
+	{"master_key", (PyCFunction) PySSLSESSION_master_key,
+	 METH_VARARGS, master_key_docs},
+	{NULL}
 };
 
-void init_sslsession(void) {
-   Py_InitModule3("_sslsession", PySSLSESSIONMethods,
-                  "SSL session module");
+void init_sslsession(void)
+{
+	Py_InitModule3("_sslsession", PySSLSESSIONMethods,
+		       "SSL session module");
 }
