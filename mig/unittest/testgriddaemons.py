cc01efb92c8dbec9404a80da9b17cc8cd02d7f2f
Martin Garrigues Rehr <rehr@nbi.ku.dk>
2020-03-04 09:19:18 +0000

    Moved griddaemons unit test to mig/unittest
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@4592 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/unittest/testgriddaemons.py
+++ mig/unittest/testgriddaemons.py
@@ -3,7 +3,7 @@
 #
 # --- BEGIN_HEADER ---
 #
-# griddaemons.__main__ - Unit tests for grid daemon helper functions
+# testgriddaemons- Set of unit tests for grid daemon helper functions
 # Copyright (C) 2010-2020  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
