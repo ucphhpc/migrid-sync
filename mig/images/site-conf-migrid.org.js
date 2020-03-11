39af59c1d09e83300aa1d90d2f1ea5d36b0bd5c3
Jonas Bardino <bardino@nbi.ku.dk>
2020-03-11 16:41:06 +0000

    missing site conf for migrid
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@4634 b75ad72c-e7d7-11dd-a971-7dbc132099af

---
+++ mig/images/site-conf-migrid.org.js
@@ -0,0 +1,14 @@
+/* 
+   Local site variables to adjust look n' feel of what is displayed on the 
+   'static' html pages with javascript content loading on this site.
+
+*/
+function get_site_conf(key) {
+    var value;
+    if (key === 'content_url') {
+        value = "status-events.json";
+    } else if (key === 'system_match') {
+        value = ["ALL", "MiG", "migrid"];
+    } 
+    return value;
+}
