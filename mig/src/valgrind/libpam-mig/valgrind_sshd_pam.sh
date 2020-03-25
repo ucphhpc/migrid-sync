d0680e4336d7674ee62203b0c549bef624edf6e7
Martin Garrigues Rehr <rehr@nbi.ku.dk>
2020-03-25 13:43:39 +0000

    Added valgrind framework from libpam-mig
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@4688 b75ad72c-e7d7-11dd-a971-7dbc132099af

---
+++ mig/src/valgrind/libpam-mig/valgrind_sshd_pam.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+BASEPATH="${PWD}"
+logdir="${BASEPATH}/log"
+program="/usr/sbin/sshd -D -f /etc/ssh/sshd_config-MiG-sftp-subsys"
+datestamp=$(date +%Y-%m-%d_%T)
+#pyinit_logfile="${datestamp}.init.valgrind"
+logfile="${logdir}/${datestamp}.valgrind"
+dlopen_supp="${BASEPATH}/valgrind-dlfcn.supp"
+pyinit_supp="${BASEPATH}/valgrind-c-python-api.supp"
+pymig_supp="${BASEPATH}/valgrind-c-python-api-mig.supp"
+sshdpam_supp="${BASEPATH}/valgrind-sshd-pam.supp"
+pammig_supp="${BASEPATH}/valgrind-pam-mig.supp"
+
+valgrind_cmd="valgrind --tool=memcheck \
+			    --trace-children=yes \
+			    --dsymutil=yes \
+			    --leak-check=full \
+			    --show-leak-kinds=all \
+			    --suppressions=${dlopen_supp} \
+			    --suppressions=${pyinit_supp} \
+			    --suppressions=${pymig_supp} \
+			    --suppressions=${sshdpam_supp} \
+			    --suppressions=${pammig_supp} \
+			    --gen-suppressions=all \
+			    --log-file=${logfile} \
+			    ${program}"
+echo "Output to file: ${logfile}"
+mkdir -p ${logdir}
+echo ${valgrind_cmd}
+eval ${valgrind_cmd}
