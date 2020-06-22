ucphhpc/migrid-sync

commit 0f59297eb63b2d75b025ce92aef0512bf72194f1

added inline warning about upcoming expiry on Setup IO service pages. Saving pw/key within last two weeks before expire additionally results in an attempt to extend account expiry with the default 30 days like successful login after expire does.
git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@4808 b75ad72c-e7d7-11dd-a971-7dbc132099af

mig/shared/functionality/settingsaction.py
@@ -4,7 +4,7 @@
 # --- BEGIN_HEADER ---
 #
 # settingsaction - handle user settings updates
-# Copyright (C) 2003-2019  The MiG Project lead by Brian Vinter
+# Copyright (C) 2003-2020  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
 #
@@ -31,6 +31,7 @@ import os
 import tempfile
 
 import shared.returnvalues as returnvalues
+from shared.accountstate import check_update_account_expire
 from shared.duplicatikeywords import get_keywords_dict as duplicati_keywords
 from shared.functional import validate_input_and_cert
 from shared.gdp import get_client_id_from_project_client_id
@@ -239,6 +240,12 @@ CSRF-filtered POST requests to prevent unintended updates'''
                     {'object_type': 'warning', 'text':
                      'Only the first key will be activated on instances'})
 
+        # Try to refresh expire if possible to make sure it works for a while
+        if topic in ['sftp', 'webdavs', 'ftps']:
+            (_, account_expire, _) = check_update_account_expire(
+                configuration, client_id, min_days_left=14)
+            logger.debug("check and update account expire returned %s" %
+                         account_expire)
         else:
             output_objects.append({'object_type': 'error_text', 'text':
                                    'No such settings topic: %s' % topic

mig/shared/accountstate.py
@@ -34,7 +34,8 @@ import time
 
 from shared.base import client_id_dir, client_dir_id, requested_url_base
 from shared.defaults import expire_marks_dir, status_marks_dir, \
-    valid_account_status, oid_auto_extend_days, cert_auto_extend_days
+    valid_account_status, oid_auto_extend_days, cert_auto_extend_days, \
+    cert_valid_days, oid_valid_days
 from shared.filemarks import get_filemark, update_filemark, reset_filemark
 from shared.userdb import load_user_dict, default_db_path, update_user_dict
 
@@ -192,9 +193,13 @@ def check_account_expire(configuration, client_id, environ=None):
     return (pending_expire, account_expire, user_dict)
 
 
-def check_update_account_expire(configuration, client_id, environ=None):
+def check_update_account_expire(configuration, client_id, environ=None,
+                                min_days_left=0):
     """Check and possibly update client_id expire field in cache and user DB
-    if configured.
+    if configured. The optional environ can be used to provide current environ
+    dict instead of the default os.environ. The optional min_days_left
+    argument is used to attempt renew if the account is set to expire before N
+    days from now. Default is zero days to only renew if expired.
     """
     _logger = configuration.logger
     if not environ:
@@ -203,7 +208,7 @@ def check_update_account_expire(configuration, client_id, environ=None):
     (pending_expire, account_expire, user_dict) = check_account_expire(
         configuration, client_id, environ)
     # Now check actual expire
-    if account_expire and account_expire < time.time():
+    if account_expire and account_expire < time.time() + min_days_left * 86400:
         _logger.info("user is marked expired at %s" % account_expire)
         # NOTE: users who got this far obviously has a working auth method
         vhost_url = requested_url_base(environ)
@@ -246,7 +251,7 @@ def check_update_account_expire(configuration, client_id, environ=None):
                 _logger.info("user expire updated to %(expire)s" % user_dict)
                 pending_expire = True
             else:
-                _logger.warning("expire not supported for %s with status %r" %
+                _logger.warning("extend expire refused for %s with status %r" %
                                 (client_id, account_status))
                 pending_expire = False
         else:
@@ -257,6 +262,40 @@ def check_update_account_expire(configuration, client_id, environ=None):
                       (client_id, account_expire))
         pending_expire = True
     return (pending_expire, account_expire, user_dict)
+
+
+def account_expire_info(configuration, username, environ=None,
+                        min_days_left=14):
+    """Helper to lookup when username account expires and details about renew
+    and auto extension support in case account has less than min_days_left
+    before expiry.
+    """
+    _logger = configuration.logger
+    if not environ:
+        environ = os.environ
+    extend_days, renew_days = 0, 0
+    (_, account_expire, _) = check_account_expire(configuration, username)
+    (_, account_status, _) = check_account_status(configuration, username)
+    vhost_url = requested_url_base(environ)
+    expire_warn = False
+    if account_expire and account_expire < time.time() + min_days_left * 86400:
+        expire_warn = True
+        if vhost_url == configuration.migserver_https_ext_oid_url:
+            renew_days = oid_valid_days
+            if account_status == 'active' and configuration.auto_add_oid_user:
+                extend_days = oid_auto_extend_days
+        elif vhost_url == configuration.migserver_https_mig_oid_url:
+            renew_days = oid_valid_days
+        elif vhost_url == configuration.migserver_https_ext_cert_url:
+            renew_days = cert_valid_days
+            if account_status == 'active' and configuration.auto_add_cert_user:
+                extend_days = cert_auto_extend_days
+        elif vhost_url == configuration.migserver_https_mig_cert_url:
+            renew_days = cert_valid_days
+        else:
+            _logger.warning("unexpected vhost in expire detection: %s" %
+                            vhost_url)
+    return (expire_warn, account_expire, renew_days, extend_days)
 
 
 def detect_special_login(configuration, username, proto):

mig/shared/functionality/setup.py
@@ -28,8 +28,10 @@
 """Provide all the client access setup subpages"""
 
 import os
+import time
 
 import shared.returnvalues as returnvalues
+from shared.accountstate import account_expire_info
 from shared.auth import get_twofactor_secrets
 from shared.base import client_alias, client_id_dir, extract_field, get_xgi_bin
 from shared.defaults import seafile_ro_dirname, duplicati_conf_dir, csrf_field, \
@@ -217,11 +219,32 @@ def main(client_id, user_arguments_dict):
         return (output_objects, returnvalues.CLIENT_ERROR)
 
     save_html = save_settings_html(configuration)
+    (expire_warn, expire_time, renew_days, extend_days) = account_expire_info(
+        configuration, client_id)
+    expire_html = ''
+    if expire_warn:
+        expire_warn_msg = '''<p class="warningtext">
+NOTE: your %s account access including efficient file service access expires on
+%s. You can always repeat sign up to extend general access with another %s days.
+%s  
+</p>'''
+        auto_renew_msg = '''Alternatively simply either hit Save below now
+<em>or</em> log in here again after that date to quickly extend your access for
+%s days.'''
+        expire_date = time.ctime(expire_time)
+        logger.debug("warn about expire on %s" % expire_date)
+        extend_msg = ''
+        if extend_days > 0:
+            extend_msg = auto_renew_msg % extend_days
+        expire_html = expire_warn_msg % (configuration.short_title,
+                                         expire_date, renew_days, extend_msg)
+
     form_method = 'post'
     csrf_limit = get_csrf_limit(configuration)
     fill_helpers = {'site': configuration.short_title,
                     'form_method': form_method, 'csrf_field': csrf_field,
-                    'csrf_limit': csrf_limit, 'save_html': save_html}
+                    'csrf_limit': csrf_limit, 'save_html': save_html,
+                    'expire_html': expire_html}
 
     if 'sftp' in topic_list:
 
@@ -395,6 +418,7 @@ value="%(default_authpassword)s" />
 '''
 
         html += '''
+%(expire_html)s
 %(save_html)s
 <input type="submit" value="Save SFTP Settings" />
 '''
@@ -558,6 +582,7 @@ value="%(default_authpassword)s" />
 '''
 
         html += '''
+%(expire_html)s
 %(save_html)s
 <input type="submit" value="Save WebDAVS Settings" />
 
@@ -730,6 +755,7 @@ value="%(default_authpassword)s" />
 '''
 
         html += '''
+%(expire_html)s
 %(save_html)s
 <input type="submit" value="Save FTPS Settings" />
 

commit 430d33faeefc825ee021ef24ea5b956ea2cfa072

fix a bug introduced in r4808 which broke e.g. Save Settings due to incorrect placement of the new conditional to force account refresh on saving IO Setup.
git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@4834 b75ad72c-e7d7-11dd-a971-7dbc132099af

mig/shared/functionality/settingsaction.py
@@ -239,6 +239,11 @@ CSRF-filtered POST requests to prevent unintended updates'''
                 output_objects.append(
                     {'object_type': 'warning', 'text':
                      'Only the first key will be activated on instances'})
+        else:
+            output_objects.append({'object_type': 'error_text', 'text':
+                                   'No such settings topic: %s' % topic
+                                   })
+            return (output_objects, returnvalues.CLIENT_ERROR)
 
         # Try to refresh expire if possible to make sure it works for a while
         if topic in ['sftp', 'webdavs', 'ftps']:
@@ -246,11 +251,6 @@ CSRF-filtered POST requests to prevent unintended updates'''
                 configuration, client_id, min_days_left=14)
             logger.debug("check and update account expire returned %s" %
                          account_expire)
-        else:
-            output_objects.append({'object_type': 'error_text', 'text':
-                                   'No such settings topic: %s' % topic
-                                   })
-            return (output_objects, returnvalues.CLIENT_ERROR)
 
         try:
             os.remove(tmptopicfile)