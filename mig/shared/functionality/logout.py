6c5ae86e10a92bb0792c75b24e785f591ac45c4e
Jonas Bardino <bardino@nbi.ku.dk>
2022-06-17 15:36:26 +0000

    rudimentary openid connect logout using the vanity url helper of the apache mod auth oidc: only tested to basically work against Microfocus Access Manager.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@5464 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/functionality/logout.py
+++ mig/shared/functionality/logout.py
@@ -4,7 +4,7 @@
 # --- BEGIN_HEADER ---
 #
 # logout - force-expire local login session(s)
-# Copyright (C) 2003-2018  The MiG Project lead by Brian Vinter
+# Copyright (C) 2003-2022  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
 #
@@ -26,15 +26,20 @@
 #
 
 """Simple backend to force login session expiry"""
+
 from __future__ import absolute_import
 
 import os
 
 from mig.shared import returnvalues
 from mig.shared.auth import expire_twofactor_session
+from mig.shared.defaults import AUTH_CERTIFICATE, AUTH_OPENID_V2, \
+    AUTH_OPENID_CONNECT, AUTH_MIG_OID, AUTH_EXT_OID, AUTH_MIG_OIDC, \
+    AUTH_EXT_OIDC
 from mig.shared.functional import validate_input_and_cert
 from mig.shared.gdp.all import project_logout, get_client_id_from_project_client_id
-from mig.shared.httpsclient import extract_client_openid
+from mig.shared.httpsclient import extract_client_id, detect_client_auth, \
+    build_logout_url, extract_client_openid
 from mig.shared.init import initialize_main_variables, find_entry
 from mig.shared.useradm import expire_oid_sessions, find_oid_sessions
 
@@ -75,10 +80,10 @@ def main(client_id, user_arguments_dict, environ=None):
     '''})
 
     output_objects.append({'object_type': 'header', 'text': 'Logout'})
-    (oid_db, identity) = extract_client_openid(configuration, environ,
-                                               lookup_dn=False)
+    (auth_type, auth_flavor) = detect_client_auth(configuration, environ)
+    identity = extract_client_id(configuration, environ, lookup_dn=False)
     logger.info("%s from %s with identity %s" % (op_name, client_id, identity))
-    if client_id and client_id == identity:
+    if client_id and auth_type == AUTH_CERTIFICATE:
         output_objects.append(
             {'object_type': 'warning', 'text':
              """You're accessing %s with a user certificate so to completely
@@ -116,56 +121,76 @@ There was a potential problem with 2-factor session termination. Please contact
 the %s Admins if it happens repeatedly.
 </p>""" % configuration.short_title
                      })
-        logger.info("expiring active sessions for %s in %s" % (identity,
-                                                               oid_db))
-        (success, _) = expire_oid_sessions(configuration, oid_db, identity)
-        logger.info("verifying no active sessions left for %s" % identity)
-        (found, remaining) = find_oid_sessions(configuration, oid_db, identity)
+        if auth_type == AUTH_OPENID_V2:
+            (oid_db, identity) = extract_client_openid(configuration, environ,
+                                                       lookup_dn=False)
+            logger.info("expiring active sessions for %s in %s" % (identity,
+                                                                   oid_db))
+            (success, _) = expire_oid_sessions(configuration, oid_db, identity)
+            logger.info("verifying no active sessions left for %s" % identity)
+            (found, remaining) = find_oid_sessions(
+                configuration, oid_db, identity)
+        elif auth_type == AUTH_OPENID_CONNECT:
+            # NOTE: mod auth oidc handles local logout automatically
+            logger.info("no manual cleanup needed for %s session of %s" %
+                        (auth_type, identity))
+            success, found, remaining = True, True, []
+        else:
+            logger.error("unexpected auth_type %r in logout for in %s" %
+                         (auth_type, identity))
+            output_objects.append(
+                {'object_type': 'warning', 'text':
+                 """You're accessing %s with %s credentials not covered by
+logout, so to completely logout you need to make sure they are protected by a
+password and then close the browser. Please refer to your browser and system
+documentation for details.""" % (configuration.short_title, auth_type)})
+            return (output_objects, status)
+
         if success and found and not remaining:
             if configuration.site_enable_gdp:
                 reentry_page = "https://%s" % configuration.server_fqdn
-                if configuration.user_mig_oid_provider and \
-                        identity.startswith(
-                            configuration.user_mig_oid_provider):
-                    reentry_page = configuration.migserver_https_mig_oid_url
-                elif configuration.user_ext_oid_provider and \
-                        identity.startswith(
-                            configuration.user_ext_oid_provider):
-                    reentry_page = configuration.migserver_https_ext_oid_url
+                if auth_type == AUTH_OPENID_V2:
+                    if auth_flavor == AUTH_MIG_OID:
+                        reentry_page = configuration.migserver_https_mig_oid_url
+                    elif auth_flavor == AUTH_EXT_OID:
+                        reentry_page = configuration.migserver_https_ext_oid_url
+                elif auth_type == AUTH_OPENID_CONNECT:
+                    if auth_flavor == AUTH_MIG_OIDC:
+                        reentry_page = configuration.migserver_https_mig_oidc_url
+                    elif auth_flavor == AUTH_EXT_OIDC:
+                        reentry_page = configuration.migserver_https_ext_oidc_url
                 project_logout(
                     configuration, 'https', environ['REMOTE_ADDR'], client_id)
-                html = '''
+                html = """
                 <a id='gdp_logout' href='%s'></a>
                 <script type='text/javascript'>
                     document.getElementById('gdp_logout').click();
-                </script>''' % reentry_page
+                </script>""" % reentry_page
                 output_objects.append(
                     {'object_type': 'html_form', 'text': html})
             else:
                 output_objects.append(
-                    {'object_type': 'text', 'text': """You are now logged out of %s
-    locally - you may want to close your web browser to finish"""
-                     % configuration.short_title})
+                    {'object_type': 'text', 'text':
+                     """You are now logged out of %s locally - you may want to
+close your web browser to finish""" % configuration.short_title})
                 title_entry = find_entry(output_objects, 'title')
                 title_entry['skipmenu'] = True
+
         else:
             logger.error("remaining active sessions for %s: %s" % (identity,
                                                                    remaining))
-            output_objects.append({'object_type': 'error_text',
-                                   'text': "Could not log you out of %s!"
+            output_objects.append({'object_type': 'error_text', 'text':
+                                   "Could not log you out of %s!"
                                    % configuration.short_title})
             status = returnvalues.CLIENT_ERROR
     else:
-        local_logout = '?logout=true'
-        oid_logout = os.path.join(os.path.dirname(os.path.dirname(identity)),
-                                  'logout?return_to=%(SCRIPT_URI)s' % environ)
-        oid_logout += local_logout
+        logout_url = build_logout_url(configuration, environ)
         output_objects.append(
             {'object_type': 'text', 'text': """Are you sure you want to
 log out of %s?""" % configuration.short_title})
         output_objects.append({'object_type': 'text', 'text': ""})
         output_objects.append(
-            {'object_type': 'link', 'destination': oid_logout,
+            {'object_type': 'link', 'destination': logout_url,
              'class': 'genericbutton greenBtn', 'text': "Yes"})
         output_objects.append(
             {'object_type': 'link',
