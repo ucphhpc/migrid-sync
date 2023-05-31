0225ae40edf93d44774e75f7c269824020fd43b1
Jonas Bardino <bardino@nbi.ku.dk>
2023-05-31 09:56:00 +0000

    include client address in SSL/TLS wrap warning and error logs as requested in migrid-sync issue 13
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@5720 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/server/grid_webdavs-3.x.py
+++ mig/server/grid_webdavs-3.x.py
@@ -4,7 +4,7 @@
 # --- BEGIN_HEADER ---
 #
 # grid_webdavs - secure WebDAV server providing access to MiG user homes
-# Copyright (C) 2003-2022  The MiG Project lead by Brian Vinter
+# Copyright (C) 2003-2023  The MiG Project lead by Brian Vinter
 #
 # This file is part of MiG.
 #
@@ -387,8 +387,14 @@ class HardenedSSLAdapter(BuiltinSSLAdapter):
         """
         _socket_list = [sock]
         try:
+            client_addr = sock.getpeername()
+        except Exception as exc:
+            logger.warning("could not lookup client address for %s: %s" %
+                           (sock, exc))
+            client_addr = 'UNKNOWN'
+        try:
             # logger.debug("Wrapping socket in SSL/TLS: 0x%x : %s" %
-            #             (id(sock), sock.getpeername()))
+            #             (id(sock), client_addr))
             # logger.debug("SSL/TLS session stats: %s" %
             #             self.context.session_stats())
             ssl_sock = self.context.wrap_socket(sock, server_side=True)
@@ -410,15 +416,15 @@ class HardenedSSLAdapter(BuiltinSSLAdapter):
                 # logger.debug("SSL/TLS received EOF: %s" % exc)
                 return None, {}
             elif exc.errno == ssl.SSL_ERROR_SSL:
-                logger.warning("SSL/TLS wrap failed: %s" % exc)
+                logger.warning("SSL/TLS wrap of %s failed: %s" %
+                               (client_addr, exc))
                 if exc.args[1].find('http request') != -1:
                     # The client is speaking HTTP to an HTTPS server.
-                    logger.warning(
-                        "SSL/TLS got unexpected plain HTTP: %s" % exc)
+                    logger.debug("SSL/TLS got unexpected plain HTTP: %s" % exc)
                     raise NoSSLError
                 elif exc.args[1].find('unknown protocol') != -1:
                     # Drop clients speaking some non-HTTP protocol.
-                    logger.warning("SSL/TLS got unexpected protocol: %s" % exc)
+                    logger.debug("SSL/TLS got unexpected protocol: %s" % exc)
                     return None, {}
                 elif exc.args[1].find('wrong version number') != -1 or \
                         exc.args[1].find('no shared cipher') != -1 or \
@@ -426,13 +432,14 @@ class HardenedSSLAdapter(BuiltinSSLAdapter):
                         exc.args[1].find('ccs received early') != -1 or \
                         exc.args[1].find('parse tlsext') != -1:
                     # Drop clients trying banned protocol, cipher or operation
-                    logger.warning("SSL/TLS got invalid request: %s" % exc)
+                    logger.debug("SSL/TLS got invalid request: %s" % exc)
                     return None, {}
                 else:
                     # Make sure we clean up before we forward
                     # unexpected SSL errors
                     self.__force_close(_socket_list)
-            logger.error("unexpected SSL/TLS wrap failure: %s" % exc)
+            logger.error("SSL/TLS wrap of %s failed unexpectedly: %s" %
+                         (client_addr, exc))
             raise exc
 
         return ssl_sock, ssl_env
