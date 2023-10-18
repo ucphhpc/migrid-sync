5efee2c7806a64f5ade7b95bc714f555d4a0751a
Jonas Bardino <bardino@nbi.ku.dk>
2023-10-18 08:24:23 +0000

    removed user-projects now that the contents are migrated to a standalone migrid-user-projects repo at github
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@5837 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- user-projects/miginterface/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This file is needed to tell python that this dir is a package
-so that other modules can call, say, import shared.editing
-Please refer to http://www.network-theory.co.uk/docs/pytut/tut_51.html
-for details
-"""
-
-from miginterface import *
