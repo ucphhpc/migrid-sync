9bb67446b12b90b13719ef1fd1b0dd94c8f387a8
Jonas Bardino <bardino@nbi.ku.dk>
2024-05-29 17:24:01 +0000

    rework the write file and chunk helpers to do intelligent 'mode' selection based on input data type (unicode or bytes) as a follow-up to PR48. Thoroughly exercise the helpers using all four combinations of format and mode in unit tests to make sure we cover the cases previously handled by explicit byte and unicode conversion with force utf8 and force native str helpers when running in python3 on 'experimental' branch.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@6040 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- tests/test_mig_shared_fileio.py
+++ tests/test_mig_shared_fileio.py
@@ -12,6 +12,8 @@ import mig.shared.fileio as fileio
 
 DUMMY_BYTES = binascii.unhexlify('DEADBEEF')  # 4 bytes
 DUMMY_BYTES_LENGTH = 4
+DUMMY_UNICODE = u'UniCode123'
+DUMMY_UNICODE_LENGTH = len(DUMMY_UNICODE)
 DUMMY_FILE_WRITECHUNK = 'fileio/write_chunk'
 DUMMY_FILE_WRITEFILE = 'fileio/write_file'
 
@@ -65,6 +67,33 @@ class MigSharedFileio__write_chunk(MigTestCase):
                              "expected a hole was left")
             self.assertEqual(content[3:], DUMMY_BYTES)
 
+    def test_store_bytes_in_text_mode(self):
+        fileio.write_chunk(self.tmp_path, DUMMY_BYTES, 0, self.logger,
+                           mode="r+")
+
+        with open(self.tmp_path, 'rb') as file:
+            content = file.read(1024)
+            self.assertEqual(len(content), DUMMY_BYTES_LENGTH)
+            self.assertEqual(content[:], DUMMY_BYTES)
+
+    def test_store_unicode(self):
+        fileio.write_chunk(self.tmp_path, DUMMY_UNICODE, 0, self.logger,
+                           mode='r+')
+
+        with open(self.tmp_path, 'r') as file:
+            content = file.read(1024)
+            self.assertEqual(len(content), DUMMY_UNICODE_LENGTH)
+            self.assertEqual(content[:], DUMMY_UNICODE)
+
+    def test_store_unicode_in_binary_mode(self):
+        fileio.write_chunk(self.tmp_path, DUMMY_UNICODE, 0, self.logger,
+                           mode='r+b')
+
+        with open(self.tmp_path, 'r') as file:
+            content = file.read(1024)
+            self.assertEqual(len(content), DUMMY_UNICODE_LENGTH)
+            self.assertEqual(content[:], DUMMY_UNICODE)
+
 
 class MigSharedFileio__write_file(MigTestCase):
     def setUp(self):
@@ -102,6 +131,36 @@ class MigSharedFileio__write_file(MigTestCase):
             content = file.read(1024)
             self.assertEqual(len(content), DUMMY_BYTES_LENGTH)
             self.assertEqual(content[:], DUMMY_BYTES)
+
+    def test_store_bytes_in_text_mode(self):
+        did_succeed = fileio.write_file(DUMMY_BYTES, self.tmp_path, self.logger,
+                                        mode="w")
+        self.assertTrue(did_succeed)
+
+        with open(self.tmp_path, 'rb') as file:
+            content = file.read(1024)
+            self.assertEqual(len(content), DUMMY_BYTES_LENGTH)
+            self.assertEqual(content[:], DUMMY_BYTES)
+
+    def test_store_unicode(self):
+        did_succeed = fileio.write_file(DUMMY_UNICODE, self.tmp_path,
+                                        self.logger, mode='w')
+        self.assertTrue(did_succeed)
+
+        with open(self.tmp_path, 'r') as file:
+            content = file.read(1024)
+            self.assertEqual(len(content), DUMMY_UNICODE_LENGTH)
+            self.assertEqual(content[:], DUMMY_UNICODE)
+
+    def test_store_unicode_in_binary_mode(self):
+        did_succeed = fileio.write_file(DUMMY_UNICODE, self.tmp_path,
+                                        self.logger, mode='wb')
+        self.assertTrue(did_succeed)
+
+        with open(self.tmp_path, 'r') as file:
+            content = file.read(1024)
+            self.assertEqual(len(content), DUMMY_UNICODE_LENGTH)
+            self.assertEqual(content[:], DUMMY_UNICODE)
 
 
 if __name__ == '__main__':
