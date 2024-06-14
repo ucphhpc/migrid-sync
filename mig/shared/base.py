162967ec862c9e2d60f65add3b9c651496896a95
Jonas Bardino <bardino@nbi.ku.dk>
2024-06-14 09:31:40 +0000

    Add minor adjustment to mig.shared.base that I missed in my PR61 merge because it was only visible in the original round of commits on github, sorry.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@6056 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- mig/shared/base.py
+++ mig/shared/base.py
@@ -29,6 +29,7 @@
 
 from __future__ import print_function
 from __future__ import absolute_import
+from past.builtins import basestring
 
 from builtins import range
 from past.builtins import basestring
