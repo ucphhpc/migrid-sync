8ad62bdfbef68a1559844409d24bdf033b3f9218
Jonas Bardino <bardino@nbi.ku.dk>
2024-06-18 14:56:39 +0000

    manually merge PR65 to add more unit tests for safeinput.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@6059 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- tests/test_mig_shared_safeinput.py
+++ tests/test_mig_shared_safeinput.py
@@ -27,13 +27,31 @@
 
 """Unit tests for the migrid module pointed to in the filename"""
 
+import codecs
 import importlib
 import os
 import sys
+from past.builtins import basestring
 
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), ".")))
 
 from support import MigTestCase, testmain
+from mig.shared.safeinput import \
+    filter_commonname, valid_commonname
+
+PY2 = sys.version_info[0] == 2
+
+
+def as_string_of_unicode(value):
+    assert isinstance(value, basestring)
+    if not is_string_of_unicode(value):
+        assert PY2, "unreachable unless Python 2"
+        return unicode(codecs.decode(value, 'utf8'))
+    return value
+
+
+def is_string_of_unicode(value):
+    return type(value) == type(u'')
 
 
 class MigSharedSafeinput(MigTestCase):
@@ -44,6 +62,47 @@ class MigSharedSafeinput(MigTestCase):
     def test_existing_main(self):
         safeimport = importlib.import_module("mig.shared.safeinput")
         safeimport.main(_print=lambda _: None)
+
+    COMMONNAME_PERMITTED = (
+        'Firstname Lastname',
+        'Test Æøå',
+        'Test Überh4x0r',
+        'Harry S. Truman',
+        u'Unicode æøå')
+
+    COMMONNAME_PROHIBITED = (
+        "Invalid D'Angelo",
+        'Test Maybe Invalid Źacãŕ',
+        'Test Invalid ?',
+        'Test HTML Invalid <code/>')
+
+    def test_commonname_valid(self):
+        for test_cn in self.COMMONNAME_PERMITTED:
+            saw_raise = False
+            try:
+                valid_commonname(test_cn)
+            except Exception:
+                saw_raise = True
+            self.assertFalse(saw_raise)
+
+        for test_cn in self.COMMONNAME_PROHIBITED:
+            saw_raise = False
+            try:
+                valid_commonname(test_cn)
+            except Exception:
+                saw_raise = True
+            self.assertTrue(saw_raise)
+
+    def test_commonname_filter(self):
+        for test_cn in self.COMMONNAME_PERMITTED:
+            test_cn_unicode = as_string_of_unicode(test_cn)
+            filtered_cn = filter_commonname(test_cn)
+            self.assertEqual(filtered_cn, test_cn_unicode)
+
+        for test_cn in self.COMMONNAME_PROHIBITED:
+            test_cn_unicode = as_string_of_unicode(test_cn)
+            filtered_cn = filter_commonname(test_cn)
+            self.assertNotEqual(filtered_cn, test_cn_unicode)
 
 
 if __name__ == '__main__':
