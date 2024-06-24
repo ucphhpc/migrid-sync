83a0e2369412c38109b29dbec2dd1ce71380b5b1
Alex Burke <albu@di.ku.dk>
2024-06-24 13:55:46 +0200

    fixu?

--- mig/shared/localtypes.py
+++ mig/shared/localtypes.py
@@ -1,6 +1,5 @@
-from past.builtins import basestring
 from enum import ReprEnum
-from types import SimpleNamespace
+from past.builtins import basestring
 
 """ A module containing a set of commonly used local data strutures.
 
@@ -9,11 +8,11 @@ can be used as the basis for defining any other parts of the system.
 """
 
 
-def _isascii(s):
+def _isascii(value):
     """Given a string establish that is conatains only plain ascii.
     """
-    assert isinstance(s, basestring)
-    return not any((b > 0x7f for b in bytearray(s, 'utf8')))
+    assert isinstance(value, basestring)
+    return not any((b > 0x7f for b in bytearray(value, 'utf8')))
 
 
 class AsciiEnum(str, ReprEnum):
@@ -22,31 +21,14 @@ class AsciiEnum(str, ReprEnum):
     """
 
     def __new__(cls, *values):
-        "values must already be of type `str`"
         if len(values) != 1:
             raise TypeError(
                 'only a single argument is supported: %r' % (values, ))
-        thestring = values[0]
-        if not isinstance(thestring, str):
+        thestring = str(values[0])
+        if not isinstance(thestring, basestring):
             raise TypeError('%r is not a string' % (thestring, ))
         if not _isascii(thestring):
             raise TypeError('%r is not pure ascii' % (thestring, ))
         member = str.__new__(cls, thestring)
         member._value_ = thestring
         return member
-
-
-def namedconstants(name, d):
-    """Given a closed set of key-value pairs sepcified by a dict define
-    an immutable object which exposes these mappings via subscripting.
-    """
-
-    class _Constants(SimpleNamespace):
-        __slots__ = [k.name for k in d.keys()]
-
-        def __init__(self, defaults):
-            super(_Constants, self).__init__(**defaults)
-
-        def __getitem__(self, key):
-            return self.__getattribute__(key)
-    return _Constants(d)
