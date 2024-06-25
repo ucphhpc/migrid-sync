5719c23927d6c69b7eced629325ded15af62421e
Jonas Bardino <bardino@nbi.ku.dk>
2024-06-25 13:16:54 +0000

    minor polish in follow up to previous PRs. Only comments and docstrings.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@6066 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- tests/test_mig_shared_fileio.py
+++ tests/test_mig_shared_fileio.py
@@ -19,9 +19,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 #
-# -- END_HEADER ---
+# --- END_HEADER ---
 #
 
 """Unit test fileio functions"""
