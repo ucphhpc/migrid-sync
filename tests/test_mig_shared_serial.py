430fa9d8b618aca46bb08b1c05f8ecf8286020b7
Jonas Bardino <bardino@nbi.ku.dk>
2024-06-25 13:16:54 +0000

    minor polish in follow up to previous PRs. Only comments and docstrings.
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@6066 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- tests/test_mig_shared_serial.py
+++ tests/test_mig_shared_serial.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
+#
+# --- BEGIN_HEADER ---
+#
+# test_mig_shared_serial - unit test of the corresponding mig shared module
+# Copyright (C) 2003-2024  The MiG Project by the Science HPC Center at UCPH
+#
+# This file is part of MiG.
+#
+# MiG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# MiG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+#
+# --- END_HEADER ---
+#
+
+"""Unit test serial functions"""
 
 import os
 import sys
