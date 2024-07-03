c91022895721605b1fac674273b03b8321496d68
Jonas Bardino <bardino@nbi.ku.dk>
2024-07-03 13:34:01 +0000

    Manually merged github PR74 - thanks @albu-diku
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@6082 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- tests/test_mig_shared_compat.py
+++ tests/test_mig_shared_compat.py
@@ -19,12 +19,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 #
-# -- END_HEADER ---
+# --- END_HEADER ---
 #
 
-"""Unit test compat functions"""
+"""Unit tests for the migrid module pointed to in the filename"""
 
 import binascii
 import os
@@ -33,20 +34,31 @@ import sys
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), ".")))
 from support import MigTestCase, testmain
 
-from mig.shared.compat import ensure_native_string
+from mig.shared.compat import PY2, ensure_native_string
 
 DUMMY_BYTECHARS = b'DEADBEEF'
 DUMMY_BYTESRAW = binascii.unhexlify('DEADBEEF') # 4 bytes
+DUMMY_UNICODE = u'UniCode123½¾µßðþđŋħĸþł@ª€£$¥©®'
 
 class MigSharedCompat__ensure_native_string(MigTestCase):
-    # TODO: Add docstrings to this class and its methods
+    """Unit test helper for the migrid code pointed to in class name"""
+
     def test_char_bytes_conversion(self):
         actual = ensure_native_string(DUMMY_BYTECHARS)
+        self.assertIs(type(actual), str)
         self.assertEqual(actual, 'DEADBEEF')
 
     def test_raw_bytes_conversion(self):
         with self.assertRaises(UnicodeDecodeError):
             ensure_native_string(DUMMY_BYTESRAW)
+
+    def test_unicode_conversion(self):
+        actual = ensure_native_string(DUMMY_UNICODE)
+        self.assertEqual(type(actual), str)
+        if PY2:
+            self.assertEqual(actual, DUMMY_UNICODE.encode("utf8"))
+        else:
+            self.assertEqual(actual, DUMMY_UNICODE)
 
 
 if __name__ == '__main__':
