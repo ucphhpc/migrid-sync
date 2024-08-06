999ac8c7fad4ac814bdca4a4904d97ec4109b780
Jonas Bardino <bardino@science.ku.dk>
2024-08-06 17:13:02 +0200

    Adjusted to take albu-diku review into account. Moved generic generator method up as module function and all test strings up as top-level 'constants'.

--- tests/test_mig_shared_url.py
+++ tests/test_mig_shared_url.py
@@ -35,71 +35,63 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__))))
 from tests.support import MigTestCase, FakeConfiguration, testmain
 from mig.shared.url import _get_site_urls, check_local_site_url
 
+
+def _generate_dynamic_site_urls(url_base_list):
+    """Simple helper to construct a list of similar URLs for each base url in
+    url_base_list.
+    """
+    site_urls = []
+    for url_base in url_base_list:
+        site_urls += ['%s' % url_base, '%s/' % url_base,
+                      '%s/wsgi-bin/home.py' % url_base,
+                      '%s/wsgi-bin/logout.py' % url_base,
+                      '%s/wsgi-bin/logout.py?return_url=' % url_base,
+                      '%s/wsgi-bin/logout.py?return_url=%s' % (url_base,
+                                                               ENC_URL)
+                      ]
+    return site_urls
+
+
+DUMMY_CONF = FakeConfiguration(migserver_http_url='http://myfqdn.org',
+                               migserver_https_url='https://myfqdn.org',
+                               migserver_https_mig_cert_url='',
+                               migserver_https_ext_cert_url='',
+                               migserver_https_mig_oid_url='',
+                               migserver_https_ext_oid_url='',
+                               migserver_https_mig_oidc_url='',
+                               migserver_https_ext_oidc_url='',
+                               migserver_https_sid_url='',
+                               migserver_public_url='',
+                               migserver_public_alias_url='')
 ENC_URL = 'https%3A%2F%2Fsomewhere.org%2Fsub%0A'
-VALID_STATIC_SITE_URLS = ['', 'abc', 'abc.txt', '/', '/bla', '/bla#anchor',
-                          '/bla/', '/bla/#anchor', '/bla/bla', '/bla/bla/bla',
-                          '//bla//', './bla', './bla/', './bla/bla',
-                          './bla/bla/bla', 'logout.py', 'logout.py?bla=',
-                          '/cgi-sid/logout.py', '/cgi-sid/logout.py?bla=bla',
-                          '/cgi-sid/logout.py?return_url=%s' % ENC_URL,
-                          ]
+# Static site-anchored and dynamic full URLs for local site URL check success
+LOCAL_SITE_URLS = ['', 'abc', 'abc.txt', '/', '/bla', '/bla#anchor',
+                   '/bla/', '/bla/#anchor', '/bla/bla', '/bla/bla/bla',
+                   '//bla//', './bla', './bla/', './bla/bla',
+                   './bla/bla/bla', 'logout.py', 'logout.py?bla=',
+                   '/cgi-sid/logout.py', '/cgi-sid/logout.py?bla=bla',
+                   '/cgi-sid/logout.py?return_url=%s' % ENC_URL,
+                   ]
+LOCAL_BASE_URLS = _get_site_urls(DUMMY_CONF)
+LOCAL_SITE_URLS += _generate_dynamic_site_urls(LOCAL_SITE_URLS)
+# Dynamic full URLs for local site URL check failure
+REMOTE_BASE_URLS = ['https://someevilsite.com', 'ftp://someevilsite.com']
+REMOTE_SITE_URLS = _generate_dynamic_site_urls(REMOTE_BASE_URLS)
 
 
 class BasicUrl(MigTestCase):
     """Wrap unit tests for the corresponding module"""
 
-    def _generate_dynamic_site_urls(self, url_base_list):
-        """Simple helper to construct a list of similar URLs for each base url
-        in url_base_list.
-        """
-        site_urls = []
-        for url_base in url_base_list:
-            site_urls += ['%s' % url_base, '%s/' % url_base,
-                          '%s/wsgi-bin/home.py' % url_base,
-                          '%s/wsgi-bin/logout.py' % url_base,
-                          '%s/wsgi-bin/logout.py?return_url=' % url_base,
-                          '%s/wsgi-bin/logout.py?return_url=%s' % (url_base,
-                                                                   ENC_URL)
-                          ]
-        return site_urls
-
     def test_valid_local_site_urls(self):
         """Check known valid static and dynamic URLs"""
-        dummy_conf = FakeConfiguration(migserver_http_url='http://myfqdn.org',
-                                       migserver_https_url='https://myfqdn.org',
-                                       migserver_https_mig_cert_url='',
-                                       migserver_https_ext_cert_url='',
-                                       migserver_https_mig_oid_url='',
-                                       migserver_https_ext_oid_url='',
-                                       migserver_https_mig_oidc_url='',
-                                       migserver_https_ext_oidc_url='',
-                                       migserver_https_sid_url='',
-                                       migserver_public_url='',
-                                       migserver_public_alias_url='')
-        url_base_urls = _get_site_urls(dummy_conf)
-        valid_site_urls = VALID_STATIC_SITE_URLS
-        valid_site_urls += self._generate_dynamic_site_urls(url_base_urls)
-        for url in valid_site_urls:
-            self.assertTrue(check_local_site_url(dummy_conf, url),
+        for url in LOCAL_SITE_URLS:
+            self.assertTrue(check_local_site_url(DUMMY_CONF, url),
                             "Local site url should succeed for %s" % url)
 
     def test_invalid_local_site_urls(self):
         """Check known invalid URLs"""
-        dummy_conf = FakeConfiguration(migserver_http_url='http://myfqdn.org',
-                                       migserver_https_url='https://myfqdn.org',
-                                       migserver_https_mig_cert_url='',
-                                       migserver_https_ext_cert_url='',
-                                       migserver_https_mig_oid_url='',
-                                       migserver_https_ext_oid_url='',
-                                       migserver_https_mig_oidc_url='',
-                                       migserver_https_ext_oidc_url='',
-                                       migserver_https_sid_url='',
-                                       migserver_public_url='',
-                                       migserver_public_alias_url='')
-        url_base_urls = ['https://someevilsite.com', 'ftp://someevilsite.com']
-        invalid_site_urls = self._generate_dynamic_site_urls(url_base_urls)
-        for url in invalid_site_urls:
-            self.assertFalse(check_local_site_url(dummy_conf, url),
+        for url in REMOTE_SITE_URLS:
+            self.assertFalse(check_local_site_url(DUMMY_CONF, url),
                              "Local site url should fail for %s" % url)
 
 
