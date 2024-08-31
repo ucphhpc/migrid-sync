30359ec3df7c96721b19ecd542bf2dbc50f63768
Jonas Bardino <bardino@nbi.ku.dk>
2024-08-31 14:30:04 +0000

    Manually merge PR112 for asserting a series of related values within a single unit test
    git-svn-id: svn+ssh://svn.code.sf.net/p/migrid/code/trunk@6133 b75ad72c-e7d7-11dd-a971-7dbc132099af

--- tests/test_support.py
+++ tests/test_support.py
@@ -3,10 +3,42 @@ import os
 import sys
 import unittest
 
-from tests.support import MigTestCase, PY2, testmain, temppath
+from tests.support import MigTestCase, PY2, testmain, temppath, \
+                    AssertOver
+
+
+class InstrumentedAssertOver(AssertOver):
+    def __init__(self, *args, **kwargs):
+        AssertOver.__init__(self, *args, **kwargs)
+        self._check_callable = None
+        self._check_callable_called = False
+
+    def get_check_callable(self):
+        return self._check_callable
+
+    def has_check_callable(self):
+        return self._check_callable is not None
+
+    def was_check_callable_called(self):
+        return self._check_callable_called
+
+    def to_check_callable(self):
+        _check_callable = AssertOver.to_check_callable(self)
+        def _wrapped_check_callable():
+            self._check_callable_called = True
+            _check_callable()
+        self._check_callable = _wrapped_check_callable
+        return _wrapped_check_callable
 
 
 class SupportTestCase(MigTestCase):
+    def _class_attribute(self, name, **kwargs):
+        cls = type(self)
+        if 'value' in kwargs:
+            setattr(cls, name, kwargs['value'])
+        else:
+            return getattr(cls, name, None)
+
     @unittest.skipIf(PY2, "Python 3 only")
     def test_unclosed_files_are_recorded(self):
         tmp_path = temppath("support-unclosed", self)
@@ -30,6 +62,30 @@ class SupportTestCase(MigTestCase):
             self._logger.check_empty_and_reset()
         except:
             self.assertTrue(False, "should not be reachable")
+
+    def test_when_asserting_over_multiple_values(self):
+        def assert_is_int(value):
+            assert isinstance(value, int)
+
+        attempt_wrapper = self.assert_over(values=(1, 2, 3), _AssertOver=InstrumentedAssertOver)
+
+        # record the wrapper on the test case so the subsequent test can assert against it
+        self._class_attribute('surviving_attempt_wrapper', value=attempt_wrapper)
+
+        with attempt_wrapper as attempt:
+            attempt(assert_is_int)
+        attempt_wrapper.assert_success()
+
+        self.assertTrue(attempt_wrapper.has_check_callable())
+        # cleanup was recorded
+        self.assertIn(attempt_wrapper.get_check_callable(), self._cleanup_checks)
+
+    def test_when_asserting_over_multiple_values_after(self):
+        # test name is purposefully after ..._recorded in sort order
+        # such that we can check the check function was called correctly
+
+        attempt_wrapper = self._class_attribute('surviving_attempt_wrapper')
+        self.assertTrue(attempt_wrapper.was_check_callable_called())
 
 
 if __name__ == '__main__':
