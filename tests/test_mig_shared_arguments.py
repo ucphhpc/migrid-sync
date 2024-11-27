dedf87cd3289d9a51fe42db582c4e5e9c0d8283a
Alex Burke <albu@di.ku.dk>
2024-11-27 15:49:44 +0100

    Implement declarative argument definitions and logic to bundle values.
    Expand the arguments module with the notions of a defined grouping of arguments
    (ArgumentBundleDefinition) and bundles of particular arguments (ArgumentBundle).
    
    Use this as the mechanism by which payloads are checked for validity. As part
    of declaring a bundle definition the expected positional arguments are declared
    so implement basic length checks that catch missing positional arguments which
    were required. Each argument itself is also tested aganst an optional validity
    function which can be specified at the point of definition.

--- tests/test_mig_shared_arguments.py
+++ tests/test_mig_shared_arguments.py
@@ -3,7 +3,46 @@ import sys
 
 from tests.support import MigTestCase, testmain
 
-from mig.shared.arguments import parse_getopt_args
+from mig.shared.arguments import parse_getopt_args, \
+    ArgumentBundle, ArgumentBundleDefinition
+
+
+def _is_not_none(value):
+    return value is not None
+
+
+class TestMigSharedArguments__bundles(MigTestCase):
+    ThingsBundle = ArgumentBundleDefinition('Things', [
+        (None, 'some_thing', _is_not_none),
+        (None, 'other_thing', _is_not_none),
+    ])
+
+    def assertBundleOfKind(self, value, bundle_kind=None):
+        assert isinstance(bundle_kind, str) and bundle_kind
+        self.assertIsInstance(value, ArgumentBundle, "value is not an argument bundle")
+        self.assertEqual(value.name, bundle_kind, "expected %s bundle, got %s" % (bundle_kind, value.name))
+
+    def test_bundling_arguments_produces_a_bundle(self):
+        bundle = self.ThingsBundle('abc', 'def')
+
+        self.assertBundleOfKind(bundle, bundle_kind='Things')
+
+    def test_raises_on_missing_positional_arguments(self):
+        with self.assertRaises(ValueError) as raised:
+            self.ThingsBundle(['a'])
+        self.assertEqual(str(raised.exception), 'Error: too few arguments given (expected 2 got 1)')
+
+    def test_ensuring_an_existing_bundle_returns_it_unchanged(self):
+        existing_bundle = self.ThingsBundle('abc', 'def')
+
+        bundle = self.ThingsBundle.ensure_bundle(existing_bundle)
+
+        self.assertIs(bundle, existing_bundle)
+
+    def test_ensuring_an_list_of_arguments_returns_a_bundle(self):
+        bundle = self.ThingsBundle.ensure_bundle(['abc', 'def'])
+
+        self.assertBundleOfKind(bundle, bundle_kind='Things')
 
 
 class TestMigSharedArguments__getopt(MigTestCase):
