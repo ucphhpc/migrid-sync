25370948b3679678e21a54986c2a7460cd458835
Alex Burke <albu@di.ku.dk>
2024-12-12 10:48:50 +0100

    repair main

--- mig/services/oidc/__main__.py
+++ mig/services/oidc/__main__.py
@@ -1,9 +1,24 @@
-from mig.services.oidc.internals import OidcRequestValidator
+import sys
+
+from mig.shared.arguments import parse_getopt_args
+from mig.shared.conf import get_configuration_object
+from mig.services.oidc import create_service
 
 
-def main():
-    server = WebApplicationServer(OidcRequestValidator)
+def _required_option_error(option_argument, option_name):
+    return ValueError('Missing required option %s %s' % (option_argument, option_name.upper()))
+
+
+def main(args):
+    configuration = get_configuration_object(args.config_file)
+    service = create_service(configuration)
 
 
 if __name__ == '__main__':
-    main()
+    (opts, args) = parse_getopt_args(sys.argv[1:], 'c:')
+
+    if 'c' not in opts:
+        raise _required_option_error('-c', 'config_file')
+
+    configuration = get_configuration_object(opts.c)
+    service = create_service(configuration)
