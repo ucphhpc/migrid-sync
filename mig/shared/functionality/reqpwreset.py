80c9d1cab0a7a441a1d80f16b160266d4f078966
Jonas Bardino <bardino@science.ku.dk>
2025-02-12 17:16:45 +0100

    Update license header.

--- mig/shared/functionality/reqpwreset.py
+++ mig/shared/functionality/reqpwreset.py
@@ -4,7 +4,7 @@
 # --- BEGIN_HEADER ---
 #
 # reqpwreset - Account password reset request backend
-# Copyright (C) 2003-2023  The MiG Project lead by Brian Vinter
+# Copyright (C) 2003-2025  The MiG Project by the Science HPC Center at UCPH
 #
 # This file is part of MiG.
 #
