f8b8986960d6d93da95d7f0b5877fe30d71e69e0
Jonas Bardino <bardino@science.ku.dk>
2025-06-11 12:49:35 +0200

    Rework showvgridprivatefile to emulate a simplified version of cat.py to render html and txt files directly but force download of anything else.

--- mig/shared/output.py
+++ mig/shared/output.py
@@ -1292,9 +1292,12 @@ def html_format(configuration, ret_val, ret_msg, out_obj):
         elif i['object_type'] == 'file_output':
             if 'path' in i:
                 lines.append('File: %s<br />' % i['path'])
-            # NOTE: we shouldn't expect user file contents to be safe here
-            lines.append('<pre>%s</pre><br />' %
-                         html_escape(''.join(i['lines'])))
+            if i.get('verbatim', False):
+                lines.append(''.join(i['lines']))
+            else:
+                # NOTE: we shouldn't expect user file contents to be safe here
+                lines.append('<pre>%s</pre><br />' %
+                             html_escape(''.join(i['lines'])))
         elif i['object_type'] == 'list':
             lines.append('<ul>')
             for list_item in i['list']:
