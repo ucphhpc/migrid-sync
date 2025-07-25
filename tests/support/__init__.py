c73c5420e46b05e47fb1638c3199bea170647b09
Alex Burke <albu@di.ku.dk>
2025-07-25 15:13:48 +0200

    Lay groundwork for splitting runtime and static configuration data.
    This change paves the way adding further runtime specific data to
    configuration objects. An instance of a configuration already contains
    such data, such as the mig_server_id property when in use by daemons
    and a reference to the logger. With needs arising for further runtime
    data, make a clear separation between runtime attributes and the
    static configuration data clear.
    
    Opt to do this by making an object which internally keeps a reference
    to an underlying configuration object, proxies attribute access to it
    and can then be used as a drop-in substitute in ay call stack of logic.
    Name this RuntimeConfiguration.
    
    Stop short of moving existing runtime properties as of this commit.

--- tests/support/__init__.py
+++ tests/support/__init__.py
@@ -210,10 +210,12 @@ class MigTestCase(TestCase):
     @staticmethod
     def _make_configuration_instance(testcase, configuration_to_make):
         if configuration_to_make == 'fakeconfig':
-            return FakeConfiguration(logger=testcase.logger)
+            fake_configuration = FakeConfiguration(logger=testcase.logger)
+            from mig.shared.conf import RuntimeConfiguration
+            return RuntimeConfiguration(fake_configuration)
         elif configuration_to_make == 'testconfig':
             from mig.shared.conf import get_configuration_object
-            configuration = get_configuration_object(skip_log=True, 
+            configuration = get_configuration_object(skip_log=True,
                                                      disable_auth_log=True)
             configuration.logger = testcase.logger
             return configuration
