aad7aea5f65ae91f16408772911d5727ab03790c
Jonas Bardino <bardino@science.ku.dk>
2025-08-07 12:06:47 +0200

    Fix another case of C99 variable declaration like PR310.

--- mig/src/libnss-mig/libnss_mig.c
+++ mig/src/libnss-mig/libnss_mig.c
@@ -191,8 +191,9 @@ _nss_mig_getpwnam_r(const char *name,
     if (keep_trying && name_len == get_sharelink_length()) {
         WRITELOGMESSAGE(LOG_DEBUG, "Checking for sharelink: %s\n", name);
 
+        uint8_t i;
         char sharelink_modes[3][11] = { SHARELINK_RW_DIR, SHARELINK_RO_DIR, SHARELINK_WO_DIR };
-        for (uint8_t i = 0; i < 3; i++)
+        for (i = 0; i < 3; i++)
         {
 
         memset(pathbuf, 0, PATH_BUF_LEN);
