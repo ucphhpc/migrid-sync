c2c192f192adaea8482f12a85fde08fb94ba7476
Jonas Bardino <bardino@science.ku.dk>
2026-03-09 09:40:05 +0100

    Add comments to mark imports in line with review feedback. Removed a stale try/except wrap of imports to avoid autopep8 interference. We should not need it anymore.

--- tests/test_mig_shared_accountreq.py
+++ tests/test_mig_shared_accountreq.py
@@ -36,7 +36,6 @@ import unittest
 # Imports of the code under test
 import mig.shared.accountreq as accountreq
 # Imports required for the unit test wrapping
-
 from mig.shared.base import distinguished_name_to_user, fill_distinguished_name
 from mig.shared.defaults import keyword_auto
 # Imports required for the unit tests themselves
