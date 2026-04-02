1bd40d14b73781890ac6e4ec07cd307adff9fe30
Jonas Bardino <bardino@science.ku.dk>
2026-04-02 11:20:16 +0200

    Comment-only changes to trigger old linter errors seen elsewhere.

--- mig/server/scheduler.py
+++ mig/server/scheduler.py
@@ -4,7 +4,7 @@
 # --- BEGIN_HEADER ---
 #
 # scheduler - base scheduler class used by all schedulers
-# Copyright (C) 2003-2024  The MiG Project lead by Brian Vinter
+# Copyright (C) 2003-2026  The MiG Project by the Science HPC Center at UCPH
 #
 # This file is part of MiG.
 #
@@ -20,7 +20,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
 #
 # -- END_HEADER ---
 #
