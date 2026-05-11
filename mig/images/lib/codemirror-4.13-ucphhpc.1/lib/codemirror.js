5f4d5254b4b45b73e5beeb3e739ee0255c2b2d07
Jonas Bardino <bardino@science.ku.dk>
2026-05-11 13:00:15 +0200

    Begin the switch to a patched version of the CodeMirror 4.13 series to address various security issues with the source code there identified e.g. in code scans. Mark the folder accordingly with a README.

--- mig/images/lib/codemirror-4.13-ucphhpc.1/lib/codemirror.js
+++ mig/images/lib/codemirror-4.13-ucphhpc.1/lib/codemirror.js
@@ -5976,7 +5976,7 @@
       var prop = lineClass[1] ? "bgClass" : "textClass";
       if (output[prop] == null)
         output[prop] = lineClass[2];
-      else if (!(new RegExp("(?:^|\s)" + lineClass[2] + "(?:$|\s)")).test(output[prop]))
+      else if (!(new RegExp("(?:^|\\s)" + lineClass[2] + "(?:$|\\s)")).test(output[prop]))
         output[prop] += " " + lineClass[2];
     }
     return type;
