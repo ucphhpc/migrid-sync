2506502af1ca2a44db90841740a89772118f7566
Jonas Bardino <bardino@science.ku.dk>
2026-05-12 15:14:21 +0200

    Adjust xml/html comment end marker detection to support not only `-->` but also `--!>` as code scan suggested.

--- mig/images/lib/codemirror-4.13-ucphhpc.1/mode/xml/xml.js
+++ mig/images/lib/codemirror-4.13-ucphhpc.1/mode/xml/xml.js
@@ -76,7 +76,7 @@ CodeMirror.defineMode("xml", function(config, parserConfig) {
           if (stream.match("CDATA[")) return chain(inBlock("atom", "]]>"));
           else return null;
         } else if (stream.match("--")) {
-          return chain(inBlock("comment", "-->"));
+          return (chain(inBlock("comment", "-->")) || chain(inBlock("comment", "--!>")));
         } else if (stream.match("DOCTYPE", true, true)) {
           stream.eatWhile(/[\w\._\-]/);
           return chain(doctype(1));
