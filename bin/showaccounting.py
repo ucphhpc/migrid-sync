f099e2f33bea5faf1b74fa71a98a5e61a1eaef05
Martin Rehr <rehr@nbi.ku.dk>
2026-05-13 16:23:26 +0200

    Added support for: * Specifying number of formatting decimals * Formatting numbers due to local settings * SI byte units * CSV output format

--- bin/showaccounting.py
+++ bin/showaccounting.py
@@ -31,55 +31,94 @@ from __future__ import absolute_import, print_function
 
 import datetime
 import getopt
+import locale
+import math
+import os
 import re
 import sys
 
 from mig.lib.accounting import get_usage, human_readable_filesize
+from mig.shared.base import distinguished_name_to_user
 from mig.shared.conf import get_configuration_object
-from mig.shared.defaults import gdp_distinguished_field
+from mig.shared.defaults import gdp_distinguished_field, user_db_filename
+from mig.shared.useradm import load_user_db
 
 
-def usage(name='showaccounting.py'):
+def usage(name="showaccounting.py"):
     """Usage help"""
 
-    print("""Create accounting information based on quota.
+    print(
+        """Create accounting information based on quota.
 Usage:
 %(name)s [ACCOUNTING_OPTIONS]
 Where ACCOUNTING_OPTIONS may be one or more of:
    -h                  Show this help
    -v                  Verbose output
+   -d                  Number of decimals (default 3)
+   -i                  Show bytes in SI units (power of 10)
+   -s                  Summary in CVS format
    -c CONF_FILE        Use CONF_FILE as server configuration
    -f User filter      Regex user (CERT_DN) filter
-   -m Minimum usage    Only show accounts using more than
-                       minimum usage (TB).
+   -l locale_format    Use specific locale format (locale -a)
+   -u usage filter     Only show accounts using more than
+                       minimum usage (TiB)
    -t TIMESTAMP        Use specific timestamp, latest if unset
-""" % {'name': name})
+"""
+        % {"name": name}
+    )
 
 
-def show_accounting(configuration,
-                    timestamp,
-                    user_filter,
-                    minimum_usage,
-                    verbose):
+def show_accounting(
+    configuration,
+    timestamp,
+    user_filter,
+    minimum_usage,
+    si_byte_format,
+    decimals,
+    csv_summary,
+    verbose,
+):
     """Print user accounting report"""
     user_filter_re = None
     if user_filter:
         try:
             user_filter_re = re.compile(user_filter)
         except Exception as err:
-            print("ERROR: Failed to compile user_filter: %r error: %s"
-                  % (user_filter, err))
+            print(
+                "ERROR: Failed to compile user_filter: %r error: %s"
+                % (user_filter, err)
+            )
             return
 
-    usage = get_usage(configuration,
-                      timestamp=timestamp,
-                      verbose=verbose)
+    account_usage = get_usage(
+        configuration,
+        timestamp=timestamp,
+        decimals=decimals,
+        si_byte_format=si_byte_format,
+        verbose=verbose,
+    )
+    if account_usage is None:
+        print("ERROR: Missing account usage data")
+        return
 
-    accounting = usage.get('accounting', {})
-    accounting_timestamp = usage.get('timestamp', 0)
-    accounting_datestr \
-        = datetime.datetime.fromtimestamp(accounting_timestamp) \
-        .strftime('%d/%m/%Y-%H:%M:%S')
+    # Load userdb
+    user_db_filepath = os.path.join(
+        configuration.user_db_home, user_db_filename
+    )
+    try:
+        user_db = load_user_db(user_db_filepath)
+    except Exception as err:
+        print(
+            "WARNING: Failed to load user_db from: %r, error: %s"
+            % (user_db_filepath, err)
+        )
+        user_db = {}
+
+    accounting = account_usage.get("accounting", {})
+    accounting_timestamp = account_usage.get("timestamp", 0)
+    accounting_datestr = datetime.datetime.fromtimestamp(
+        accounting_timestamp
+    ).strftime("%d/%m/%Y-%H:%M:%S")
 
     # Sorted by total bytes and print usage for users
 
@@ -88,106 +127,198 @@ def show_accounting(configuration,
     report_total_bytes = 0
     report_shown_bytes = 0
     total_bytes_map = {}
-    for username, values in accounting.items():
+    for userid, values in accounting.items():
         # Do not show GDP project users
         # projects are accounted for by the main user
-        if configuration.site_enable_gdp \
-                and username.find("/%s=" % gdp_distinguished_field) != -1:
+        if (
+            configuration.site_enable_gdp
+            and userid.find("/%s=" % gdp_distinguished_field) != -1
+        ):
             continue
         report_total_users += 1
-        total_bytes = values.get('total_bytes', 0)
+        total_bytes = values.get("total_bytes", 0)
         report_total_bytes += total_bytes
-        if total_bytes < minimum_usage \
-                or user_filter_re and not user_filter_re.fullmatch(username):
+        if (
+            total_bytes < minimum_usage
+            or (user_filter_re
+            and not user_filter_re.fullmatch(userid))
+        ):
             continue
         report_shown_users += 1
         report_shown_bytes += total_bytes
         total_bytes_map_userlist = total_bytes_map.get(total_bytes, [])
-        total_bytes_map_userlist.append(username)
+        total_bytes_map_userlist.append(userid)
         total_bytes_map[total_bytes] = total_bytes_map_userlist
     sorted_total_bytes = sorted(list(total_bytes_map), reverse=True)
 
-    print("\nAccounting (%d) %s for storage quota(s):"
-          % (accounting_timestamp, accounting_datestr))
-    for quota_fs, values in usage.get('quota', {}).items():
-        quota_mtime = values.get('mtime', 0)
-        quota_datestr = datetime.datetime.fromtimestamp(quota_mtime) \
-            .strftime('%d/%m/%Y-%H:%M:%S')
-        print(" - %s (%d) %s" % (quota_fs,
-                                 quota_mtime,
-                                 quota_datestr))
+    if csv_summary:
+        for total_bytes in sorted_total_bytes:
+            for userid in total_bytes_map[total_bytes]:
+                user_db_ent = user_db.get(userid, {})
+                report = accounting[userid]
+                user_total = report.get("total_bytes", 0)
+                faculty = user_db_ent.get("faculty", "")
+                institute = user_db_ent.get("institute", "")
+                username = user_db_ent.get("full_name", "")
+                email = user_db_ent.get("email", "")
+                if si_byte_format:
+                    user_total_tb = locale.format_string(
+                        "%.*f", (decimals, user_total * 1.0 / 1000**4)
+                    )
+                else:
+                    user_total_tb = locale.format_string(
+                        "%.*f", (decimals, user_total * 1.0 / 1024**4)
+                    )
+                print(
+                    "%(faculty)s;%(institute)s;%(username)s;%(email)s;%(user_total)s;%(user_total_tb)s"
+                    % {
+                        "faculty": faculty,
+                        "institute": institute,
+                        "username": username,
+                        "email": email,
+                        "user_total": user_total,
+                        "user_total_tb": user_total_tb,
+                    }
+                )
 
-    print("Found a total of %s users using %s storage"
-          % (report_total_users,
-             human_readable_filesize(report_total_bytes)))
-    print("Showing details for %s users using %s storage "
-          % (report_shown_users,
-             human_readable_filesize(report_shown_bytes)))
-    print("User filter: %r" % user_filter)
-    print("Minimum usage: %s" % human_readable_filesize(minimum_usage))
-    for total_bytes in sorted_total_bytes:
-        total_bytes_human = human_readable_filesize(total_bytes)
-        for username in total_bytes_map[total_bytes]:
-            report = accounting[username]
-            home_report = report.get('home_report', '')
-            freeze_report = report.get('freeze_report', '')
-            vgrid_report = report.get('vgrid_report', '')
-            ext_users_report = report.get('ext_users_report', '')
-            peers_report = report.get('peers_report', '')
-            print("\n%s:" % username)
-            print("Total usage: %s" % total_bytes_human)
-            if home_report:
-                print(home_report)
-            if freeze_report:
-                print(freeze_report)
-            if vgrid_report:
-                print(vgrid_report)
-            if ext_users_report:
-                print(ext_users_report)
-            if peers_report:
-                print(peers_report)
+    else:
+        print(
+            "\nAccounting (%d) %s for storage quota(s):"
+            % (accounting_timestamp, accounting_datestr)
+        )
+        for quota_fs, values in account_usage.get("quota", {}).items():
+            quota_mtime = values.get("mtime", 0)
+            quota_datestr = datetime.datetime.fromtimestamp(
+                quota_mtime
+            ).strftime("%d/%m/%Y-%H:%M:%S")
+            print(" - %s (%d) %s" % (quota_fs, quota_mtime, quota_datestr))
+
+        print(
+            "Found a total of %s users using %s storage"
+            % (
+                report_total_users,
+                human_readable_filesize(report_total_bytes, decimals=decimals),
+            )
+        )
+        print(
+            "Showing details for %s users using %s storage"
+            % (
+                report_shown_users,
+                human_readable_filesize(report_shown_bytes, decimals=decimals),
+            )
+        )
+        print("User filter: %r" % user_filter)
+        print(
+            "Minimum usage: %s"
+            % human_readable_filesize(minimum_usage, decimals=decimals)
+        )
+        for total_bytes in sorted_total_bytes:
+            for userid in total_bytes_map[total_bytes]:
+                user_dict = distinguished_name_to_user(userid)
+                user_db_ent = user_db.get(userid, {})
+                report = accounting[userid]
+                total_report = report.get("total_report", "")
+                home_report = report.get("home_report", "")
+                freeze_report = report.get("freeze_report", "")
+                vgrid_report = report.get("vgrid_report", "")
+                ext_users_report = report.get("ext_users_report", "")
+                peers_report = report.get("peers_report", "")
+                print(
+                    "\n-------------------------------------------------------------------------------"
+                )
+                if verbose:
+                    print("%s:" % userid)
+                print("Username: %s" % user_dict.get("full_name", ""))
+                print("Email: %s" % user_dict.get("email", ""))
+                print("Organization: %s" % user_db_ent.get("organization", ""))
+                print("Faculty: %s" % user_db_ent.get("faculty", ""))
+                print("Institute: %s" % user_db_ent.get("institute", ""))
+                print(
+                    "Expire: %s"
+                    % datetime.datetime.fromtimestamp(
+                        user_db_ent.get("expire", 0)
+                    ).strftime("%d/%m/%Y-%H:%M:%S")
+                )
+                if total_report:
+                    print(total_report)
+                if home_report:
+                    print(home_report)
+                if freeze_report:
+                    print(freeze_report)
+                if vgrid_report:
+                    print(vgrid_report)
+                if ext_users_report:
+                    print(ext_users_report)
+                if peers_report:
+                    print(peers_report)
 
 
-if '__main__' == __name__:
+if "__main__" == __name__:
     conf_path = None
     user_filter = None
+    si_byte_format = False
+    csv_summary = False
+    decimals = 3
     timestamp = 0
+    locale_format = None
     minimum_usage = 0
     verbose = False
-    opt_args = 'hvc:f:m:t:'
+    opt_args = "hvsic:d:f:l:u:t:"
     try:
         (opts, args) = getopt.getopt(sys.argv[1:], opt_args)
-        for (opt, val) in opts:
-            if opt == '-h':
+        for opt, val in opts:
+            if opt == "-h":
                 usage()
                 sys.exit(0)
-            if opt == '-v':
+            elif opt == "-v":
                 verbose = True
-            elif opt == '-c':
+            elif opt == "-s":
+                csv_summary = True
+            elif opt == "-i":
+                si_byte_format = True
+            elif opt == "-c":
                 conf_path = val
-            elif opt == '-f':
+            elif opt == "-d":
+                decimals = int(val)
+            elif opt == "-f":
                 user_filter = val
-            elif opt == '-m':
-                minimum_usage = float(val)*(1024**4)
-            elif opt == '-t':
+            elif opt == "-l":
+                locale_format = val
+            elif opt == "-u":
+                minimum_usage = math.ceil(float(val) * (1024**4))
+            elif opt == "-t":
                 timestamp = int(val)
             else:
-                print('Error: %s not supported!' % opt)
+                print("Error: %s not supported!" % opt)
                 usage()
                 sys.exit(1)
     except getopt.GetoptError as err:
-        print('Error: ', err.msg)
+        print("Error: %s" % err)
         usage()
         sys.exit(1)
 
-    configuration = get_configuration_object(config_file=conf_path,
-                                             skip_log=True,
-                                             disable_auth_log=True)
+    try:
+        if locale_format:
+            locale.setlocale(locale.LC_ALL, locale_format)
+    except Exception as err:
+        print(
+            "Failed to set locale format: %r, error: %s" % (locale_format, err)
+        )
+        sys.exit(1)
 
-    show_accounting(configuration,
-                    timestamp,
-                    user_filter,
-                    minimum_usage,
-                    verbose)
+    configuration = get_configuration_object(
+        config_file=conf_path, skip_log=True, disable_auth_log=True
+    )
+
+    show_accounting(
+        configuration,
+        timestamp,
+        user_filter,
+        minimum_usage,
+        si_byte_format,
+        decimals,
+        csv_summary,
+        verbose,
+    )
 
     sys.exit(0)
