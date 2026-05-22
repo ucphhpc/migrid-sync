cf6b8759a5c6a272445ac789469352f88bd17109
Jonas Bardino <bardino@science.ku.dk>
2026-05-22 16:11:15 +0200

    Wrap Apache curve selection in version conditional like for dhparams to make sure it is possible. Rework cipher and curve selection in tlsserver module to use AUTO keyword by default with automatic selection of strongest supported values. Acknowledge that Python is not there yet regarding PQC support in the native ssl module or pyopenssl. In effect limit PQC to Apache and only include TODOs about extending support to Python later (3.15+). Minor polish to clarify and sync structure between ssl and pyopenssl hardened context helpers.

--- mig/shared/tlsserver.py
+++ mig/shared/tlsserver.py
@@ -35,19 +35,27 @@ from __future__ import absolute_import
 import ssl
 import sys
 
-from mig.shared.defaults import STRONG_TLS_CIPHERS, STRONG_TLS_CURVES
+from mig.shared.defaults import keyword_auto, STRONG_TLS_CIPHERS, \
+    STRONG_TLS_LEGACY_CURVES
 
 
 def hardened_ssl_context(configuration, keyfile, certfile, dhparamsfile=None,
-                         ciphers=STRONG_TLS_CIPHERS,
-                         curve_priority=STRONG_TLS_CURVES,
-                         allow_pre_tlsv13=True,
-                         allow_renegotiation=False,
+                         ciphers=keyword_auto, curve_priority=keyword_auto,
+                         allow_pre_tlsv13=True, allow_renegotiation=False,
                          ):
     """Build and return a hardened native SSL context to apply to a socket"""
     _logger = configuration.logger
+    # NOTE: auto select best ciphers and curves unless specificly requested
+    if ciphers is keyword_auto:
+        _logger.debug("Auto select strong ciphers")
+        ciphers = STRONG_TLS_CIPHERS
+    if curve_priority is keyword_auto:
+        # TODO: switch to STRONG_TLS_CURVES once Python gains support (3.15+)
+        _logger.debug("Auto select strong legacy TLS curves without PQC")
+        curve_priority = STRONG_TLS_LEGACY_CURVES
     _logger.info("enforcing strong SSL/TLS connections")
     _logger.debug("using SSL/TLS ciphers: %s" % ciphers)
+    _logger.debug("using SSL/TLS curves: %s" % curve_priority)
     ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     ssl_ctx.minimum_version = ssl.TLSVersion.TLSv1_2
     ssl_ctx.load_cert_chain(certfile, keyfile)
@@ -80,14 +88,14 @@ def hardened_ssl_context(configuration, keyfile, certfile, dhparamsfile=None,
         ssl_ctx.options |= ssl_options
     else:
         _logger.info("can't enforce strong SSL/TLS options")
-        _logger.warning("Upgrade to python 3.7+ for maximum security")
+        _logger.warning("Upgrade to at least python 3.7+ for best security")
 
     pfs_available = False
     if dhparamsfile:
         try:
             ssl_ctx.load_dh_params(dhparamsfile)
             pfs_available = True
-        except Exception as exc:
+        except Exception:
             _logger.warning("Could not load optional dhparams from %s" %
                             dhparamsfile)
             _logger.info("""You can create a suitable dhparams file with:
@@ -96,10 +104,10 @@ openssl dhparam 2048 -out %s""" % dhparamsfile)
     # We must explicitly set curve here to actually enable ciphers
     # using them. They can provide Perfect Forward Secrecy.
     # http://stackoverflow.com/questions/32094145/python-paste-ssl-server-with-tlsv1-2-and-forward-secrecy#32101078
-    # Some help for installing pyopenssl with EC support at
-    # http://stackoverflow.com/questions/7340784/easy-install-pyopenssl-error/34048924#34048924
+    # NOTE: PQC curves / KEMs like x25519mlkem768 are NOT supported here
+    # TODO: use ssl_ctx.set_groups(curves) for PQC once Python 3.15+ lands
+    activated_curve = None
     if curve_priority:
-        activated_curve = None
         for curve_name in curve_priority.split(':'):
             try:
                 _logger.debug("Blindly trying elliptic curve %s" % curve_name)
@@ -110,8 +118,9 @@ openssl dhparam 2048 -out %s""" % dhparamsfile)
             except Exception as exc:
                 _logger.warning("Couldn't init elliptic curve %s: %s" %
                                 (curve_name, exc))
-        if not activated_curve:
-            _logger.info("""You need a recent openssl built with elliptic
+
+    if not activated_curve:
+        _logger.info("""You need a modern openssl built with elliptic
 curves to take advantage of this optional improved security feature""")
 
     if not pfs_available:
@@ -124,16 +133,23 @@ dhparams nor elliptic curves available.""")
 
 def hardened_openssl_context(configuration, OpenSSL, keyfile, certfile,
                              cacertfile=None, dhparamsfile=None,
-                             ciphers=STRONG_TLS_CIPHERS,
-                             curve_priority=STRONG_TLS_CURVES,
-                             allow_pre_tlsv13=True,
-                             allow_renegotiation=False,
+                             ciphers=keyword_auto, curve_priority=keyword_auto,
+                             allow_pre_tlsv13=True, allow_renegotiation=False,
                              ):
     """Build and return a hardened OpenSSL context to apply to a socket"""
     _logger = configuration.logger
+    # NOTE: auto select best ciphers and curves unless specificly requested
+    if ciphers is keyword_auto:
+        _logger.debug("Auto select strong ciphers")
+        ciphers = STRONG_TLS_CIPHERS
+    if curve_priority is keyword_auto:
+        # TODO: switch to STRONG_TLS_CURVES once PyOpenSSL gains support
+        _logger.debug("Auto select strong legacy TLS curves without PQC")
+        curve_priority = STRONG_TLS_LEGACY_CURVES
     SSL, crypto = OpenSSL.SSL, OpenSSL.crypto
     _logger.info("enforcing strong SSL/TLS connections")
     _logger.debug("using SSL/TLS ciphers: %s" % ciphers)
+    _logger.debug("using SSL/TLS curves: %s" % curve_priority)
     ssl_ctx = SSL.Context(SSL.TLS_SERVER_METHOD)
     ssl_ctx.set_min_proto_version(SSL.TLS1_2_VERSION)
     # Mimic native ssl exposure of options
@@ -179,7 +195,7 @@ def hardened_openssl_context(configuration, OpenSSL, keyfile, certfile,
         ssl_ctx._options = ssl_options
     else:
         _logger.info("can't enforce strong SSL/TLS options")
-        _logger.warning("Upgrade to python 3.7+ for maximum security")
+        _logger.warning("Upgrade to at least python 3.7+ for best security")
         # Mimic native ssl exposure of options
         ssl_ctx._options = None
 
@@ -188,7 +204,7 @@ def hardened_openssl_context(configuration, OpenSSL, keyfile, certfile,
         try:
             ssl_ctx.load_tmp_dh(dhparamsfile)
             pfs_available = True
-        except Exception as exc:
+        except Exception:
             _logger.warning("Could not load optional dhparams from %s" %
                             dhparamsfile)
             _logger.info("""You can create a suitable dhparams file with:
@@ -199,6 +215,9 @@ openssl dhparam 2048 -out %s""" % dhparamsfile)
     # http://stackoverflow.com/questions/32094145/python-paste-ssl-server-with-tlsv1-2-and-forward-secrecy#32101078
     # Some help for installing pyopenssl with EC support at
     # http://stackoverflow.com/questions/7340784/easy-install-pyopenssl-error/34048924#34048924
+    # NOTE: PQC curves / KEMs like x25519mlkem768 are NOT supported here
+    # TODO: mimic ssl_ctx.set_groups(curves) for PQC once Python 3.15+ lands
+    activated_curve = None
     if curve_priority:
         try:
             # Returns a python set of curves to grab best one from
@@ -207,6 +226,7 @@ openssl dhparam 2048 -out %s""" % dhparamsfile)
             for curve_name in curve_priority.split(':'):
                 if curve_name in curve_map:
                     use_curve = curve_map[curve_name]
+                    activated_curve = curve_name
                     break
             _logger.debug("Found elliptic curves %s and picked %s" %
                           (', '.join(list(curve_map)), use_curve.name))
@@ -215,7 +235,9 @@ openssl dhparam 2048 -out %s""" % dhparamsfile)
         except Exception as exc:
             _logger.warning("Couldn't init elliptic curve ciphers: %s" %
                             exc)
-            _logger.info("""You need a recent pyopenssl built with elliptic
+
+    if not activated_curve:
+        _logger.info("""You need a modern (py)openssl built with elliptic
 curves to take advantage of this optional improved security feature""")
 
     if not pfs_available:
