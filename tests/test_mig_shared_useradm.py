58f1c0b03e6deb3bd3b0ea4fe9b1028bd57236a5
Jonas Bardino <bardino@science.ku.dk>
2026-05-28 14:05:37 +0200

    Add duplicate unit tests to cover create_user with the UUID user ID as a start. Bail out in create_user if UUID format is chosen and user doesn't have the corresponding unique_id field.

--- tests/test_mig_shared_useradm.py
+++ tests/test_mig_shared_useradm.py
@@ -38,7 +38,7 @@ from past.builtins import basestring
 # Imports required for the unit test wrapping
 from mig.shared.base import client_id_dir, distinguished_name_to_user
 from mig.shared.defaults import (
-    DEFAULT_USER_ID_FORMAT,
+    DEFAULT_USER_ID_FORMAT, UUID_USER_ID_FORMAT,
     htaccess_filename,
     keyword_auto,
     ssh_conf_dir,
@@ -148,6 +148,7 @@ class TestMigSharedUseradm__create_user(
 
     def before_each(self):
         configuration = self.configuration
+        configuration.site_user_id_format = DEFAULT_USER_ID_FORMAT
 
         _ensure_dirs_needed_for_userdb(self.configuration)
 
@@ -409,6 +410,225 @@ class TestMigSharedUseradm__create_user(
                 user_dict, self.configuration, keyword_auto, default_renew=True
             )
         except Exception:
+            self.assertFalse(True, "should not be reached")
+
+        # NOTE: reset distinguished_name and introduce an ID conflict to test
+        del user_dict["distinguished_name"]
+        user_dict["organization"] = "Another Org"
+        with self.assertRaises(Exception):
+            create_user(
+                user_dict,
+                self.configuration,
+                keyword_auto,
+                default_renew=True,
+                ask_renew=False,
+            )
+
+
+# TODO: consider merging the tests for UUID and X509 format
+class TestMigSharedUseradm__create_user_uuid_user_id(
+    MigTestCase, FixtureAssertMixin, PickleAssertMixin
+):
+    """Coverage of useradm create_user function with UUID format."""
+
+    TEST_USER_DN_GDP = "%s/GDP" % (TEST_USER_DN,)
+    TEST_USER_PASSWORD_HASH = "PBKDF2$sha256$10000$XMZGaar/pU4PvWDr$w0dYjezF6JGtSiYPexyZMt3lM2134uix"
+
+    def before_each(self):
+        configuration = self.configuration
+        configuration.site_user_id_format = UUID_USER_ID_FORMAT
+
+        _ensure_dirs_needed_for_userdb(self.configuration)
+
+        self.expected_user_db_home = os.path.normpath(
+            configuration.user_db_home
+        )
+        self.expected_user_db_file = os.path.join(
+            self.expected_user_db_home, "MiG-users.db"
+        )
+        ensure_dirs_exist(self.configuration.mig_system_files)
+
+    def _provide_configuration(self):
+        return "testconfig"
+
+    def test_user_db_is_created(self):
+        user_dict = {}
+        user_dict["unique_id"] = "UniqueUserIdForTestUser"
+        user_dict["full_name"] = "Test User"
+        user_dict["organization"] = "Test Org"
+        user_dict["state"] = "NA"
+        user_dict["country"] = "DK"
+        user_dict["email"] = "user@example.com"
+        user_dict["comment"] = "This is the create comment"
+        user_dict["password"] = "password"
+        create_user(
+            user_dict, self.configuration, keyword_auto, default_renew=True
+        )
+
+        # presence of user home
+        path_kind = MigTestCase._absolute_path_kind(self.expected_user_db_home)
+        self.assertEqual(path_kind, "dir")
+
+        # presence of user db
+        path_kind = MigTestCase._absolute_path_kind(self.expected_user_db_file)
+        self.assertEqual(path_kind, "file")
+
+    def test_user_creation_records_a_user(self):
+        def _adjust_user_dict_for_compare(user_obj):
+            obj = dict(user_obj)
+            obj["created"] = 9999999999.9999999
+            obj["expire"] = 9999999999.9999999
+            obj["unique_id"] = "__UNIQUE_ID__"
+            return obj
+
+        expected_user_id = TEST_USER_DN
+        expected_user_password_hash = self.TEST_USER_PASSWORD_HASH
+
+        user_dict = {}
+        user_dict["unique_id"] = "UniqueUserIdForTestUser"
+        user_dict["full_name"] = "Test User"
+        user_dict["organization"] = "Test Org"
+        user_dict["state"] = "NA"
+        user_dict["country"] = "DK"
+        user_dict["email"] = "test@example.com"
+        user_dict["comment"] = "This is the create comment"
+        user_dict["locality"] = ""
+        user_dict["organizational_unit"] = ""
+        user_dict["password"] = ""
+        user_dict["password_hash"] = expected_user_password_hash
+
+        create_user(
+            user_dict, self.configuration, keyword_auto, default_renew=True
+        )
+
+        pickled = self.assertPickledFile(
+            self.expected_user_db_file,
+            apply_hints=["convert_dict_bytes_to_strings_kv"],
+        )
+        self.assertIn(expected_user_id, pickled)
+
+        prepared = self.prepareFixtureAssert(
+            "MiG-users.db--example", fixture_format="json"
+        )
+
+        # TODO: remove resetting the handful of keys here
+        #       this is done to allow the comparision to succeed
+        actual_user_object = _adjust_user_dict_for_compare(
+            pickled[expected_user_id]
+        )
+        expected_user_object = _adjust_user_dict_for_compare(
+            prepared.fixture_data[expected_user_id]
+        )
+
+        self.maxDiff = None
+        self.assertEqual(actual_user_object, expected_user_object)
+
+    def test_user_creation_records_a_user_with_gdp(self):
+        self.configuration.site_enable_gdp = True
+
+        user_dict = {}
+        user_dict["unique_id"] = "UniqueUserIdForTestUser"
+        user_dict["full_name"] = "Test User"
+        user_dict["organization"] = "Test Org"
+        user_dict["state"] = "NA"
+        user_dict["country"] = "DK"
+        user_dict["email"] = "test@example.com"
+        user_dict["comment"] = "This is the create comment"
+        user_dict["locality"] = ""
+        user_dict["organizational_unit"] = ""
+        user_dict["password"] = ""
+        user_dict["password_hash"] = self.TEST_USER_PASSWORD_HASH
+        # explicitly setting set a DN suffixed user DN to force GDP
+        user_dict["distinguished_name"] = self.TEST_USER_DN_GDP
+
+        try:
+            create_user(
+                user_dict, self.configuration, keyword_auto, default_renew=True
+            )
+        except:
+            self.assertFalse(True, "should not be reached")
+
+    def test_user_creation_and_renew_records_a_user(self):
+        user_dict = {}
+        user_dict["unique_id"] = "UniqueUserIdForTestUser"
+        user_dict["full_name"] = "Test User"
+        user_dict["organization"] = "Test Org"
+        user_dict["state"] = "NA"
+        user_dict["country"] = "DK"
+        user_dict["email"] = "test@example.com"
+        user_dict["comment"] = "This is the create comment"
+        user_dict["locality"] = ""
+        user_dict["organizational_unit"] = ""
+        user_dict["password"] = ""
+        user_dict["password_hash"] = self.TEST_USER_PASSWORD_HASH
+
+        try:
+            create_user(
+                user_dict,
+                self.configuration,
+                keyword_auto,
+                default_renew=True,
+                ask_renew=False,
+            )
+        except:
+            self.assertFalse(True, "should not be reached")
+
+        try:
+            create_user(
+                user_dict,
+                self.configuration,
+                keyword_auto,
+                default_renew=True,
+                ask_renew=False,
+            )
+        except:
+            self.assertFalse(True, "should not be reached")
+
+    def test_user_creation_fails_in_renew_when_locked(self):
+        user_dict = {}
+        user_dict["unique_id"] = "UniqueUserIdForTestUser"
+        user_dict["full_name"] = "Test User"
+        user_dict["organization"] = "Test Org"
+        user_dict["state"] = "NA"
+        user_dict["country"] = "DK"
+        user_dict["email"] = "test@example.com"
+        user_dict["comment"] = "This is the create comment"
+        user_dict["locality"] = ""
+        user_dict["organizational_unit"] = ""
+        user_dict["password"] = ""
+        user_dict["password_hash"] = self.TEST_USER_PASSWORD_HASH
+        # explicitly setting set a DN suffixed user DN to force GDP
+        user_dict["distinguished_name"] = self.TEST_USER_DN_GDP
+        user_dict["status"] = "locked"
+
+        try:
+            create_user(
+                user_dict,
+                self.configuration,
+                keyword_auto,
+                default_renew=True,
+                ask_renew=False,
+            )
+        except:
+            self.assertFalse(True, "should not be reached")
+
+    def test_user_creation_with_id_collission_fails(self):
+        user_dict = {}
+        user_dict["unique_id"] = "UniqueUserIdForTestUser"
+        user_dict["full_name"] = "Test User"
+        user_dict["organization"] = "Test Org"
+        user_dict["state"] = "NA"
+        user_dict["country"] = "DK"
+        user_dict["email"] = "user@example.com"
+        user_dict["comment"] = "This is the create comment"
+        user_dict["password"] = "password"
+        user_dict["distinguished_name"] = TEST_USER_DN
+
+        try:
+            create_user(
+                user_dict, self.configuration, keyword_auto, default_renew=True
+            )
+        except:
             self.assertFalse(True, "should not be reached")
 
         # NOTE: reset distinguished_name and introduce an ID conflict to test
@@ -1091,7 +1311,6 @@ class TestMigSharedUseradm__get_any_oid_user_dn(
                                      user_check=False, do_lock=True
                                      )
         self.assertEqual(result, raw_login)
-
 
 if __name__ == "__main__":
     testmain()
