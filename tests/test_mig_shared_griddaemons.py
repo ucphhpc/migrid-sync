2b18634056fbcf9aa58b1be9f4e6fff5dea9c097
Jonas Bardino <bardino@science.ku.dk>
2026-06-10 14:20:50 +0200

    Minor clean up.

--- tests/test_mig_shared_griddaemons.py
+++ tests/test_mig_shared_griddaemons.py
@@ -3,7 +3,7 @@
 #
 # --- BEGIN_HEADER ---
 #
-# test_mig_shared_griddaemons - unit tests for the griddaemons login helpers
+# test_mig_shared_griddaemons - unit tests for the griddaemons helpers
 # Copyright (C) 2003-2026  The MiG Project by the Science HPC Center at UCPH
 #
 # This file is part of MiG.
@@ -25,7 +25,7 @@
 # --- END_HEADER ---
 #
 
-"""Unit tests for the griddaemons login helper functions"""
+"""Unit tests for the griddaemons helper functions"""
 
 import os
 import time
@@ -48,6 +48,7 @@ from tests.support import (
 )
 
 TEST_USER_SHORT_ID = "abc123@some.org"
+TEST_SHARE_ID = 'abcdef1234'
 
 # NOTE: this is a sample valid but unused ssh public key as it must be parsable
 TEST_USER_PUB_KEY = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCuJrICshi7S2KhV03qvgNVOx5ejmHsswdGbvR34wf+eN23Ghq6OZhwGye2S+J6LPVFI3p4SCqxX4URnUM8BRAsiuvbf/+GQfE2pAO0C+/g4V3hhYbYzIyrtPsP1Hl8GioxvZD5nDoLEA4TWokDC4D7SRfv+NEkFLplyVBwHtpUunXBS/zXYdQ4lgk7u8HBBCMqUGbZHfCc+6ibFVn/5WS6vVokL+fSWtxg9tUVWqsS/xtDGPH1wbJUf1Dm3D58KmdX8ca73tBoScwH8qUQwEcyM1JtWtbv1BAZFb+Qk6SEe4GPRsn3I4AAgC7xtU3HKQsiqe8Fzpick/uI5PU+vguitcV/9+AASnGVZJ9M+a63UvlFFloEYcI1LwdZ03JYPQXfXCzJSYiA+pTX4/cf10G4rlxsque4m4OcuCwLKvpTWA/Lla+UJqYhdQW+m7mSizPRoDPgh8mOta1PQob2sGSw8rhqLfApptAPZ0mkN0QY3Dv3i3ItgpYGcPNVXVdjmhU='
@@ -99,7 +100,7 @@ class MigSharedGriddaemonsLogin(MigTestCase):
         past_timestamp = time.time() - 3600
         dummy_user = Login(
             configuration=self.configuration,
-            username='dummy',
+            username=TEST_USER_SHORT_ID,
             home='dummy_home',
             password=TEST_USER_PW_HASH,
             digest=None,
@@ -133,9 +134,6 @@ class MigSharedGriddaemonsLogin(MigTestCase):
         # Set the file modification times to a time in the past
         current_time = time.time()
         past_timestamp = time.time() - 3600
-        # os.utime(self.auth_keys_path, (past_timestamp, past_timestamp))
-        # os.utime(self.auth_passwords_path, (past_timestamp, past_timestamp))
-        # os.utime(self.auth_digests_path, (past_timestamp, past_timestamp))
 
         # Create a dummy user with last_update matching the file mtime
         dummy_user = Login(
@@ -168,21 +166,20 @@ class MigSharedGriddaemonsLogin(MigTestCase):
         daemon_conf = self.configuration.daemon_conf
         daemon_conf['allow_publickey'] = True
 
-        share_id = 'Abcdef1234'
         user_shared_dir = os.path.join(self.configuration.user_home,
                                        'TestUser', 'shared', 'data')
         ensure_dirs_exist(user_shared_dir)
         user_shared_keys = os.path.join(user_shared_dir, '.ssh',
                                         'authorized_keys')
         share_link_path = os.path.join(self.configuration.sharelink_home,
-                                       share_id)
+                                       TEST_SHARE_ID)
         os.symlink(user_shared_dir, share_link_path)
 
         # Create a dummy share with a last_update in the past
         past_timestamp = time.time() - 3600
         dummy_share = Login(
             configuration=self.configuration,
-            username=share_id,
+            username=TEST_SHARE_ID,
             home='share_home',
             password=TEST_USER_PW_HASH,
             digest=None,
@@ -197,7 +194,7 @@ class MigSharedGriddaemonsLogin(MigTestCase):
 
         changed_paths = get_share_changes(
             daemon_conf,
-            share_id,
+            TEST_SHARE_ID,
             share_link_path,
             self.auth_keys_path
         )
