ucphhpc/migrid-sync

commit 04561455a4fcac3ddb6c82a2be5bf1b1c26320df

Prevent concurrent edit user account interference  (#406)
* Temporarily mark user account as 'locked' in user database during the entire
`edit_user` handling when the edit includes ID changes, to make sure that any
concurrent attempts to renew or edit the same user are prevented. This marking
covers not only the user database itself but also resulting filesystem updates
in that case.
Similarly it first checks that the status is not 'locked' when trying to edit
an existing user unless it's a meta-only edit. In the latter case the plain user
database locking already prevents concurrent edits so that should be safe.

* Adds a new official 'locked' account status for keeping track of such on-going account edits in line with issue #401 and default status to 'active' for old accounts where it is implicit in order to make sure status is properly restored
upon edit completion.

* Handles missing saved_status after edit and default to 'active' when extracting.

* Adds unit test to cover ordinary renew and (failing) renew when locked.

* Makes sure that failing in status check releases lock on user database as unit test clearly showed it didn't.

mig/shared/useradm.py
@@ -4,7 +4,7 @@
 # --- BEGIN_HEADER ---
 #
 # useradm - user administration functions
-# Copyright (C) 2003-2025  The MiG Project by the Science HPC Center at UCPH
+# Copyright (C) 2003-2026  The MiG Project by the Science HPC Center at UCPH
 #
 # This file is part of MiG.
 #
@@ -566,6 +566,11 @@ def create_user_in_db(configuration, db_path, client_id, user, now, authorized,
             _logger.debug("proceed with %s account during edit user" %
                           account_status)
         else:
+            if do_lock:
+                unlock_user_db(flock)
+            if verbose:
+                print('Refusing to renew %s account for %r' % (account_status,
+                                                               client_id))
             raise Exception('refusing to renew %s account! (%s)' %
                             (account_status, accepted_peer_list))
         if reset_token and not new_expire:
@@ -1297,6 +1302,7 @@ def edit_user(client_id, changes, removes, conf_path, db_path, force=False,
 
     user_dict = {}
     new_id = ''
+    saved_status = None
     try:
         old_user = user_db[client_id]
         user_dict.update(old_user)
@@ -1314,7 +1320,14 @@ def edit_user(client_id, changes, removes, conf_path, db_path, force=False,
                 if do_lock:
                     unlock_user_db(flock)
                 raise Exception("Edit aborted: new user already exists!")
-            _logger.info("Force old user renew to fix any missing files")
+            # Keep track of updates in status to prevent concurrent changes
+            if old_user.get('status', 'active') == 'locked':
+                if do_lock:
+                    unlock_user_db(flock)
+                raise Exception("Edit aborted: concurrent user update active!")
+            saved_status = user_dict.get('status', 'active')
+            old_user['status'] = user_dict['status'] = 'locked'
+            _logger.info("Force old user update to also fix any missing files")
             create_user(old_user, conf_path, db_path, force, verbose,
                         ask_renew=False, default_renew=True, do_lock=False,
                         from_edit_user=True, create_backup=True)
@@ -1556,7 +1569,15 @@ def edit_user(client_id, changes, removes, conf_path, db_path, force=False,
 
     _logger.info("Renamed user %s to %s" % (client_id, new_id))
     mark_user_modified(configuration, new_id)
-    _logger.info("Force new user renew to fix access")
+    _logger.info("Force new user renew to fix access and restore %s status" %
+                 saved_status)
+    if saved_status is None:
+        if verbose:
+            print('unexpectedly got no saved status after edit %r' % client_id)
+        _logger.error("something failed in edit user %r - delay unlocking" %
+                      client_id)
+    else:
+        user_dict['status'] = saved_status
     # NOTE: only backup user DB here if we didn't already do so in call above
     create_user(user_dict, conf_path, db_path, force, verbose,
                 ask_renew=False, default_renew=True, from_edit_user=True,

tests/test_mig_shared_useradm.py
@@ -3,7 +3,7 @@
 # --- BEGIN_HEADER ---
 #
 # test_mig_shared_useradm - unit test of the corresponding mig shared module
-# Copyright (C) 2003-2025  The MiG Project by the Science HPC Center at UCPH
+# Copyright (C) 2003-2026  The MiG Project by the Science HPC Center at UCPH
 #
 # This file is part of MiG.
 #
@@ -97,7 +97,8 @@ class TestMigSharedUsedadm_create_user(MigTestCase,
 
         _ensure_dirs_needed_for_userdb(self.configuration)
 
-        self.expected_user_db_home = os.path.normpath(configuration.user_db_home)
+        self.expected_user_db_home = os.path.normpath(
+            configuration.user_db_home)
         self.expected_user_db_file = os.path.join(
             self.expected_user_db_home, 'MiG-users.db')
 
@@ -159,8 +160,10 @@ class TestMigSharedUsedadm_create_user(MigTestCase,
 
         # TODO: remove resetting the handful of keys here
         #       this is done to allow the comparision to succeed
-        actual_user_object = _adjust_user_dict_for_compare(pickled[expected_user_id])
-        expected_user_object = _adjust_user_dict_for_compare(prepared.fixture_data[expected_user_id])
+        actual_user_object = _adjust_user_dict_for_compare(
+            pickled[expected_user_id])
+        expected_user_object = _adjust_user_dict_for_compare(
+            prepared.fixture_data[expected_user_id])
 
         self.maxDiff = None
         self.assertEqual(actual_user_object, expected_user_object)
@@ -188,6 +191,60 @@ class TestMigSharedUsedadm_create_user(MigTestCase,
         except:
             self.assertFalse(True, "should not be reached")
 
+    def test_user_creation_and_renew_records_a_user(self):
+        user_dict = {}
+        user_dict['full_name'] = "Test User"
+        user_dict['organization'] = "Test Org"
+        user_dict['state'] = "NA"
+        user_dict['country'] = "DK"
+        user_dict['email'] = "test@example.com"
+        user_dict['comment'] = "This is the create comment"
+        user_dict['locality'] = ""
+        user_dict['organizational_unit'] = ""
+        user_dict['password'] = ""
+        user_dict['password_hash'] = self.TEST_USER_PASSWORD_HASH
+        # explicitly setting set a DN suffixed user DN to force GDP
+        user_dict['distinguished_name'] = self.TEST_USER_DN_GDP
+        user_dict['status'] = "active"
+
+        try:
+            create_user(user_dict, self.configuration, keyword_auto,
+                        default_renew=True, ask_renew=False)
+        except:
+            self.assertFalse(True, "should not be reached")
+
+        try:
+            create_user(user_dict, self.configuration, keyword_auto,
+                        default_renew=True, ask_renew=False)
+        except:
+            self.assertFalse(True, "should not be reached")
+
+    def test_user_creation_fails_in_renew_when_locked(self):
+        user_dict = {}
+        user_dict['full_name'] = "Test User"
+        user_dict['organization'] = "Test Org"
+        user_dict['state'] = "NA"
+        user_dict['country'] = "DK"
+        user_dict['email'] = "test@example.com"
+        user_dict['comment'] = "This is the create comment"
+        user_dict['locality'] = ""
+        user_dict['organizational_unit'] = ""
+        user_dict['password'] = ""
+        user_dict['password_hash'] = self.TEST_USER_PASSWORD_HASH
+        # explicitly setting set a DN suffixed user DN to force GDP
+        user_dict['distinguished_name'] = self.TEST_USER_DN_GDP
+        user_dict['status'] = "locked"
+
+        try:
+            create_user(user_dict, self.configuration, keyword_auto,
+                        default_renew=True, ask_renew=False)
+        except:
+            self.assertFalse(True, "should not be reached")
+
+        with self.assertRaises(Exception):
+            create_user(user_dict, self.configuration, keyword_auto,
+                        default_renew=True, ask_renew=False)
+
 
 class MigSharedUseradm__assure_current_htaccess(MigTestCase):
     """Coverage of useradm behaviours around htaccess."""
@@ -211,8 +268,6 @@ class MigSharedUseradm__assure_current_htaccess(MigTestCase):
             with io.open(generated) as htaccess_file:
                 generated = htaccess_file.read()
 
-        #print("DEBUG: generated htaccess:\n%s" % generated)
-
         generated_lines = generated.split('\n')
         if not expected in generated_lines:
             raise AssertionError("no such require user line: %s" % expected)
@@ -229,7 +284,6 @@ class MigSharedUseradm__assure_current_htaccess(MigTestCase):
             # File should not exist here at all
             self.assertNotEqual(path_kind, "file")
         except OSError as ignore_oserr:
-            #print("DEBUG: oserror found as expected: %s" % ignore_oserr)
             pass
 
     def test_creates_missing_htaccess_file(self):

mig/shared/defaults.py
@@ -218,7 +218,7 @@ peers_expire_max_days = 3652
 # for caching account status using integer timestamps outside user DB.
 # IMPORTANT: generally do NOT rearrange order, and ONLY append new values
 #            unless also purging ALL status filemarks in the process.
-valid_account_status = ['active', 'temporal', 'suspended', 'retired']
+valid_account_status = ['active', 'temporal', 'suspended', 'retired', 'locked']
 
 auth_openid_mig_db = 'mod_auth_openid-mig-users.db'
 auth_openid_ext_db = 'mod_auth_openid-ext-users.db'

commit 913e5b108846386c84abc2abb9b39adc37f54fd0

Fix a regression introduced with #406 whereby expire and status cache marks for the original user are not removed after edit_user completes. This should be mostly harmless but can interfere with multiple renames where the temporary 'locked' mark doesn't get removed and can then get in the way. Add clean up as a part of edit_user to address the issue. Includes a regression test which helped confirm the issue and now assures it is fixed.

mig/shared/useradm.py
@@ -1374,6 +1374,10 @@ def edit_user(client_id, changes, removes, conf_path, db_path, force=False,
     update_account_expire_cache(configuration, user_dict)
     update_account_status_cache(configuration, user_dict)
 
+    # Clean up leftover markers for original user
+    update_account_expire_cache(configuration, old_user, delete=True)
+    update_account_status_cache(configuration, old_user, delete=True)
+
     if meta_only:
         return user_dict
 

tests/test_mig_shared_useradm.py
@@ -36,9 +36,17 @@ import unittest
 from past.builtins import basestring
 
 # Imports required for the unit test wrapping
+from mig.shared.accountstate import (
+    get_account_expire_cache,
+    get_account_status_cache,
+    update_account_expire_cache,
+    update_account_status_cache
+)
 from mig.shared.base import client_id_dir, distinguished_name_to_user
 from mig.shared.defaults import (
     DEFAULT_USER_ID_FORMAT,
+    expire_marks_dir,
+    status_marks_dir,
     htaccess_filename,
     keyword_auto,
     ssh_conf_dir,
@@ -708,6 +716,10 @@ class TestMigSharedUseradm__edit_user(
             self.expected_user_db_home, "MiG-users.db"
         )
         ensure_dirs_exist(self.configuration.mig_system_files)
+        ensure_dirs_exist(os.path.join(self.configuration.mig_system_run,
+                                       expire_marks_dir))
+        ensure_dirs_exist(os.path.join(self.configuration.mig_system_run,
+                                       status_marks_dir))
         ensure_dirs_exist(self.configuration.resource_home)
 
     def _provide_configuration(self):
@@ -881,8 +893,8 @@ class TestMigSharedUseradm__edit_user(
         self._flush_test_user(TEST_USER_DN)
         self._flush_test_user(OTHER_USER_DN)
 
-    def test_edit_user_renames_user(self):
-        """Test editing fields that change the distinguished name"""
+    def test_edit_user_renames_user_with_cache_updates(self):
+        """Test edit user fields that change the user ID with cache updates"""
         self._provision_test_user(self, TEST_USER_DN)
         # Extract original for later verification
         pickled = self.assertPickledFile(
@@ -890,7 +902,23 @@ class TestMigSharedUseradm__edit_user(
             apply_hints=["convert_dict_bytes_to_strings_kv"],
         )
         user_dict = pickled[TEST_USER_DN]
+        # NOTE: we set missing status field here for testing later
+        user_dict["status"] = "active"
+        old_home = os.path.join(self.configuration.user_home, TEST_USER_DIR)
+        self.assertTrue(os.path.isdir(old_home))
+        # Mimic account expire and status cache updates from real use
+        result = update_account_expire_cache(self.configuration, user_dict)
+        self.assertTrue(result)
+        cached_expire = get_account_expire_cache(self.configuration,
+                                                 TEST_USER_DN)
+        self.assertEqual(cached_expire, user_dict["expire"])
+        result = update_account_status_cache(self.configuration, user_dict)
+        self.assertTrue(result)
+        cached_status = get_account_status_cache(self.configuration,
+                                                 TEST_USER_DN)
+        self.assertEqual(cached_status, user_dict["status"])
 
+        # Now proceed with actual function test
         changes = {"full_name": "Renamed User"}
         removes = []
         result = edit_user(
@@ -898,9 +926,9 @@ class TestMigSharedUseradm__edit_user(
             keyword_auto
         )
 
+        # Verify that the requested updates have fully completed
         new_dn = result["distinguished_name"]
         new_dir = new_dn.replace("/", "+").replace(" ", "_")
-
         pickled = self.assertPickledFile(
             self.expected_user_db_file,
             apply_hints=["convert_dict_bytes_to_strings_kv"],
@@ -916,16 +944,23 @@ class TestMigSharedUseradm__edit_user(
             else:
                 self.assertEqual(edited_user[field], user_dict[field])
 
-        old_home = os.path.join(self.configuration.user_home, TEST_USER_DIR)
         new_home = os.path.join(self.configuration.user_home, new_dir)
         self.assertFalse(os.path.exists(old_home))
         self.assertTrue(os.path.isdir(new_home))
 
-        old_settings = os.path.join(
-            self.configuration.user_settings, TEST_USER_DIR)
+        old_settings = os.path.join(self.configuration.user_settings,
+                                    TEST_USER_DIR)
         new_settings = os.path.join(self.configuration.user_settings, new_dir)
         self.assertFalse(os.path.exists(old_settings))
         self.assertTrue(os.path.isdir(new_settings))
+
+        # Make sure even expire and status cache files are gone
+        cached_expire = get_account_expire_cache(self.configuration,
+                                                 TEST_USER_DN)
+        self.assertEqual(cached_expire, None)
+        cached_status = get_account_status_cache(self.configuration,
+                                                 TEST_USER_DN)
+        self.assertEqual(cached_status, None)
 
         self._flush_test_user(new_dn)
 