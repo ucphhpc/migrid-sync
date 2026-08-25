2c546bea71588b55e4a7ff6c4c3002fe10120608
rasmunk <rasmunk@users.noreply.github.com>
2026-08-25 14:32:58 +0200

    Bump the migrid-ux repository revision and the version that should be installed 1.8.0

--- envhelp/scripts/expand_sources.py
+++ envhelp/scripts/expand_sources.py
@@ -43,7 +43,7 @@ SOURCE_URL_BASE = "https://github.com/ucphhpc"
 PACKAGE_REPOS_TO_BRANCH = {
     "migux": {
         "repository": "migrid-ux",
-        "repository_rev": "1b3638fb04bef69124780f3be877d9013107add7",
+        "repository_rev": "442cb65f466e8ba119747e3eae841519d64e6367",
     }
 }
 
