62b409a7b45239a5d8ccff4ee22d2aab35d2985f
rasmunk <rasmunk@users.noreply.github.com>
2026-09-17 10:45:34 +0200

    Bump migux to version 1.19.0

--- envhelp/scripts/expand_sources.py
+++ envhelp/scripts/expand_sources.py
@@ -43,7 +43,7 @@ SOURCE_URL_BASE = "https://github.com/ucphhpc"
 PACKAGE_REPOS_TO_BRANCH = {
     "migux": {
         "repository": "migrid-ux",
-        "repository_rev": "9df41f8672143e12f1b9bb1bea70be4d9ab741ed",
+        "repository_rev": "973472aabcf5269aaf2a1fa2f4a55f080bf2ef6b",
     }
 }
 
